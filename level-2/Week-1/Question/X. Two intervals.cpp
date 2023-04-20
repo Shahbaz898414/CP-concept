b2bccb9bb6a30f08a9848db90fa843266d0d1cc7
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-20 21:10:27 +0530

    1

---
+++ level-2/Week-1/Question/X. Two intervals.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+  
+  int a,b,c,d;cin>>a>>b>>c>>d;
+   int mx1=max(a,b);
+   int mx2=min(c,d);
+
+   
+
+
+
+
+  return 0;
+}
