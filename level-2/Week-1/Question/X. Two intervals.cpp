5e617683c99ba4e5082bcbd22305d713d2215031
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-20 21:50:53 +0530

    1

--- level-2/Week-1/Question/X. Two intervals.cpp
+++ level-2/Week-1/Question/X. Two intervals.cpp
@@ -5,17 +5,24 @@ using namespace std;
 int main(){
   
   int a,b,c,d;cin>>a>>b>>c>>d;
-   int m1=max(a,b);
-   int m2=min(a,b);
+   int m2=max(a,b);
+   int m1=min(a,b);
    int m3=min(c,d);
    int m4=max(c,d);
 
-   if(mx2>mx1){
+   if(m3>m2){
     cout<<-1<<endl;
    }else {
+    if(m1==m3) 
+      cout<<m2<<" "<<m3<<endl;
+    else {
+      if(m2>m4){
+        cout<<m3<< " "<<m4<<endl;
+      }else {
+        cout<<m3<<" "<<m2<<endl;
+      }
+    }  
 
-    
-      cout<<mx2<<" "<<mx1<<endl;
    }
 
    
