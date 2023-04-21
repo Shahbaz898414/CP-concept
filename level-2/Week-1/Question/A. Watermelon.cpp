8d0043d92ca82992c9a4cd4bc12a1808a5635129
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-21 21:19:35 +0530

    1

--- level-2/Week-1/Question/A. Watermelon.cpp
+++ level-2/Week-1/Question/A. Watermelon.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 
+
 int main() {
   
   int n;cin>>n;
+  if(n<=2) cout<<"NO"<<endl;
+  else {
+    if(n%2)cout<<"NO"<<endl;
+    else {
+      cout<<"YES"<<endl;
+    }
+  }
 
 
   return 0;
