c12758d872b1897694f6d5dcc188e35f9c4d7209
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-21 23:28:39 +0530

    1

---
+++ level-2/Week-1/Question/Matched Brackets.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+
+int main() {
+  
+  int n,m;cin>>n;
+
+  int arr[n];
+
+  for(int i=0;i<n;i++) {
+    cin>>arr[i];
+  }
+
+  for(int i=0;i<n;i++) {
+    cout<<arr[i]<<" ";
+  }
+
+  
+
+
+
+
+  return 0;
+}
