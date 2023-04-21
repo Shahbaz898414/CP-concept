62177469b112e46a770dd5661676baf005fbc5f3
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-21 21:50:20 +0530

    1

--- level-2/Week-1/Question/A. Domino piling.cpp
+++ level-2/Week-1/Question/A. Domino piling.cpp
@@ -6,19 +6,10 @@ using namespace std;
 
 int main() {
   
-  int t;cin>>t;
+  int n,m;cin>>n>>m;
 
-  while(t--) {
-    string s;cin>>s;
 
-    int len=s.size();
 
-    if(len<=10){
-      cout<<s<<endl;
-    }else {
-       cout<<s[0]<<len-2<<s[len-1]<<endl;
-    }
-   
-  }
+  
   return 0;
 }
