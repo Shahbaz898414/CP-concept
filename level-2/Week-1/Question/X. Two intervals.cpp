2e538073ccd8089cbfc3322a2e2244be1841ddae
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-21 20:59:39 +0530

    1

--- level-2/Week-1/Question/X. Two intervals.cpp
+++ level-2/Week-1/Question/X. Two intervals.cpp
@@ -5,27 +5,26 @@ using namespace std;
 
 int main() {
   
-  int n1,n2,n3,n4;
+   int n1,n2,n3,n4;
   cin>>n1>>n2>>n3>>n4;
   int start,end;
-  if((n3<n1 && n4<n1)||(n3>n2 && n4>n2)) {
+  if((n3<n1 && n4<n1)||(n3>n2 && n4>n2)){
     cout<<-1<<endl;
-  }else {
-    if(n1>n3) {
+  }else{
+    if(n1>n3){
       start=n1;
  
-    }else {
+    }else{
       start=n3;
     }
-    if(n2>n4) {
+    if(n2>n4){
       end=n4;
-    }else {
+    }else{
       end=n2;
     }
-    cout<<start<<" "<<end<<endl;
-
+    cout<<start<<" "<<end;
+    
   }
-
 
   return 0;
 }
