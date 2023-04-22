ce466f735d9c09f33bb29a98a51befa524b4138a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-22 14:25:23 +0530

    1

--- level-2/Week-1/Question/Matched Brackets.cpp
+++ level-2/Week-1/Question/Matched Brackets.cpp
@@ -6,13 +6,45 @@ using namespace std;
 
 int main() {
   
-  int n,m;cin>>n;
+  int n;
+    cin >> n;
 
-  int arr[n];
+    vector<int> stck;
+    vector<pair<int, int>> closed;
 
-  for(int i=0;i<n;i++) {
-    cin>>arr[i];
-  }
+    int nesting = 0;
+    int nesting_pos = -1;
+
+    int longest = 0;
+    int longest_pos = -1;
+
+    for (int i = 1; i <= n; i++) {
+        int inp;
+        cin >> inp;
+
+        if (inp == 1) {
+            stck.push_back(i);
+            if (stck.size() > nesting) {
+                nesting = stck.size();
+                nesting_pos = i;
+            }
+        } else {
+            int el = stck.back();
+            stck.pop_back();
+            closed.push_back({el, i});
+        }
+    }
+
+    for (auto el: closed) {
+        if (el.second - el.first + 1 > longest) {
+            longest = el.second - el.first + 1;
+            longest_pos = el.first;
+        }
+    }
+
+    cout << nesting << " " << nesting_pos << " " << longest << " " << longest_pos << endl;
+
+
 
 
   
