a2eff1804413e1968760960e426f7aad2f0a08c2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-04-23 16:59:05 +0530

    1

---
+++ level-2/Week-1/Question/Compilers and parsers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+
+int main() {
+  
+  int t;cin>>t;
+
+  while(t--) {
+    string s;cin>>s;
+    int len=s.size();
+  }
+
+
+  return 0;
+}
--- level-2/Week-1/Question/Matched Brackets.cpp
+++ level-2/Week-1/Question/Matched Brackets.cpp
@@ -6,45 +6,38 @@ using namespace std;
 
 int main() {
   
-  int n;cin >> n;
-
-    vector<int> stck;
-    vector<pair<int, int>> closed;
-
-    int nesting = 0;int nesting_pos = -1;
-
-    int longest = 0;
-    int longest_pos = -1;
-
-    for (int i = 1; i <= n; i++) {
-        int inp;
-        cin >> inp;
-
-        if (inp == 1) {
-            stck.push_back(i);
-            if (stck.size() > nesting) {
-                nesting = stck.size();
-                nesting_pos = i;
+  int n; cin >> n;
+    int v;
+    int pos = 1;
+    int posOpening, posDepth, posWidthMax;
+    int depth = 0;
+    int depthMax = 0;
+    int widthMax = 0;
+    for(int i=0; i<n; i++) {
+        cin >> v;
+        if(v==1) { // opening bracket
+            if(depth==0)
+                posOpening = pos;
+            depth++;
+            if(depth>depthMax) {
+                depthMax = depth;
+                posDepth = pos;
             }
-
-        } else {
-            int el = stck.back();
-            stck.pop_back();
-            closed.push_back({el, i});
         }
-    }
-
-    for (auto el: closed) {
-        if (el.second - el.first + 1 > longest) {
-            longest = el.second - el.first + 1;
-            longest_pos = el.first;
+        else { // closing bracket
+            depth--;
+            if(depth==0) {
+                int width = pos-posOpening+1;
+                if(width>widthMax) {
+                    widthMax = width;
+                    posWidthMax = posOpening;
+                }
+            }
         }
+        pos++;
     }
-
-    cout << nesting << " " << nesting_pos << " " << longest << " " << longest_pos << endl;
-
-
-
+    cout << depthMax << ' ' << posDepth << ' ';
+    cout << widthMax << ' ' <<  posWidthMax << endl;
 
   
 
