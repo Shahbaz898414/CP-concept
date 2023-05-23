f90bf618ed9bcd5bca423949efdb6c37f381562b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-23 16:30:50 +0530

    1

--- level-3/Week-1/Questions/B. Password.cpp
+++ level-3/Week-1/Questions/B. Password.cpp
@@ -12,32 +12,7 @@ int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-     string s;
-    cin>>s;
-    int n = s.size();
-    vector<int> pfx(n);
-    vector<int> sfx(n);
-    long long p = 1;
-    for(int i=0;i<n;++i){
-        pfx[i] = (p*1LL*(s[i]-'a'+1))%mod;
-        if(i != 0){
-            pfx[i] += pfx[i-1];
-            pfx[i] %= mod;
-        }
-        p *= 31;
-        p %= mod;
-    }
-    p = 31;
-    for(int i=n-1;i>=0;--i){
-        sfx[i] = (s[i]-'a'+1);
-        if(i != n-1){
-            sfx[i] += ((p*1LL*sfx[i+1])%mod);
-            sfx[i] %= mod;
-        }
-    }
-
-
-   
+       
 }
 
 
