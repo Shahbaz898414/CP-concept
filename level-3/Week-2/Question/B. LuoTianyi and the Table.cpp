19db916f6ae7b864042a0a003f045922ec979448
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-27 21:52:11 +0530

    1

--- level-3/Week-2/Question/B. LuoTianyi and the Table.cpp
+++ level-3/Week-2/Question/B. LuoTianyi and the Table.cpp
@@ -1,17 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MOD=1e9+7;
+const int MOD = 1e9 + 7;
 
+int main()
+{
 
-int main() {
-  
-  
-  
+  int t;
+  cin >> t;
+
+  while (t--)
+  {
+    int n, m;
+    cin >> n >> m;
+
+    int arr[n * m];
+
+    for (int i = 0; i < n * m; i++)
+    {
+      cin >> arr[i];
+    }
+
+    sort(arr, arr + n * m);
+    if (n < m)
+      swap(n, m);
+    long long ans = max((n - 1) * m * (arr[n * m] - arr[1]) + (m - 1) * (arr[n * m] - arr[2]), (n - 1) * m * (arr[n * m] - arr[1]) + (m - 1) * (arr[n * m - 1] - arr[1]));
+    cout << ans << "\n";
+  }
+
   return 0;
 }
-
-
-
-
-
