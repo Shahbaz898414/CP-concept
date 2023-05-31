6aca40a1901f1ff3bc142ca0a5f95e2bde2ce4e8
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-05-31 13:24:43 +0530

    1

--- level-3/Week-2/Question/152. Maximum Product Subarray.cpp
+++ level-3/Week-2/Question/152. Maximum Product Subarray.cpp
@@ -1,25 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-  int maxProduct(vector<int> &nums)
-  {
+  int maxProduct(vector<int> &nums) {
     vector<int> v(nums.size(), 0);
     int odd = 0, oval;
     int till = 1, ans = nums[0];
     v[0] = nums[0];
-    if (nums[0] < 0)
-    {
+    if (nums[0] < 0) {
       oval = 0;
       odd += 1;
     }
 
-    for (int i = 1; i < nums.size(); i++)
-    {
-      if (nums[i] > 0)
-      {
+    for (int i = 1; i < nums.size(); i++) {
+      if (nums[i] > 0) {
         if (v[i - 1] < 0)
           v[i] = nums[i];
         else if (!v[i - 1])
@@ -27,8 +22,7 @@ public:
         else
           v[i] = nums[i] * v[i - 1];
       }
-      else if (nums[i] < 0)
-      {
+      else if (nums[i] < 0) {
         if (v[i - 1])
           v[i] = v[i - 1] * nums[i];
         else
@@ -36,28 +30,26 @@ public:
         odd += 1;
         if (odd == 1)
           oval = i;
-        else
-        {
-          if (odd % 2 == 0)
-          {
+        else {
+          if (odd % 2 == 0) {
             if (oval == i - 1)
               continue;
             v[i] *= v[oval];
           }
-          else
-          {
+          else {
             v[i] /= v[oval];
           }
         }
       }
-      else
-      {
+      else {
         v[i] = 0;
         odd = 0;
       }
     }
+
+
     for (int x : v)
-      ans = max(ans, x);
+      ans = max(ans, x); 
     return ans;
   }
 };
@@ -104,14 +96,14 @@ int maxProduct(vector<int>& arr) {
 
 class Solution {
 public:
+
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
 
         int maxi =1 , mini=1, res = INT_MIN;
 
         for(int i =0;i<n;i++){
-            int temp = maxi;
-    // storing maxi in temp as it's value will change in next line 
+            int temp = maxi; 
             maxi = max({nums[i], maxi*nums[i], mini*nums[i]});
             mini = min({nums[i], temp*nums[i], mini*nums[i]});
 
@@ -119,6 +111,8 @@ public:
         }
         return res;
     }
+
+
 };
 
 
