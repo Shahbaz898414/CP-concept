2f9acf3690810225c01c46d39b55f824f6f8ef7e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-01 23:06:42 +0530

    1

---
+++ Graph/Number of Provinces.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+    }
+};
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
