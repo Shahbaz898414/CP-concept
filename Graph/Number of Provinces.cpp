c9deb95cd37bf4363c31fbeeb262100a386281de
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-01 23:33:52 +0530

    1

--- Graph/Number of Provinces.cpp
+++ Graph/Number of Provinces.cpp
@@ -2,40 +2,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-class Solution {
-  public:
-    int numProvinces(vector<vector<int>> adj, int V) {
-       
-    }
+class Solution
+{
+public:
+  int numProvinces(vector<vector<int>> adj, int V)
+  {
+  }
 };
 
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int V, x;
+    cin >> V;
 
+    vector<vector<int>> adj;
 
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V,x;
-        cin>>V;
-        
-        vector<vector<int>> adj;
-        
-        for(int i=0; i<V; i++)
-        {
-            vector<int> temp;
-            for(int j=0; j<V; j++)
-            {
-                cin>>x;
-                temp.push_back(x);
-            }
-            adj.push_back(temp);
-        }
-        
-
-        Solution ob;
-        cout << ob.numProvinces(adj,V) << endl;
+    for (int i = 0; i < V; i++)
+    {
+      vector<int> temp;
+      for (int j = 0; j < V; j++)
+      {
+        cin >> x;
+        temp.push_back(x);
+      }
+      adj.push_back(temp);
     }
-    return 0;
+
+    Solution ob;
+    cout << ob.numProvinces(adj, V) << endl;
+  }
+  return 0;
 }
