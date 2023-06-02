0845e11057b9485d1abdacf835a443e704f43dde
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-03 00:09:09 +0530

    1

--- Graph/Rotten Oranges.cpp
+++ Graph/Rotten Oranges.cpp
@@ -1,27 +1,63 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution 
+class Solution
 {
-    public:
-   
-    int orangesRotting(vector<vector<int>>& grid) {
-       
-    }
+public:
+	int orangesRotting(vector<vector<int>> &grid)
+	{
+		//  Die here
+		int n = grid.size();
+		int m = grid[0].size();
+
+		queue << pair<int, int>, int >> q;
+		vector<vector<int>> vis;
+
+		for (int i = 0; i < n; i++) {
+			/*    */
+			for (int j = 0; j < m; j++) {
+				/*    */
+
+				if (grid[i][j] == 2) {
+					q.push({{i,j},0});
+					vis[i][j]=2;
+				}
+			}
+		}
+
+		int tm=0;
+
+		while(!q.empty()){
+			int r=q.front().first.first;
+			int c=q.front().first.second;
+
+			int t=q.front().second;
+
+			q.pop();
+
+
+		}
+
+
+
+
+	}
 };
 
-
-int main(){
+int main()
+{
 	int tc;
 	cin >> tc;
-	while(tc--){
+	while (tc--)
+	{
 		int n, m;
 		cin >> n >> m;
-		vector<vector<int>>grid(n, vector<int>(m, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
+		vector<vector<int>> grid(n, vector<int>(m, -1));
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
 				cin >> grid[i][j];
 			}
 		}
