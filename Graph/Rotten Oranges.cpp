d43b0d4859b6cc40086b1e22724171aaaceaf904
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-03 00:10:11 +0530

    1

--- Graph/Rotten Oranges.cpp
+++ Graph/Rotten Oranges.cpp
@@ -53,16 +53,17 @@ int main()
 		int n, m;
 		cin >> n >> m;
 		vector<vector<int>> grid(n, vector<int>(m, -1));
+
 		for (int i = 0; i < n; i++)
-		{
 			for (int j = 0; j < m; j++)
-			{
+			
 				cin >> grid[i][j];
-			}
-		}
+		
+
 		Solution obj;
 		int ans = obj.orangesRotting(grid);
 		cout << ans << "\n";
+
 	}
 	return 0;
 }
