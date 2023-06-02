cd14df76543b81d97ebd989aa869400d067439e4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-02 22:23:06 +0530

    1

--- level-3/Week-2/Question/2321. Maximum Score Of Spliced Array.cpp
+++ level-3/Week-2/Question/2321. Maximum Score Of Spliced Array.cpp
@@ -2,8 +2,7 @@
 #define ll long long
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
   int maxSubArraySum(vector<int>&a,vector<int> &b,int size){
     int mx1=0,mx2=0;
@@ -44,55 +43,11 @@ public:
   }
 };
 
-int main()
-{
+int main() {
   ll t = 1;
   // cin >> t;
-  while (t--)
-  {
-    string s;
-    cin >> s;
-    int len = s.size();
-    vector<int> vec(26, -1);
-
-    for (int i = 0; i < len; i++)
-    {
-      vec[s[i] - 'a'] = i;
-    }
-
-    string s1, s2;
-
-    int i = 0;
-    int cur = 0;
-
-    while (i < len)
-    {
-      while (i <= vec[cur])
-      {
-        s1 += s[i];
-        while (s1.size() and s1.back() - 'a' == cur)
-        {
-          s2 += s1.back();
-          s1.pop_back();
-        }
-
-        i++;
-      }
-
-      cur++;
-
-      while (s1.size() and s1.back() - 'a' <= cur)
-      {
-        s2 += s1.back();
-        s1.pop_back();
-      }
-    }
-
-    reverse(s1.begin(), s1.end());
-
-    s2 += s1;
-
-    cout << s2 << endl;
+  while (t--) {
+    
   }
 }
 
