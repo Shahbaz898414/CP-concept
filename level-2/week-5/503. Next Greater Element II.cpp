674786b2fc22048b8e59969d9e09cceb457f0240
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-04 23:09:42 +0530

    1

--- level-2/week-5/503. Next Greater Element II.cpp
+++ level-2/week-5/503. Next Greater Element II.cpp
@@ -101,28 +101,28 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
 
-    vector<int> vec;
+        vector<int> vec;
 
-    for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
 
-      bool flag=false;
-      int k=i+1;
-       int idx=INT_MIN;
+          bool flag=false;
+          int k=i+1;
+          int idx=INT_MIN;
 
-        while(idx!=i){
+            while(idx!=i){
 
-            idx=k%nums.size();
-            if(nums[idx]>nums[i]){
-                flag=true;
-                vec.push_back(nums[idx]);
-                break;
+                idx=k%nums.size();
+                if(nums[idx]>nums[i]){
+                    flag=true;
+                    vec.push_back(nums[idx]);
+                    break;
+                }
+                k++;
+
             }
-            k++;
-
+            if(!flag) vec.push_back(-1);
         }
-         if(!flag) vec.push_back(-1);
-    }
-        return vec;
+            return vec;
 
     }
 };
