8b4e752419f267f127894927154cf248bf28a443
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-04 23:09:06 +0530

    1

--- Graph/Rotten Oranges.cpp
+++ Graph/Rotten Oranges.cpp
@@ -11,44 +11,70 @@ public:
 		int n = grid.size();
 		int m = grid[0].size();
 
-		queue << pair<int, int>, int >> q;
-		vector<vector<int>> vis;
+		queue <pair< pair<int, int>, int >> q;
+		int vis[n][m];
 
-		for (int i = 0; i < n; i++) {
-			/*    */
-			for (int j = 0; j < m; j++) {
-				/*    */
+		for (int i = 0; i < n; i++)
+		{
+			/* Die here   */
+			for (int j = 0; j < m; j++)
+			{
+				/* Die here   */
 
-				if (grid[i][j] == 2) {
-					q.push({{i,j},0});
-					vis[i][j]=2;
+				if (grid[i][j] == 2)
+				{
+					q.push({{i, j}, 0});
+					vis[i][j] = 2;
+				}
+				else
+				{
+					vis[i][j] = 0;
+				}
+			}
+		}
+
+		int tm = 0;
+
+		int drow[] = {-1, 0, +1, 0};
+		int dcol[] = {0, 1, 0, -1};
+
+		while (!q.empty())
+		{
+			int r = q.front().first.first;
+			int c = q.front().first.second;
+
+			int t = q.front().second;
+			tm = max(tm, t);
+			q.pop();
+
+			for (int i = 0; i < 4; i++)
+			{
+				int nrow = r + drow[i];
+				int ncol = c + dcol[i];
+				if (nrow >= 0 and nrow < n and ncol >= 0 and ncol < m and vis[nrow][ncol] != 2 and grid[nrow][ncol] == 1)
+				{
+					q.push({{nrow, ncol}, t + 1});
+					vis[nrow][ncol] = 2;
 				}
 			}
 		}
 
 
-		int tm=0;
-
-		int drow[]={-1,0,+1,0};
-		int dcol[]={0,1,0,-1};
-
-		while(!q.empty()){
-			int r=q.front().first.first;
-			int c=q.front().first.second;
-
-			int t=q.front().second;
-
-			q.pop();
-
-			for (int i = 0; i <4; i++) {
-				int nrow=r+drow[i];
-				int ncol=c+dcol[i];
+		for (int i = 0; i < n; i++)
+		{
+			/* code */
+			for (int j = 0; j < m; j++)
+			{
+				/* code */
+				if(vis[i][j]!=2 and grid[i][j]==1){
+					return -1;
+				}
 			}
 			
-
-
 		}
 
+		return tm;
+		
 	}
 };
 
@@ -64,14 +90,12 @@ int main()
 
 		for (int i = 0; i < n; i++)
 			for (int j = 0; j < m; j++)
-			
+
 				cin >> grid[i][j];
-		
 
 		Solution obj;
 		int ans = obj.orangesRotting(grid);
 		cout << ans << "\n";
-
 	}
 	return 0;
 }
