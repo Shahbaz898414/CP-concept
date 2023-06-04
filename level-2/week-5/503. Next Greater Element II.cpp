39f26c9f783913f66170a82700383530c97240de
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-04 22:15:08 +0530

    1

---
+++ level-2/week-5/503. Next Greater Element II.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        
+    }
+};
+
+int main() {
+  int n;cin>>n;
+
+  vector<int>  v(n);
+
+  for (int i = 0; i < n; i++ ) {
+
+    cin >>arr[i];
+  }
+
+  vector<int>  num=nextGreaterElements(v);
+
+  for(auto it:num){
+    cout<<it<<" ";
+  }
+  
+  cout<<endl;
+}
