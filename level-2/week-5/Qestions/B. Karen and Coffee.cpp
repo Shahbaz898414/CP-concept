8553c924970a4dd7b315eda46f9f2425d8b6690f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-04 18:15:56 +0530

    1

--- level-2/week-5/Qestions/B. Karen and Coffee.cpp
+++ level-2/week-5/Qestions/B. Karen and Coffee.cpp
@@ -1,40 +1,37 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 
 #define ll long long
 using namespace std;
 
+int main()
+{
 
-int main() {
-
-  int n,k,q;cin>>n>>k>>q;
-  vector<ll>  v(n+1,0);
-  vector<ll> bt(n+1,0);
-  for (int i = 0; i <n; i++) {
-    ll x,y;cin>>x>>y;
+  int n, k, q;
+  cin >> n >> k >> q;
+  vector<ll> v(n + 1, 0);
+  vector<ll> bt(n + 1, 0);
+  for (int i = 0; i < n; i++)
+  {
+    ll x, y;
+    cin >> x >> y;
     v[x]++;
-    v[y]--;
-
+    v[y + 1]--;
   }
 
+  for (int i = 0; i < n + 1; i++)
+  {
 
+    cout << v[i] << " ";
+  }
 
-  // for (int i = 0; i <=n+1; i++)
-  // {
-  
-  //   cout<<v[i]<<" ";
-  // }
+  cout << endl;
 
-  // cout<<endl;
+  ll sum = 0;
 
-  ll sum=0;
-
-  
-  
-
-
-
+  for (int i = 0; i < n; i++)
+  {
+    /* code */
+    sum += v[i];
+    bt[i] = sum;
+  }
 }
-
-
-
