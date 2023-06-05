bfeeebd6f46130e60617fec75f4924f246028afc
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-06 01:31:03 +0530

    1

--- level-2/week-5/Qestions/Cherry and Bits.cpp
+++ level-2/week-5/Qestions/Cherry and Bits.cpp
@@ -2,24 +2,35 @@
 #define ll long long
 using namespace std;
 
+int main()
+{
+  ll n, m;
+  cin >> n >> m;
 
+  ll arr[n + 1][m + 1];
 
-int main() {
-  ll n,m;cin>>n>>m;
-
-
-  ll arr[n][m];
-
-  for (ll i = 0; i < n; i++)
+  for (ll i = 1; i <= n; i++)
   {
-    /* code */
-    for (ll j = 0; j < count; j++)
-    {
-      /* code */
-      
-    }
-    
-  }
-  
-}
 
+    for (ll j = 1; j <= m; j++)
+    {
+
+      cin >> arr[i][j];
+    }
+  }
+
+  ll ans[n + 1][m + 1];
+
+  for (int i = 1; i <= n; i++)
+  {
+    /*  */
+    for (int j = 1; j <= m; j++)
+    {
+      /*  */
+    }
+  }
+
+  while (m-- > 0)
+  {
+  }
+}
