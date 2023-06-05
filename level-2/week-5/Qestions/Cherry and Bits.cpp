d023cf7bd65c86217f463e312d73336d9f6a0d95
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-06 02:44:18 +0530

    1

--- level-2/week-5/Qestions/Cherry and Bits.cpp
+++ level-2/week-5/Qestions/Cherry and Bits.cpp
@@ -56,7 +56,7 @@ int main()
   // {
   //   for (int j = 1; j <= n; j++)
   //   {
-  //     cout<<ans[j][i]<<' '; 
+  //     cout<<ans[j][i]<<' ';
   //   }
   //   cout<<endl;
   // }
@@ -66,7 +66,7 @@ int main()
   // {
   //   for (int j = 1; j <= m; j++)
   //   {
-  //   cout<<ans[j][i]<<' '; 
+  //   cout<<ans[j][i]<<' ';
   //   }
   //   cout<<endl;
   // }
@@ -88,8 +88,8 @@ int main()
 
   // cout << endl;
 
- 
- 
+
+
     for(int i = 0 ; i <= m ; i++){
         for(int j = 1 ; j <= n ; j++){
             ans[j][i] += ans[j-1][i];
@@ -128,55 +128,83 @@ int main()
 
   // cout << endl;
 
- 
+
 
 
 }
 */
 
-int n , m;
-    cin>>n >>m;
-    int a[n][m];
-    // int b[n+1][m+1];
-    vector<vector<int>> b(n+1, vector<int>(m+1, 0));
-    char c;
-    for(int i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < m ; j++){
-            cin>>c;
-            a[i][j] = c-'0';
-        }
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  int a[n][m];
+  // int b[n+1][m+1];
+  vector<vector<int>> b(n + 1, vector<int>(m + 1, 0));
+  char c;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cin >> c;
+      a[i][j] = c - '0';
     }
-    int q;
-    cin>>q;
-    while(q--){
-        int x1 , y1 , x2 , y2;
-        cin>>x1 >>y1 >>x2 >>y2;
-        b[x1][y1] ++;
-        if(x2+1<=n && y2+1<=m)
-        b[x2+1][y2+1] ++;
-        if(y2+1<=m)
-        b[x1][y2+1] --;
-        if(x2+1<=n)
-        b[x2+1][y1] --;
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      cout << a[i][j] << " ";
     }
-    for(int i = 0 ; i <= m ; i++){
-        for(int j = 1 ; j <= n ; j++){
-            b[j][i] += b[j-1][i];
-        }
+    cout << endl;
+  }
+
+  
+  int q;
+  cin >> q;
+  while (q--)
+  {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    b[x1][y1]++;
+    if (x2 + 1 <= n && y2 + 1 <= m)
+      b[x2 + 1][y2 + 1]++;
+    if (y2 + 1 <= m)
+      b[x1][y2 + 1]--;
+    if (x2 + 1 <= n)
+      b[x2 + 1][y1]--;
+  }
+  for (int i = 0; i <= m; i++)
+  {
+    for (int j = 1; j <= n; j++)
+    {
+      b[j][i] += b[j - 1][i];
     }
-    for(int i = 0 ; i <= n ; i++){
-        for(int j = 1 ; j <= m ; j++){
-            b[i][j] += b[i][j-1];
-        }
+  }
+  for (int i = 0; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      b[i][j] += b[i][j - 1];
     }
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1 ; j <= m ; j++){
-            if(b[i][j] % 2 == 0){
-                cout<<a[i-1][j-1];
-            }
-            else{
-                cout<<(a[i-1][j-1] ^ 1);
-            }
-        }
-        cout<<endl;
+  }
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 1; j <= m; j++)
+    {
+      if (b[i][j] % 2 == 0)
+      {
+        cout << a[i - 1][j - 1];
+      }
+      else
+      {
+        cout << (a[i - 1][j - 1] ^ 1);
+      }
     }
+    cout << endl;
+  }
+  return 0;
+}
