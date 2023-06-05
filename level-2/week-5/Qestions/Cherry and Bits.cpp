48d6da9e5755f2eae9a8afade8a1b72d0afa6b88
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-06 01:58:03 +0530

    1

--- level-2/week-5/Qestions/Cherry and Bits.cpp
+++ level-2/week-5/Qestions/Cherry and Bits.cpp
@@ -25,12 +25,26 @@ int main()
   {
     int x1, y1, x2, y2;
     cin >> x1 >> y1 >> x2 >> y2;
-    b[x1][y1]++;
+    ans[x1][y1]++;
     if (x2 + 1 <= n && y2 + 1 <= m)
-      b[x2 + 1][y2 + 1]++;
+      ans[x2 + 1][y2 + 1]++;
     if (y2 + 1 <= m)
-      b[x1][y2 + 1]--;
+      ans[x1][y2 + 1]--;
     if (x2 + 1 <= n)
-      b[x2 + 1][y1]--;
+      ans[x2 + 1][y1]--;
   }
+
+  for (int i = 1; i <= n; i++)
+  {
+
+    for (int j = 1; j <= m; j++)
+    {
+
+      cout << ans[i][j] << " ";
+    }
+
+    cout << endl;
+  }
+
+  cout << endl;
 }
