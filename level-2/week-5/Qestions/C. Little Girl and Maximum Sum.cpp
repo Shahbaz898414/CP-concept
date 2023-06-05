9469175b012d622da6db35bfe775e13181ee0c6e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-05 20:33:13 +0530

    1

--- level-2/week-5/Qestions/C. Little Girl and Maximum Sum.cpp
+++ level-2/week-5/Qestions/C. Little Girl and Maximum Sum.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 #define ll long long
 using namespace std;
 
@@ -7,7 +6,17 @@ int main()
 {
   int n, q;
   cin >> n >> q;
-  long long a[200010]={},t[200010]={},f=0;
+  long long a[200010] = {}, cnt[200010] = {}, f = 0;
+
+  for (int i = 0; i < n; i++)
+    cin >> a[i];
+
+  for (int i = 1; i <= q; i++)
+  {
+    cin >> l >> r;
+    cnt[l]++;
+    cnt[r + 1]--;
+  }
 }
 
 /*
@@ -29,6 +38,29 @@ sort(t,t+n);
 for(i=0;i<n;i++)f+=a[i]*t[i];
 cout<<f<<endl;
 }
+
+/////////////////////////////////////////////////////
+
+#include <bits/stdc++.h>
+using namespace std;
+long long a[200005],n,q, cnt[200005],l, r;
+long long d;
+int main(){
+  cin>>n>>q;
+  for (int i=1;i<=n;i++)cin>>a[i];
+  for (int i=1;i<=q;i++){
+  cin>>l>>r;
+  cnt[l]++;
+  cnt[r+1]--;
+  }
+  for(int i=1;i<=n;i++)cnt[i]=cnt[i-1]+cnt[i];
+  sort(cnt+1,cnt+n+1);
+  sort(a+1,a+n+1);
+  for(int i=1;i<=n;i++)d+=(cnt[i]*a[i]);
+  cout<<d;
+  return 0;
+}
+
 
 
 
