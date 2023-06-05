430df7edf0917697a9ab5f28c62cbe9208f3dd34
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-06 00:39:18 +0530

    1

---
+++ level-2/week-5/Qestions/A. Greg and Array.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main( ) {
+
+
+  int n, m,k;cin >> n >> m>>k;
+  ll arr[n];
+  for (int i = 0; i < n; i++) {
+    cin>>arr[i];
+  }
+  
+
+
+  
+
+
+}
+
---
+++ level-2/week-5/Qestions/D2. Encrypting Messages.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main( ) {
+  int n, m,c;cin >> n >> m>>c;
+  ll arr[n],b[m];
+
+  for (int i = 0; i < n; i++) {
+    cin>>arr[i];
+  }
+
+  for (ll i = 0; i < m; i++) {
+    cin>>b[i];
+  }
+  
+
+
+  
+
+
+}
+
