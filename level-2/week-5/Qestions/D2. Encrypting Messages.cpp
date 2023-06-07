92f8cd35210aab09a40aaab52f758168e23c225f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-07 20:20:16 +0530

    1

--- level-2/week-5/Qestions/D2. Encrypting Messages.cpp
+++ level-2/week-5/Qestions/D2. Encrypting Messages.cpp
@@ -2,10 +2,12 @@
 #define ll long long
 using namespace std;
 
-
+const mod=1000;
 int main( ) {
   int n, m,c;cin >> n >> m>>c;
   ll arr[n],b[m];
+
+  vector<ll> t(n,0);
 
   for (int i = 0; i < n; i++) {
     cin>>arr[i];
@@ -13,12 +15,30 @@ int main( ) {
 
   for (ll i = 0; i < m; i++) {
     cin>>b[i];
+
+    ll l=1;
+    ll r=n-m+i;
+    t[l]+=b[i];
+    if(r+1<n){
+      t[r+1]-=b[i];
+    }
   }
+
+  ll sum=0;
+
+  for (int i = 0; i < n; i++)
+  {
+    /* code */
+    sum+=b[i];
+    t[i]=sum;
+    ll ans=((sum%mod)+(a[i]%mod))%mod;
+
+    cout<<ans<<" ";
+
+  }
+
+  cout<<endl;
   
-
-
   
-
-
 }
 
