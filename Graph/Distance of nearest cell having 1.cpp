74a7199e2726e9769bd93369f8148bed4797f3f2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-09 02:05:44 +0530

    1

--- Graph/Distance of nearest cell having 1.cpp
+++ Graph/Distance of nearest cell having 1.cpp
@@ -1,38 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution 
+class Solution
 {
-    public:
-	vector<vector<int>> nearest(vector<vector<int>>grid)
-	{
-	    
-	}
+public:
+  vector<vector<int>> nearest(vector<vector<int>> grid)
+  {
+    int n = grid.size();
+    int m = grid[0].size();
+
+    vector<vector<int>> vis(n, vector<int>(m, 0));
+    vector<vector<int>> dist(n, vector<int>(m, 0));
+    queue<pair<pair<int, int>, int>> q;
+  }
 };
 
-
-
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<int>>grid(n, vector<int>(m, -1));
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j++){
-				cin >> grid[i][j];
-			}
-		}
-		Solution obj;
-		vector<vector<int>> ans = obj.nearest(grid);
-		for(auto i: ans){
-			for(auto j: i){
-				cout << j << " ";
-			}
-			cout << "\n";
-		}
-	}
-	return 0;
+int main()
+{
+  int tc;
+  cin >> tc;
+  while (tc--)
+  {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> grid(n, vector<int>(m, -1));
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        cin >> grid[i][j];
+      }
+    }
+    Solution obj;
+    vector<vector<int>> ans = obj.nearest(grid);
+    for (auto i : ans)
+    {
+      for (auto j : i)
+      {
+        cout << j << " ";
+      }
+      cout << "\n";
+    }
+  }
+  return 0;
 }
