7c6f8d4b3561f93daaa535aa470525071a2d5f66
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-09 02:12:31 +0530

    1

--- Graph/Distance of nearest cell having 1.cpp
+++ Graph/Distance of nearest cell having 1.cpp
@@ -18,14 +18,40 @@ public:
       for (int j = 0; j < m; j++)
       {
         /* code */
-        if (grid[i][j] == 0)
+        if (grid[i][j] == 1)
         {
           q.push({{i, j}, 0});
           vis[i][j] = 1;
         }
         else
         {
-          v[i][j] = 0;
+          vis[i][j] = 0;
+        }
+      }
+    }
+
+    int delrow[] = {-1, 0, +1, 0};
+    int delcol[] = {0, +1, 0, -1};
+
+    while (!empty())
+    {
+      int row = q.front().first.first;
+      int col = q.front().first.second;
+      int steps = q.front().second;
+
+      q.pop();
+
+      dist[row][col] = steps;
+
+      for (int i = 0; i < 4; i++)
+      {
+        int nrow = row + delrow[i];
+        int ncol = col + delcol[i];
+        // check for valid unvisited cell
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0)
+        {
+          vis[nrow][ncol] = 1;
+          q.push({{nrow, ncol}, steps + 1});
         }
       }
     }
