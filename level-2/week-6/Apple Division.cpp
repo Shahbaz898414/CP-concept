d813285474597d5d25d200c8597b558116924f8b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-12 02:42:55 +0530

    1

---
+++ level-2/week-6/Apple Division.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int mod=1e9+1;
+
+int n;
+
+int a[23];
+
+
+int main() {
+  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
