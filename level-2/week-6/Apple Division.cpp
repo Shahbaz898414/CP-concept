1e6e1a68b51febd01b1aa3bdd70a8bf37ca03484
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-12 02:48:44 +0530

    1

--- level-2/week-6/Apple Division.cpp
+++ level-2/week-6/Apple Division.cpp
@@ -1,17 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define int long long
 
-const int mod=1e9+1;
+const int mod = 1e9 + 1;
 
 int n;
 
 int a[23];
 
+int sum = 0;
 
-int main() {
+int solve(int i = 0, int s = 0)
+{
+  if (i == n)
+  {
+    return;
+  }
+}
+
+int main()
+{
   ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+
+    cin >> a[i];
+
+    sum += a[i];
+  }
+
+  cout << solve() << endl;
 }
