f4457d7b0dc98ad7b4907ecc00b8c6d137b39607
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-16 01:37:36 +0530

    1

--- level-2/week-6/D. Flowers.cpp
+++ level-2/week-6/D. Flowers.cpp
@@ -3,43 +3,56 @@ using namespace std;
 
 #define int long long
 
-const int mod = 1e9 + 1;
+const int mod = 1e9 + 7;
 
-int n;
+int tc,k;
 
-int a[23];
+int dp[100013];
 
-int sum = 0;
+int solve(int n){
 
-int solve(int i = 0, int s = 0)
-{
-  if (i == n)
-  {
-    int other = sum - s;
 
-    return abs(s - other);
-  }
 
-  return min(
-    solve(i + 1, a[i] + s),
-    solve(i + 1, s)
-    );
+  if(n==0) return 1;
+  if(n<0)  return 0;
+  // if(n<k) return 1;
 
-    
+  if(dp[n]!=-1)  return dp[n];
+
+  return dp[n]=(solve(n-k)+solve(n-1))%mod;
 }
 
-signed main()
-{
+
+signed main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin >> n;
+  memset(dp,-1,sizeof dp);
 
-  for (int i = 0; i < n; i++) {
-    cin >> a[i];
-    sum += a[i];
+  cin>>tc>>k;
+
+  int pre[100013];
+  pre[0]=solve(0);
+  for (int i = 1; i < 100013; i++)
+  {
+    /* code */
+    pre[i]=(pre[i-1]+solve(i))%mod;
   }
+  
 
-  cout << solve() << endl;
+
+  while(tc--){
+    int a,b;cin>>a>>b;
+
+    // int sum=0;
+    cout<<((pre[b]-pre[a-1])%mod+mod)%mod<<endl;
+    // for (int i = a; i <=b; i++) {
+    //   sum=(solve(i)+sum)%mod;
+    // }
+
+    // cout<<sum<<endl;
+    
+  }
+  
 }
---
+++ level-2/week-6/zz1.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+const int mod = 1e9 + 7;
+
+int tc,k;
+
+int dp[100013];
+
+int solve(int n){
+
+
+
+  if(n==0) return 1;
+  if(n<0)  return 0;
+  // if(n<k) return 1;
+
+  if(dp[n]!=-1)  return dp[n];
+
+  return dp[n]=(solve(n-k)+solve(n-1))%mod;
+}
+
+
+signed main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  memset(dp,-1,sizeof dp);
+
+  int t;cin>>t;
+
+  while(t--) {
+    int n;cin>>n;
+
+
+
+    if(n%2 or n==2){
+      cout<<"Bob"<<endl;
+    }else cout<<"Alice"<<endl;
+  }
+
+  // cin>>tc>>k;
+
+  // int pre[100013];
+  // pre[0]=solve(0);
+  // for (int i = 1; i < 100013; i++)
+  // {
+  //   /* code */
+  //   pre[i]=(pre[i-1]+solve(i))%mod;
+  // }
+  
+
+
+  // while(tc--){
+  //   int a,b;cin>>a>>b;
+
+  //   // int sum=0;
+  //   cout<<((pre[b]-pre[a-1])%mod+mod)%mod<<endl;
+  //   // for (int i = a; i <=b; i++) {
+  //   //   sum=(solve(i)+sum)%mod;
+  //   // }
+
+  //   // cout<<sum<<endl;
+    
+  // }
+  
+}
