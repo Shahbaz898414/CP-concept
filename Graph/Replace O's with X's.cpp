7101325c4ff5fc22d9a15ad937c1898847f3620d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-23 02:57:49 +0530

    1

--- Graph/Replace O's with X's.cpp
+++ Graph/Replace O's with X's.cpp
@@ -3,10 +3,74 @@ using namespace std;
 
 
 class Solution{
-public:
-    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+
+
+  void dfs(int row,int col,vector<vector<int>> &vis,vector<vector<char>> &mat,int delrow[],int delcol[]){
+    vis[row][col]=1;
+
+    int n=mat.size();
+    int m=mat[0].size();
+    for (int i = 0; i < 4; i++)
     {
-        // code here
+      /* code */
+      int nrow=row+delrow[i];
+      int ncol=col+delcol[i];
+
+      if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and !vis[nrow][ncol] and mat[nrow][ncol]=='O'){
+        dfs(nrow,ncol,vis,mat,delrow,delcol);
+      }
+
+    }
+    
+  }
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat) {
+
+        int delrow[]={-1,0,+1,0};
+        int delcol[]={0,1,0,-1};
+
+        vector<vector<int>> vis(n,vector<int>(m,0));
+
+        for (int j = 0; j < m; j++) {
+          /* first row */
+          if(!vis[0][j] and mat[0][j]=='O'){
+            dfs(0,j,vis,mat,delrow,delcol);
+          }
+
+          //last row 
+          if(!vis[n-1][j]  and mat[n-1][j]=='O'){
+            dfs(n-1,j,vis,mat,delrow,delcol);
+          }
+        }
+        
+
+        for (int i = 0; i <n; i++)
+        {
+          /* first column */
+          if(!vis[i][0] and mat[i][0]=='O'){
+            dfs(i,0,vis,mat,delrow,delcol);
+          }
+
+          if(!vis[i][m-1] and mat[i][m-1]=='O'){
+            dfs(i,m-1,vis,mat,delrow,delcol);
+          }
+        }
+
+
+        for (int i = 0; i < n; i++)
+        {
+          /* code */
+          for (int j = 0; j < m; j++)
+          {
+            /* code */
+            if(!vis[i][j] and mat[i][j]=='O'){
+              mat[i][j]='X';
+            }
+          }
+        }
+        
+
+      return mat;
     }
 };
 
