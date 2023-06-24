a1dc2697edbfff4e1a2f7c8a159529e429732009
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-25 02:58:58 +0530

    1

---
+++ Graph/Find the number of islands.cpp
@@ -0,0 +1,38 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+class Solution {
+  public:
+    
+    int numIslands(vector<vector<char>>& grid) {
+       
+    }
+};
+
+
+
+
+
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
