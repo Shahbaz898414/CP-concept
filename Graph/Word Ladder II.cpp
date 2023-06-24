cc5d15e76e7bb9ffda92c42ae0d1cc0488e8532f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-25 01:16:33 +0530

    1

--- Graph/Word Ladder II.cpp
+++ Graph/Word Ladder II.cpp
@@ -6,7 +6,46 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
-        
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        queue<vector<string>> q;
+
+        q.push({beginWord});
+
+        vector<string> usedOnLevel;
+        usedOnLevel.push_back(beginWord);
+        int level = 0;     
+        vector<vector<string>> ans;
+
+        while(!q.empty()){
+          vector<string> vec=q.front();
+          q.pop();
+
+          if(vec.size()>level){
+            level++;
+            for(auto it:usedOnLevel){
+              st.erase(it);
+            }
+          }
+
+          string word=vec.back();
+
+           if (word == endWord) {
+                
+              if (ans.size() == 0)
+                ans.push_back(vec);
+              else if (ans[0].size() == vec.size())
+                ans.push_back(vec);
+                
+            }
+
+
+
+
+
+        }
+
+
+
     }
 };
 
