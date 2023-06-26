baaa8ec429db221be4566b7d4c0e6a9e661f766f
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-27 00:48:51 +0530

    1

---
+++ level-2/Week -9/B. Lecture Sleep.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+
+  ll n,m;
+  cin>>n>>m;
+
+  ll a[n],b[n],total=0,ans;
+
+  for (int i = 0; i < n; i++) {
+    cin>>a[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    cin>>b[i];
+    if(b[i]) total+=a[i];
+  }
+
+  ll sum=0;
+
+  for (int i = 0; i <m; i++) {
+    if(!b[i]) sum+=a[i];
+  }
+  ans=sum;
+
+  for (int i = m; i < n; i++)
+  {
+    /* code */
+    if(!b[i]) sum+=a[i];
+    if(!b[i-m]) sum-=a[i-m];
+    ans=max(ans,sum);
+  }
+
+
+  cout<<ans+total<<endl;
+  
+  return 0;
+}
