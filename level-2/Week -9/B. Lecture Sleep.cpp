6b481a59fec6c766a0088dfc52b7c8b96cf2565b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-06-30 21:54:01 +0530

    1

--- level-2/Week -9/B. Lecture Sleep.cpp
+++ level-2/Week -9/B. Lecture Sleep.cpp
@@ -1,44 +1,50 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long int
 using namespace std;
 
-int main() {
+int main()
+{
 
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
+  ll n, m;
+  cin >> n >> m;
 
-  ll n,m;
-  cin>>n>>m;
+  ll a[n], b[n], total = 0, ans;
 
-  ll a[n],b[n],total=0,ans;
-
-  for (int i = 0; i < n; i++) {
-    cin>>a[i];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
   }
 
-  for (int i = 0; i < n; i++) {
-    cin>>b[i];
-    if(b[i]) total+=a[i];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> b[i];
+    if (b[i])
+      total += a[i];
   }
 
-  ll sum=0;
+  ll sum = 0;
 
-  for (int i = 0; i <m; i++) {
-    if(!b[i]) sum+=a[i];
+  for (int i = 0; i < m; i++)
+  {
+    if (!b[i])
+      sum += a[i];
   }
-  ans=sum;
+  ans = sum;
 
   for (int i = m; i < n; i++)
   {
-    /* code */
-    if(!b[i]) sum+=a[i];
-    if(!b[i-m]) sum-=a[i-m];
-    ans=max(ans,sum);
+    
+    if (!b[i])
+      sum += a[i];
+    if (!b[i - m])
+      sum -= a[i - m];
+    ans = max(ans, sum);
   }
 
+  cout << ans + total << endl;
 
-  cout<<ans+total<<endl;
-  
   return 0;
 }
