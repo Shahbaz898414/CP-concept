915c0c4f8e7d1f5ba9b1906411ad27bf69ae4533
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-01 02:01:57 +0530

    1

--- Graph/Topological sort.cpp
+++ Graph/Topological sort.cpp
@@ -8,10 +8,33 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to return list containing vertices in Topological order. 
+
+  void dfs(int node,int vis[],stack<int> st,vector<int> adj[]){
+      vis[node]=1;
+
+      for(auto it:adj[]){
+        
+      }
+  }
+	
+
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
+
+      int vis[V]={0};
+
+      stack<int> st;
+
+      for (int i = 0; i < V; i++)
+      {
+        
+
+        if(!vis[i]){
+          dfs(i,vis,st,adj);
+        }
+      }
+      
 	}
 };
 
