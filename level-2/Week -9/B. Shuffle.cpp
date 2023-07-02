5b9ba1b7b806611b451b7b2eb38dd16cc060a676
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-03 01:49:20 +0530

    1

---
+++ level-2/Week -9/B. Shuffle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    ll n,x,m;cin>>n>>x>>m;
+
+
+    for (int i = 0; i < m; i++) {
+      
+    }
+    
+  }
+}
