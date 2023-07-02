7648930885cfa3f0b28d1fc4c798435c5b1cfa01
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-03 02:33:19 +0530

    1

--- level-2/Week -9/B. Shuffle.cpp
+++ level-2/Week -9/B. Shuffle.cpp
@@ -7,17 +7,17 @@ int main() {
   int t;
   cin >> t;
   while (t--) {
-    ll n,x,m;cin>>n>>x>>m;
-    ll a=x,b=x;
+    ll n,x,m;cin>>n>>x>>m; ll a=x,b=x;
 
     for (int i = 1; i <=m; i++) {
       ll c,d;cin>>c>>d;
+
       if(b>=c and a<=d){
         b=max(b,d);
         a=min(a,c);
       }
 
-      
+
     }
 
 
