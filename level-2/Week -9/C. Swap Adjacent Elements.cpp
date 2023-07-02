c88e4d89f8dd3c80ca318fb4050cff270c263a14
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-03 00:12:58 +0530

    1

--- level-2/Week -9/C. Swap Adjacent Elements.cpp
+++ level-2/Week -9/C. Swap Adjacent Elements.cpp
@@ -17,19 +17,54 @@ int main()
   ll n, m;
   cin >> n;
 
-  ll a[n], b[n], total = 0, ans;
+  ll total = 0, ans;
+
+  bool fl=false;
+
+  vector<ll> a(n),pos(n+1),pre(n);
 
   for (int i = 0; i < n; i++)
-  {
     cin >> a[i];
-  }
-
-  ll sum = 0;
 
   string s;
   cin >> s;
 
+  // ll sum = 0;
   ll len = s.size();
+  
+  for (int i = 0; i < n; i++) {
+    pos[a[i]]=i;
+  }
+  
+  pre[0]=s[0]-'0';
+
+  for (ll i = 1; i <n-1; i++)
+  {
+    /* code */
+    pre[i]+=pre[i-1]+(s[i]-'0');
+  }
+
+  pre[n-1]=pre[n-2];
+
+  for (int i = 1; i < n; i++)
+  {
+    /* code */
+    if(pos[i]>pos[i+1]){
+      ll l=pos[i+1],r=pos[i];
+      ll sum=pre[r-1]-(l-1>=0?pre[l-1]:0);
+      if(sum<(r-l)){
+        fl=true;
+        break;
+      }
+    }
+  }
+
+  if(fl) cout<<"NO"<<endl;
+  else cout<<"YES"<<endl;
+  
+  
+
+  
 
   return 0;
 }
