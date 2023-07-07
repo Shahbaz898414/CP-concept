2db359bf1d06b63dd6d29ae2db901ab3ec91ce80
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-07 19:56:46 +0530

    1

--- level-2/Week-10/C. Sagheer and Nubian Market.cpp
+++ level-2/Week-10/C. Sagheer and Nubian Market.cpp
@@ -3,64 +3,37 @@ using namespace std;
 #define ll long long
 
 
-const int N=1e6+10;
-
-int a[N],b[N];
-ll n,s;
-
-
-ll check(ll x) {
-  for (int i =1; i <=n; i++)
-  {
-    /* code */
-    b[i]=a[i]+x*i;
-
-  }
-
-  sort(b+1,b+1+n);
-  ll sum=0;
-
-  for (int i=1; i <=x; i++)
-  {
-    /* code */
-    sum+=b[i];
-  }
-  
-  if(sum<=s)  return sum;
-
-  else return -1;
-}
-
-int32_t main() {
- 
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  
-
-  cin>>n>>s;
-
-  for (int i =1; i <=n; i++)
-  {
-    /* code */
-    scanf("%d",a+i);
-  }
-
-  int l=0,r=n;
-
-  while(l<r) {
-    int mid=(l+r+1)/2;
-    if(check(mid)>=0){
-      l=mid;
-    }else{
-      r=mid-1;
+int n;
+long long S;
+const int N = 1e5 + 5;
+int a[N];
+long long b[N];
+long long check(long long x){
+    for(int i = 1;i <= n;i++){
+        b[i] = a[i] + x * i;
     }
-  }
-  
-
-  cout<<l<<" "<<check(l)<<endl;
-
-
-  return 0;
+    sort(b + 1,b + 1 + n);
+    long long sum = 0;
+    for(int i = 1;i <= x;i++){
+        sum += b[i];
+    }
+    if(sum <= S) return sum;
+    else return -1;
 }
-
-
+ 
+int main(int argc, const char * argv[]) {
+    while(cin >> n >> S){
+        for(int i = 1;i <= n;i++){
+            scanf("%d",a + i);
+        }
+        int l = 0,r = n;
+        while(l < r){
+            int mid = (l + r + 1) / 2;
+            if(check(mid) >= 0){
+                l = mid;
+            }else r = mid - 1;
+        }
+        cout << l << " " << check(l) << endl;
+    }
+    return 0;
+}
