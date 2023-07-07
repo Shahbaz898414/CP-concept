17a8b6117a59c9887e772f4585ee3c0a0a4ed8c3
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-07 23:35:49 +0530

    1

--- level-2/Week-10/C. Sagheer and Nubian Market.cpp
+++ level-2/Week-10/C. Sagheer and Nubian Market.cpp
@@ -2,38 +2,49 @@
 using namespace std;
 #define ll long long
 
-
 int n;
 long long S;
 const int N = 1e5 + 5;
 int a[N];
 long long b[N];
-long long check(long long x){
-    for(int i = 1;i <= n;i++){
-        b[i] = a[i] + x * i;
-    }
-    sort(b + 1,b + 1 + n);
-    long long sum = 0;
-    for(int i = 1;i <= x;i++){
-        sum += b[i];
-    }
-    if(sum <= S) return sum;
-    else return -1;
+long long check(long long x)
+{
+  for (int i = 1; i <= n; i++)
+  {
+    b[i] = a[i] + x * i;
+  }
+  sort(b + 1, b + 1 + n);
+  long long sum = 0;
+  for (int i = 1; i <= x; i++)
+  {
+    sum += b[i];
+  }
+  if (sum <= S)
+    return sum;
+  else
+    return -1;
 }
- 
-int main(int argc, const char * argv[]) {
-    while(cin >> n >> S){
-        for(int i = 1;i <= n;i++){
-            scanf("%d",a + i);
-        }
-        int l = 0,r = n;
-        while(l < r){
-            int mid = (l + r + 1) / 2;
-            if(check(mid) >= 0){
-                l = mid;
-            }else r = mid - 1;
-        }
-        cout << l << " " << check(l) << endl;
+
+int main(int argc, const char *argv[])
+{
+  while (cin >> n >> S)
+  {
+    for (int i = 1; i <= n; i++)
+    {
+      scanf("%d", a + i);
     }
-    return 0;
+    int l = 0, r = n;
+    while (l < r)
+    {
+      int mid = (l + r + 1) / 2;
+      if (check(mid) >= 0)
+      {
+        l = mid;
+      }
+      else
+        r = mid - 1;
+    }
+    cout << l << " " << check(l) << endl;
+  }
+  return 0;
 }
