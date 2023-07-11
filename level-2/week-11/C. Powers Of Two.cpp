587432610223ef9d88cb3d3d58943e3e619c03d4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-11 16:10:51 +0530

    1

--- level-2/week-11/C. Powers Of Two.cpp
+++ level-2/week-11/C. Powers Of Two.cpp
@@ -280,8 +280,6 @@ bool compare(const pair<int, int> &a, const pair<int, int> &b)
     return (a.second > b.second);
 }
 
-
-
 int32_t main()
 {
 
@@ -292,28 +290,26 @@ int32_t main()
   // while (t--)
   // {
 
-   int n,k;
-	cin>>n>>k;
-	int a[k+1],sum=k;
-	fill(a,a+k,1);
-	for(int i=k-1;i>=0;i--)
-	{
-		while(sum+a[i]<=n)
-		{
-			sum+=a[i];
-			a[i]*=2;
-		}
-	}
-	if(sum!=n)
-	cout<<"NO\n";
-	else {
-		cout<<"YES\n";
-		for(int i=0;i<k;i++)
-		cout<<a[i]<<" ";
-	}
-  
-
-
+  int n, k;
+  cin >> n >> k;
+  int a[k + 1], sum = k;
+  fill(a, a + k, 1);
+  for (int i = k - 1; i >= 0; i--)
+  {
+    while (sum + a[i] <= n)
+    {
+      sum += a[i];
+      a[i] *= 2;
+    }
+  }
+  if (sum != n)
+    cout << "NO\n";
+  else
+  {
+    cout << "YES\n";
+    for (int i = 0; i < k; i++)
+      cout << a[i] << " ";
+  }
 
   // }
 
