e1a813bc8e91e0b0162bfb1a47fe9c0c43126846
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-11 21:16:14 +0530

    1

--- level-3/Week-2/Question/E. Easy Assembly.cpp
+++ level-3/Week-2/Question/E. Easy Assembly.cpp
@@ -8,30 +8,10 @@ int main()
     int t;cin>>t;
     
     while(t--) {
-        int n;cin>>n;
-        
-        
-        int a[n];
-       long long int alt=0;
-        
-        for(int i=0;i<n;i++) {
-            cin>>a[i];
-        }
-        for(int i=0;i<n;i++) { 
-            if(i%2==0) {
-                alt-=a[i];
-            }else {
-                alt+=a[i];
-            }
-                
-        }
-        
-        if(n%2==1 || alt>=0) {
-            cout<<"YES"<<endl;
-        }else {
-            cout<<"NO"<<endl;
-        }
+       
     }
 
     return 0;
 }
+
+// Bachelor's/Master's degree
