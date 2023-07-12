8b96a611e2e7ee241f31f693b1c385cc290afdda
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-12 05:30:23 +0530

    1

--- level-3/Week-2/Question/E. Easy Assembly.cpp
+++ level-3/Week-2/Question/E. Easy Assembly.cpp
@@ -1,17 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+
 int main()
 {
 
     
-    int t;cin>>t;
+    // int t;cin>>t;
     
-    while(t--) {
+    // while(t--) {
        
+    // }
+
+    ll n;cin>>n;
+
+    vector<ll>  arr[n];
+
+    vector<ll>  v;
+
+    for (ll i = 0; i < n; i++) {
+        ll x;cin>>x;
+        for (int j = 0; j < x; j++) {
+            ll y;cin>>y;
+            arr[i].push_back(y);
+            v.push_back(y);
+        }
     }
+
+    sort(v.begin(),v.end());
+
+    int split=n;
+
+    for (int i = 0; i < n; i++)
+    {
+        /* code */
+        int j=arr[i].size();
+        for (int z = 0; z < j-1; z++)
+        {
+            
+         int k=lower_bound(v.begin(),v.end(),arr[i][z])- v.begin();
+
+         if(k==v.size()-1){
+            split++;
+         }else if(arr[i][z+1]!=v[k+1]){
+            split++;
+         }
+
+        }
+        
+    }
+        
+
+    cout<<(split-n)<<" "<<(split-1)<<endl;
 
     return 0;
 }
 
-// Bachelor's/Master's degree
+// broken_soul
---
+++ level-3/Week-2/Question/N meetings in one room.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,int>>  meet(n);
+
+        for (int i = 0; i <n; i++)
+        {
+          /* code */
+          meet[i]={end[i],start[i]};
+        }
+
+        sort(meet.begin(),meet.end());
+
+        int avail=0;
+
+        int cnt=0;
+
+        for (int i = 0; i < n; i++)
+        {
+          /* code */
+          if(avail<=meet[i].second){
+            cnt++;
+            avail=meet[i].first+1;
+          }
+        }
+
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
