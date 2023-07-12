443742d1bc25e34f856d8c49557895d392169664
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-12 18:57:34 +0530

    1

--- level-3/Week-2/Question/N. Number Reduction.cpp
+++ level-3/Week-2/Question/N. Number Reduction.cpp
@@ -12,10 +12,48 @@ int main()
       string s;cin>>s;
 
       ll k;cin>>k;
+
+      int st=0;
+
+      for (int i = 0; i <=k; i++)
+      {
+        /* code */
+        if(s[i]!='0' and s[i]<s[st]){
+          st=i;
+        }
+      }
+
+      cout<<st<<endl;
+
+      k-=st;
+
+      string ans;
+
+      ans.push_back(s[st]);
+
+      int cnt=0;
+
+      for (int i =st+1; i <s.size(); i++)
+      {
+        /* code */
+        while(k>0 and ans.size()>1 and ans.back()>s[i]){
+          ans.pop_back();
+
+          k--;
+        }
+
+        ans+=s[i];
+
+        // cout<<ans<<" "<<cnt++<<endl;
+      }
+
+      while(k--) ans.pop_back();
+      
+
+      cout<<ans<<endl;
+
     }
 
-   
     return 0;
 }
 
-// broken_soul
