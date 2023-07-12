2d0d2d5bf9595e17852d6893d621d78f7a534345
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-12 05:40:16 +0530

    1

--- level-3/Week-2/Question/E. Easy Assembly.cpp
+++ level-3/Week-2/Question/E. Easy Assembly.cpp
@@ -6,53 +6,57 @@ using namespace std;
 int main()
 {
 
-    
     // int t;cin>>t;
-    
+
     // while(t--) {
-       
+
     // }
 
-    ll n;cin>>n;
+    ll n;
+    cin >> n;
 
-    vector<ll>  arr[n];
+    vector<ll> arr[n];
 
-    vector<ll>  v;
+    vector<ll> v;
 
-    for (ll i = 0; i < n; i++) {
-        ll x;cin>>x;
-        for (int j = 0; j < x; j++) {
-            ll y;cin>>y;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        for (int j = 0; j < x; j++)
+        {
+            ll y;
+            cin >> y;
             arr[i].push_back(y);
             v.push_back(y);
         }
     }
 
-    sort(v.begin(),v.end());
+    sort(v.begin(), v.end());
 
-    int split=n;
+    int split = n;
 
     for (int i = 0; i < n; i++)
     {
-        /* code */
-        int j=arr[i].size();
-        for (int z = 0; z < j-1; z++)
+
+        int j = arr[i].size();
+        for (int z = 0; z < j - 1; z++)
         {
-            
-         int k=lower_bound(v.begin(),v.end(),arr[i][z])- v.begin();
 
-         if(k==v.size()-1){
-            split++;
-         }else if(arr[i][z+1]!=v[k+1]){
-            split++;
-         }
+            int k = lower_bound(v.begin(), v.end(), arr[i][z]) - v.begin();
 
+            if (k == v.size() - 1)
+            {
+                split++;
+            }
+            else if (arr[i][z + 1] != v[k + 1])
+            {
+                split++;
+            }
         }
-        
     }
-        
 
-    cout<<(split-n)<<" "<<(split-1)<<endl;
+    cout << (split - n) << " " << (split - 1) << endl;
 
     return 0;
 }
