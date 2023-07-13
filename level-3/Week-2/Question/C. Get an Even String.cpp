6eff3ede78a71c5ab4cc5d48478df8d7a0b282bf
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-13 09:16:05 +0530

    1

---
+++ level-3/Week-2/Question/C. Get an Even String.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    string s, t;
+    cin >> s >> t;
+
+    ll n = s.length();
+
+    ll m = t.length();
+
+    set<int> posn[26];
+
+    for (int i = 0; i < n; i++)
+    {
+        
+        posn[s[i] - 'a'].insert(i);
+    }
+
+    int ans = 1, lastindex = -1;
+
+    for (int i = 0; i < m; i++)
+    {
+        
+        int ch = t[i] - 'a';
+        if(posn[ch].size()==0){
+            cout<<-1<<endl;
+            return;
+        }
+
+        auto it=posn[ch].upper_bound(lastindex);
+        
+        if(it==posn[ch].end())
+
+        ans++,lastindex=*posn[ch].begin();
+
+        else lastindex=*it;
+    }
+
+    cout<<ans<<endl;
+}
+
+int main()
+{
+
+    int t;cin>>t;
+    while(t--) {
+        solve();
+    }
+
+  
+
+    return 0;
+}
+
+
