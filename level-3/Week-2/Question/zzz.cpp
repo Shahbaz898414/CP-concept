b5408e0ea3ed9c3b445fb35cbf52dc7bcbc5251d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-16 01:43:53 +0530

    1

--- level-3/Week-2/Question/zzz.cpp
+++ level-3/Week-2/Question/zzz.cpp
@@ -3,29 +3,43 @@ using namespace std;
 #define ll long long
 #define lld long long int
 
-void solve() {
-    ll n;cin>>n;
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
 
     vector<ll> arr(n);
+    ll g = 0;
 
-    for (ll i = 0; i <n; i++) {
-        cin>>arr[i];
+    ll rem = 0, store = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    
-  
+
+    for (ll i = 0; i < n; i++)
+    {
+        if (arr[i] < k)
+        {
+            g += arr[i];
+        }
+        else
+        {
+            store = arr[i];
+            rem = (arr[i] % k);
+        }
+    }
 }
 
 int main()
 {
 
-    int t;cin>>t;
-    while(t--) {
+    int t;
+    cin >> t;
+    while (t--)
+    {
         solve();
     }
 
-  
-
     return 0;
 }
-
-
