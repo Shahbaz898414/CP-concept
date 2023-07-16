44a687eb0f57ee8d8115ffa456473ed9f77dcdce
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-16 19:21:40 +0530

    1

--- level-3/Week-2/Question/A. Powered Addition.cpp
+++ level-3/Week-2/Question/A. Powered Addition.cpp
@@ -3,25 +3,25 @@ using namespace std;
 #define ll long long
 #define lld long long int
 
+int main()
+{
 
-
-int main() {
-
-    int t;
-    cin>>t;
-    while(t--) {
-      
-    
-    
-
-
-      
-
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    ll n, x, y, m = 0;
+    cin >> n;
+    cin >> x;
+    for (ll i = 1; i < n; i++)
+    {
+      cin >> y;
+      if (y < x)
+        m = max(m, x - y);
+      x = max(x, y);
     }
+    cout << ceil(log2(m + 1)) << endl;
+  }
 
-  
-
-    return 0;
+  return 0;
 }
-
-
