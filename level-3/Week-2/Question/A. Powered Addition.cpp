592ccae98dd93809ce606feb37e25c2388058760
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-16 15:18:07 +0530

    1

---
+++ level-3/Week-2/Question/A. Powered Addition.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long int
+
+
+
+int main() {
+
+    int t;
+    cin>>t;
+    while(t--) {
+      
+    
+    
+
+
+      
+
+    }
+
+  
+
+    return 0;
+}
+
+
