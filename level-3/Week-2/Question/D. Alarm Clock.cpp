df2780cc7e76b4fd8cdba9050a77f51ce7bddd07
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-17 05:29:49 +0530

    1

--- level-3/Week-2/Question/D. Alarm Clock.cpp
+++ level-3/Week-2/Question/D. Alarm Clock.cpp
@@ -3,47 +3,43 @@ using namespace std;
 #define ll long long
 #define lld long long int
 
+int main()
+{
 
-int main() {
- 
-  ll n,m,k;cin>>n>>m>>k;
+  ll n, m, k;
+  cin >> n >> m >> k;
   vector<ll> arr(n);
 
-  for (ll i = 0; i <n; i++) {
-    cin>>arr[i];
+  for (ll i = 0; i < n; i++)
+  {
+    cin >> arr[i];
   }
 
   queue<int> q;
 
-  int ans=0;
+  int ans = 0;
 
-  sort(arr.begin(),arr.end());
+  sort(arr.begin(), arr.end());
 
-  for (ll i = 0; i <n; i++)
+  for (ll i = 0; i < n; i++)
   {
-    
-    while(q.size() and arr[i]-q.front()>=m){
+
+    while (q.size() and arr[i] - q.front() >= m)
+    {
       q.pop();
     }
 
-    if(q.size()>=k-1){
+    if (q.size() >= k - 1)
+    {
       ans++;
-    }else{
+    }
+    else
+    {
       q.push(arr[i]);
     }
-
   }
 
-  cout<<ans<<endl;
+  cout << ans << endl;
 
   return 0;
 }
-
-
-
-
-
-
-
-
-
