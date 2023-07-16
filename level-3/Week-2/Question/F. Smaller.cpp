4bb7647a787f0d74d20a1dc8e494434f82a0f6a5
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-16 14:02:02 +0530

    1

--- level-3/Week-2/Question/F. Smaller.cpp
+++ level-3/Week-2/Question/F. Smaller.cpp
@@ -4,15 +4,56 @@ using namespace std;
 #define ll long long
 
 #define lld long long int
+#define fo(i, n)          for (int i = 0; i < n; i++)
 
 
-
-int main()
-{
+int main() {
 
     int t;cin>>t;
     while(t--) {
-       ll s;cin>>s;
+      ll n;cin>>n;
+      map<char,int> mp1,mp2;
+
+      char c='a';
+
+      mp1[c]++;
+      mp2[c]++;
+
+      int k1=0,k2=0;
+
+      while(n--) {
+        int n,k;cin>>n>>k;
+
+        string s;cin>>s;
+
+        if(n==1){
+          for (ll i = 0; i < s.size(); i++)
+          {
+            
+            mp1[s[i]]+=k;
+            k1+=k;
+          }
+          
+        }else {
+          for(ll i = 0; i < s.size(); i++)
+          {
+            
+            mp2[s[i]]+=k;
+            k2+=k;
+          }
+        }
+
+        if(mp2.size()>=2) cout<<"YES"<<endl;
+        else if(mp1.size()==1 and mp2.size()==1 and k2>k1){
+          cout<<"YES"<<endl;
+        } else{
+          cout<<"NO"<<endl;
+        }
+
+
+      }
+
+
     }
 
   
