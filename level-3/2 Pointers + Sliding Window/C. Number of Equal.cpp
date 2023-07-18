e75fd2231afada311e736695b4f5cff36a1feda5
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-19 02:44:37 +0530

    1

--- level-3/2 Pointers + Sliding Window/C. Number of Equal.cpp
+++ level-3/2 Pointers + Sliding Window/C. Number of Equal.cpp
@@ -2,52 +2,44 @@
 using namespace std;
 #define ll long long
 
+int main()
+{
+  ll t;
+  cin >> t;
+  while (t--)
+  {
+    ll n, m;
+    cin >> n >> m;
 
-int main() {
-  ll t; cin >> t;
-  while (t--) {
-    ll n,m; cin >> n>>m; 
-
-    vector<ll>  a(n);
+    vector<ll> a(n);
 
     vector<ll> b(m);
 
     vector<ll> ans(n);
 
-    map<int,int> mp1,mp2;
+    map<int, int> mp1, mp2;
 
     for (ll i = 0; i < n; i++)
     {
-      /* code */
-      cin>>a[i];
+
+      cin >> a[i];
       mp1[a[i]]++;
     }
 
     for (ll i = 0; i < m; i++)
     {
-      /* code */
-      cin>>b[i];
+
+      cin >> b[i];
       mp2[b[i]]++;
     }
-    
-    int i=0,j=0,k=0;
 
-    while(i<n){
-      while(j<m and b[j]< a[i]){
-        j++;
+    for (auto it : mp1)
+    {
+      if (it.second > 1)
+      {
+        // if(mp1.find(mp))
       }
-
-      ans[i]=j;
-
-      i++;
     }
-
-    for(auto it:ans){
-      cout<<it<<" ";
-    }
-  
-    cout<<endl;
   }
   return 0;
 }
-
