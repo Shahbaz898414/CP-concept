28d278e2d2891014edbf51b4680d3f8fa7f0a8cd
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-07-18 14:24:16 +0530

    1

--- level-3/Week-3/Question/C. Hidden Permutations.cpp
+++ level-3/Week-3/Question/C. Hidden Permutations.cpp
@@ -1,18 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
+
+ll query(ll i)
+{
+  cout << "? " << i + 1 << endl;
+  ll x;
+  cin >> x;
+  return x - 1;
+}
 
 int main()
 {
-
   ll t;
   cin >> t;
+
   while (t--)
   {
     ll n;
     cin >> n;
-  }
+    ll p[n];
+    memset(p, -1, sizeof p);
 
+    for (ll i = 0; i < n; i++)
+    {
+      if (p[i] == -1)
+      {
+        ll cur = query(i);
+        while (p[cur] == -1)
+        {
+          ll temp = cur;
+          cur = query(i);
+          p[temp] = cur;
+        }
+      }
+    }
+
+    cout << "! ";
+    for (ll i = 0; i < n; i++)
+      cout << p[i] + 1 << " ";
+    cout << endl;
+  }
   return 0;
 }
+
+/*
+
+
+
+
+
+*/
---
+++ level-3/Week-3/Question/C. Searching Local Minimum.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll query(ll i)
+{
+  cout << "? " << i + 1 << endl;
+  ll x;
+  cin >> x;
+  return x - 1;
+}
+
+int main()
+{
+  // ll t;
+  // cin >> t;
+
+  // while (t--)
+  // {
+    ll n;
+    cin >> n;
+    ll p[n];
+    ll l=0;
+    ll r=n+1;
+
+    ll a,b;
+
+    while(l+2<r){
+      int mid=(l+r)/2;
+      cout<<"?"<<mid<<endl;
+      cin>>a;
+      cout<<"?"<<mid+1<<endl;
+      cin>>b;
+
+      if(a>b)  l=mid;
+      else r=mid+1;
+    }
+
+    cout<<"! "<<l+1<<endl;
+
+  // }
+  return 0;
+}
+
+/*
+
+
+
+
+
+*/
