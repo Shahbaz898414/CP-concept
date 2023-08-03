fc862d16922afd74ecf0851fa20cbee306c4e2fc
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-03 15:29:22 +0530

    1

---
+++ level-3/Week-2/Question/B. Bear and Different Names.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long long int
+
+int main()
+{
+
+  ll n, m, k;
+  cin >> n >> k;
+  char s[50];
+
+  for (int i = 0; i < k - 1; i++)
+  {
+    /* code */
+    s[i] = i;
+  }
+
+  string q;
+  cin >> q;
+
+  for (int i = k - 1; i < n; i++)
+  {
+    /* code */
+    cin >> q;
+    if (q == "NO")
+    {
+      s[i] = s[i - k + 1];
+    }
+    else
+      s[i] = i;
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    /* code */
+    if (s[i] < 26)
+      cout << char(s[i] + 65) << " ";
+    else
+      cout << 'A' << char(s[i] - 26 + 97) << " ";
+  }
+
+  return 0;
+}
