e2717d6ed2c8947f5903a3966444bfc9ce922e7a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-05 12:07:39 +0530

    1

--- level-3/Week-2/Question/A. Hometask.cpp
+++ level-3/Week-2/Question/A. Hometask.cpp
@@ -7,14 +7,26 @@ int main()
 {
 
   
-  string s;cin>>s;
-  ll k;cin>>k;
-
-  while(k--){
-    string x;cin>>x;
-    cout<<x<<" ";
-  }
-  
+ string s, x;
+	int k, a, b, ans = 0;
+	cin >> s >> k;
+	for(int i = 1; i <= k; i ++)
+	{
+		cin >> x;
+		a = b = 0;
+		for(int i = 0; i < s.size(); i ++)
+		{
+			if(s[i] == x[0]) a ++;
+			else if(s[i] == x[1]) b ++;
+			else
+			{
+				ans += min(a, b);
+				a = b = 0;
+			}
+		}
+		ans += min(a, b);
+	}
+	cout << ans << endl;
 
 
   return 0;
