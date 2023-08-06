ed0c611aa1c55115618d13723747c5e7e5d3232d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-06 16:49:38 +0530

    1

--- level-3/Week-2/Question/A. Weird Game.cpp
+++ level-3/Week-2/Question/A. Weird Game.cpp
@@ -3,31 +3,45 @@ using namespace std;
 #define ll long long
 #define lld long long int
 
-
-
-
-
-int main() {
+int main()
+{
 
   ll n, k;
   cin >> n;
-  string s,t;cin>>s>>t;
+  string s, t;
+  cin >> s >> t;
 
-
+  int c = 0, ac = 0, bc = 0;
 
   // vector<ll> arr(n);
 
-  for (ll i = 0; i < n; i++)
+  for (ll i = 0; i < 2 * n; i++)
   {
-    
-    cin >> arr[i];
+
+    // cin >> arr[i];
+
+    if (s[i] == '1')
+      ac++;
+    if (t[i] == '1')
+      bc++;
+    if (s[i] == '1' && t[i] == '1')
+      c++;
   }
 
   // vector<ll> vec, cnt(k + 1);
 
-  
+  if (c % 2 == 1)
+    c = 1;
+  else if (c % 2 == 0)
+    c = 0;
+  if (bc > ac + c)
+    c++;
+  if (ac + c > bc)
+    cout << "First";
+  if (ac + c < bc)
+    cout << "Second";
+  if (ac + c == bc)
+    cout << "Draw";
 
   return 0;
 }
-
-
