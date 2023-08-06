917a9a8c459dc5dbf54b1d39856ee474c7b99bd1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-06 09:33:30 +0530

    1

--- level-3/Week-2/Question/zzz.cpp
+++ level-3/Week-2/Question/zzz.cpp
@@ -1,231 +1,58 @@
-
-
-// using namespace std;
-/*
-void solve()
-{
-}
-
-int main()
-{
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n, k;
-        cin >> n >> k;
-        long long int arr[n + 1];
-        vector<vector<long long int>> vex(k + 1);
-
-
-         // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-        vector<long long int> brr(k + 1, 0);
-
-        for (int i = 1; i <= n; i++)
-            cin >> arr[i];
-
-        for (int i = 1; i <= n; i++)
-        {
-            vex[arr[i]].push_back(i - brr[arr[i]] - 1ll);
-
-             // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-            brr[arr[i]] = i;
-        }
-
-        long long int bnv = INT_MAX;
-        for (int i = 1; i <= k; i++)
-        {
-            vex[i].push_back(n - brr[i]);
-             // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-            sort(vex[i].begin(), vex[i].end());
-             // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-            reverse(vex[i].begin(), vex[i].end());
-            long long int res = vex[i][0] / 2;
-
-             // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-            if (vex[i].size() >= 2)
-
-             // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-                res = max(res, vex[i][1]);
-
-            bnv = min(bnv, res);
-
-             // long long int res = vex[i][0] / 2;
-            // if (vex[i].size() >= 2)
-            //     res = max(res, vex[i][1]);
-        }
-
-        cout << bnv << endl;
-    }
-    return 0;
-}
-*/
-
-/*
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int minMaxPlanks(vector<int> &planks, int k)
-{
-    int n = planks.size();
-    int ans = n;
-
-    for (int i = 0; i < k; i++)
-    {
-        int left = 0, right = 0;
-        int cnt = 0;
-
-        while (right < n)
-        {
-            if (planks[right] == i)
-            {
-                cnt++;
-                ans = min(ans, n - (right - left + 1) + cnt);
-                right++;
-            }
-            else
-            {
-                if (cnt > 0)
-                {
-                    cnt--;
-                    left++;
-                }
-                else
-                {
-                    left++;
-                    right++;
-                }
-            }
-        }
-    }
-
-    return ans;
-}
-
-int main()
-{
-
-    int t;
-    cin >> t;
-
-    while (t--)
-    {
-        int n, k;
-        cin >> n >> k;
-
-        vector<int> planks(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> planks[i];
-        }
-
-        int result = minMaxPlanks(planks, k);
-        cout << result << endl;
-    }
-
-    return 0;
-}
-*/
-
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canRearrange(vector<int> &A, vector<int> &B, vector<int> &C)
-{
-    int n = A.size();
-    vector<pair<int, int>> sortedPairs;
-
-    for (int i = 0; i < n; i++)
-    {
-        sortedPairs.push_back(make_pair(max(A[i], B[i]), C[i]));
-    }
-
-    sort(sortedPairs.begin(), sortedPairs.end());
-
-    for (int i = 0; i < n; i++)
-    {
-        int maxVal = sortedPairs[i].first;
-        int targetVal = sortedPairs[i].second;
-
-        if (A[i] != maxVal && B[i] != maxVal)
-        {
-            return false;
-        }
-
-        if (A[i] == maxVal && B[i] != targetVal)
-        {
-            swap(A[i], B[i]);
-        }
-        else if (B[i] == maxVal && A[i] != targetVal)
-        {
-            swap(A[i], B[i]);
-        }
-    }
-
-    return true;
-}
+#define ll long long
+const int M = 1e9 + 7;
 
 int main()
 {
-
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(nullptr);
     int t;
     cin >> t;
-
     while (t--)
     {
-        int n;
+        ll n;
         cin >> n;
-        vector<int> A(n), C(n);
-
-        int mi = INT_MAX;
-        int mx = INT_MIN;
-        int sum = 0;
-
-        map<int, int> mp;
+        vector<ll> a(n),b(n);
         for (int i = 0; i < n; i++)
         {
-            cin >> A[i];
-            // mi=min(mi,A[i]);
-
-            mp[A[i]]++;
+            cin >> a[i];
         }
 
-        if (n == 1)
+        for (ll i = 0; i < n; i++)
         {
-            cout << "NO" << endl;
-            continue;
+            /* code */
+            cin>>b[i];
         }
+        
 
-        if (mp.size() == n)
+        ll l=0,r=0;
+
+
+        ll maxi = LLONG_MIN;
+
+        for (ll i = 0; i < n; i++)
         {
-            cout << "YES" << endl;
-            continue;
+            /* code */
+            if(a[i]!=b[i] and l==0){
+                l=i+1;
+                continue;
+            }
+
+            if(a[i]!=b[i] and r==0){
+                r=i+1;
+                break;   
+            }
+
+
         }
 
-        // if(is_sorted(A.begin(),A.end())){
-        //     cout<<0<<endl;
-        //     continue;}
+        cout<<r<<" "<<l<<endl;
 
-        //  vector<int>  B=A;
-
-        //  sort(B.begin(),B.end());
+        // cout<<((r)-(l))<<endl;
+        
+        
     }
-
     return 0;
 }
