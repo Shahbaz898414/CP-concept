ee7a4527e4ee2c770c4883257ceba7b2523943a8
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-06 19:51:47 +0530

    1

--- level-3/Week-2/Question/zzz.cpp
+++ level-3/Week-2/Question/zzz.cpp
@@ -8,20 +8,29 @@ int main()
 {
     // ios_base::sync_with_stdio(false);
     // cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--)
-    {
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
         ll n;
         cin >> n;
-        vector<ll> a(n), b(n);
+        vector<string> a(n), b(n);
         for (int i = 0; i < n; i++)
             cin >> a[i];
         
 
 
-        ll x,y;cin>>x>>y;
+      for (ll i = 0; i < n; i++) {
+        for (ll j = 0; j < n; j++) {
+            int cnt=0;
+            if(j>0 and a[i][j-1]=='o') cnt++;
+            if(j<n-1 and v[i][j+1]=='o')c++;
+         if(i>0 and v[i-1][j]=='o')c++;
+         if(i<n-1 and v[i+1][j]=='o')c++;   
+        }
+      }
+      
         
-    }
+    // }
     return 0;
 }
