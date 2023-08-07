a573d285d1cbedfaf6b940c06639e41a4183ec7b
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-07 14:24:39 +0530

    1

--- level-3/Week-2/Question/A. Feed with Candy.cpp
+++ level-3/Week-2/Question/A. Feed with Candy.cpp
@@ -1,14 +1,13 @@
-
-
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 const int M = 1e9 + 7;
 
-int main()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
+
+
+int main() {
+  // ios_base::sync_with_stdio(false);
+  // cin.tie(nullptr);
   // int t;
   // cin >> t;
   // while (t--)
@@ -17,8 +16,7 @@ int main()
   cin >> n >> x;
   ll ans = 1;ll sum=0;
 
-  while (n-- > 0)
-  {
+  while (n-- > 0) {
     ll t, h, m;
     cin >> t >> h >> m;
   }
