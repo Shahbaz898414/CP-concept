4791bc907cd65e76ba3734e1210b1ca2167cbf89
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-07 23:46:54 +0530

    1

--- level-3/Week-3/Question/Factory Machines.cpp
+++ level-3/Week-3/Question/Factory Machines.cpp
@@ -3,43 +3,11 @@ using namespace std;
 
 #define ll long long
 
-int main()
-{
+int main( ) {
 
-    int t;cin>>t;
+   ll n,k;cin>>n>>k;
 
-    while(t--) {
-      ll n;cin>>n;
-
-      ll p=0;
-      vector<ll> arr(n+1);
-
-
-      for (ll i =1; i <=n; i++)
-      {
-        /* code */
-        cin>>arr[i];
-        if(arr[i]) p=i;
-
-        arr[i]+=arr[i-1];
-      }
-
-      if(arr[n])  cout<<"NO"<<endl;
-      else{
-        bool fl=1;
-        for (ll i =1; i <p; i++)
-        {
-          /* code */
-          if(arr[i]<=0) fl=0; 
-        }
-
-
-        if(fl)  cout<<"YES"<<endl;
-        else cout<<"NO"<<endl;
-        
-      }
-      
-    }
+   
 
     return 0;
 }
