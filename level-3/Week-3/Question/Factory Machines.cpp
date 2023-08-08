977ff66baf1292ae61c04de05181ab7a148ba5ba
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-08 13:57:22 +0530

    1

--- level-3/Week-3/Question/Factory Machines.cpp
+++ level-3/Week-3/Question/Factory Machines.cpp
@@ -1,46 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
-
-
-
-int main( ) {
-
-  
-
+int main()
+{
 
    int n;
-	ll t;
-	cin >> n >> t;
-	vector<ll> k(n);
-	for (int i = 0; i < n; i++) { cin >> k[i]; }
-	ll lo = 0;
-	ll hi = 1e18;
-	ll ans = 0;
-	while (lo <= hi) {
-		ll mid = (lo + hi) / 2;
-		ll sum = 0;
-		for (int i = 0; i < n; i++) {
-			sum += (mid / k[i]);
-			if (sum >= t) {  // deal with overflow
-				break;
-			}
-		}
-		if (sum >= t) {
-			ans = mid;
-			hi = mid - 1;
-		} else {
-			lo = mid + 1;
-		}
-	}
-	cout << ans << "\n";
+   ll t;
+   cin >> n >> t;
+   vector<ll> k(n);
+   for (int i = 0; i < n; i++)
+   {
+      cin >> k[i];
+   }
+   ll lo = 0;
+   ll hi = 1e18;
+   ll ans = 0;
+   while (lo <= hi)
+   {
+      ll mid = (lo + hi) / 2;
+      ll sum = 0;
+      for (int i = 0; i < n; i++)
+      {
+         sum += (mid / k[i]);
+         if (sum >= t)
+         {
+            break;
+         }
+      }
+      if (sum >= t)
+      {
+         ans = mid;
+         hi = mid - 1;
+      }
+      else
+      {
+         lo = mid + 1;
+      }
+   }
+   cout << ans << "\n";
 
-
-
-
-
-
-    return 0;
+   return 0;
 }
-
