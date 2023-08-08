b236de4fd7cd28c04cba65e15528adbdbf6f3ebb
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-08 13:02:59 +0530

    1

--- level-3/Week-3/Question/zzz.cpp
+++ level-3/Week-3/Question/zzz.cpp
@@ -2,20 +2,36 @@
 using namespace std;
 #define ll long long
 
+
+
+bool vs(int  v,int s){
+  if(v>s){
+    return v;
+
+  }else{
+    return s;
+  }
+}
 int main( ){
 
-  // ll t;
-  // cin >> t;
-  // while (t--)
-  // {
+  ll t;
+  cin >> t;
+  while (t--)
+  {
    ll  n;cin>>n;
 
    ll s;cin>>s;
+
+   ll x=ceil((double)n/2);
+
+   n-=(x-1);
+
+   cout<<s/n<<endl;
     
 
 
 
-  // }
+  }
 
   return 0;
 }
