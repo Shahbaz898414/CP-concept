b02da7b02984e0654b6b8fe7dc9803bc0fada8a2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-08 22:34:41 +0530

    1

--- level-3/Week-3/Question/C. Set or Decrease.cpp
+++ level-3/Week-3/Question/C. Set or Decrease.cpp
@@ -37,3 +37,70 @@ int main()
 }
 
 
+
+
+/*
+
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> PII;
+typedef long long ll;
+
+const int N = 2e5 + 10;
+
+int a[N];
+ll n, k;
+ll s[N];
+
+bool check(ll x)
+{
+	ll t = 0;
+	ll sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		t = i;
+		sum = s[n - i] + i * s[1];
+		if (sum > k)
+		{
+			t += (sum - k + i) / (i + 1);
+		}
+		if (t <= x) return true;
+	}
+}
+
+
+
+int main()
+{
+	int T; cin >> T;
+	while (T--)
+	{
+		cin >> n >> k;
+		for (int i = 1; i <= n; i++) cin >> a[i];
+		sort(a, a + n + 1);
+		for (int i = 1; i <= n; i++) s[i] = s[i - 1] + a[i];
+
+		ll l = 0, r = 1e16;
+		while (l < r)
+		{
+			ll mid = l + r >> 1;
+			if (check(mid)) r = mid;
+			else l = mid + 1;
+		}
+
+		cout << l << endl;
+		
+	}
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+*/
