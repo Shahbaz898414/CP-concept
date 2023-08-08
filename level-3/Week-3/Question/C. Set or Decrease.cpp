9eb6bd66144e2fb1c2a3e0bb8c5df8558456091e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-08 14:05:36 +0530

    1

--- level-3/Week-3/Question/C. Set or Decrease.cpp
+++ level-3/Week-3/Question/C. Set or Decrease.cpp
@@ -9,11 +9,11 @@ int main( ) {
 
    ll n,t;cin>>n>>t;
    vector<ll>  arr(n);
-      // ll sum=0;
+      ll sum=0;
 
    for (ll i = 0; i < n; i++) {
       cin>>arr[i];
-      // sum+=arr[i];
+      sum+=arr[i];
    }
 
 
