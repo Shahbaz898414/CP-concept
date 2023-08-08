ff617b67f13cd0642e7f819a85815783294eb958
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-08 14:12:45 +0530

    1

--- level-3/Week-3/Question/C. Set or Decrease.cpp
+++ level-3/Week-3/Question/C. Set or Decrease.cpp
@@ -1,28 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
+int main()
+{
 
+  ll t;
+  cin >> t;
 
+  while (t--)
+  {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> arr(n);
+    ll sum = 0;
 
-int main( ) {
+    for (ll i = 0; i < n; i++)
+    {
+      cin >> arr[i];
+      sum += arr[i];
+    }
+  }
 
-   ll n,t;cin>>n>>t;
-   vector<ll>  arr(n);
-      ll sum=0;
-
-   for (ll i = 0; i < n; i++) {
-      cin>>arr[i];
-      sum+=arr[i];
-   }
-
-
-   
-
-
-
-
-
-    return 0;
+  return 0;
 }
-
