997fce375edc7dfc20e853a08c9290bc955034b7
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-08 15:56:32 +0530

    1

--- level-3/Week-3/Question/B. Ropes.cpp
+++ level-3/Week-3/Question/B. Ropes.cpp
@@ -51,66 +51,7 @@ int main() {
 
 
 
-/*
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-bool canCutPieces(const vector<int> &ropes, int k, double len)
-{
-    int count = 0;
-    for (int rope : ropes)
-    {
-        count += rope / len;
-    }
-    return count >= k;
-}
-
-int findMaxLengthOfPieces(const vector<int> &ropes, int k)
-{
-    int minLen = *min_element(ropes.begin(), ropes.end());
-    int left = 1;
-    int right = minLen;
-    double result = 0;
-
-    while (left <= right)
-    {
-        double mid = left + (right - left) / 2;
-        if (canCutPieces(ropes, k, mid))
-        {
-            result = max(result, mid);
-            left = mid + 1;
-        }
-        else
-        {
-            right = mid - 1;
-        }
-    }
-
-    return result;
-}
-
-int main()
-{
-    int n, k;
-    cin >> n >> k;
-
-    vector<int> ropes(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> ropes[i];
-    }
-
-    int maxLength = findMaxLengthOfPieces(ropes, k);
-    cout << maxLength << endl;
-
-    return 0;
-}
-
-
-*/
 
 
 
