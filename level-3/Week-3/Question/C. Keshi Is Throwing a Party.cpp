038f2123eab1f5642986a367033accf25f23840d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-09 00:05:23 +0530

    1

--- level-3/Week-3/Question/C. Keshi Is Throwing a Party.cpp
+++ level-3/Week-3/Question/C. Keshi Is Throwing a Party.cpp
@@ -1,43 +1,23 @@
-/*
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 
-int main()
-{
 
-  ll t;
-  cin >> t;
 
-  while (t--)
-  {
+
+int main( ) {
+
+  ll t; cin >> t;
+  while (t--) {
     ll n, k;
-    cin >> n >> k;
-    vector<ll> arr(n);
-    ll sum = 0;
-    ll ans=0;
+    cin >> n;
 
-    for (ll i = 0; i < n; i++)
-    {
-      cin >> arr[i];
-      sum += arr[i];
-    }
-
-    ll l=0,h=1e8;
+    
+   
 
   }
 
   return 0;
 }
-
-
-
-*/
-
-
-
 
 
