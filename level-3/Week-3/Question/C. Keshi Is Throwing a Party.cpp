3f08213e4f52e68149c02c84f6096dd2ddf79fb0
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-09 13:08:29 +0530

    1

--- level-3/Week-3/Question/C. Keshi Is Throwing a Party.cpp
+++ level-3/Week-3/Question/C. Keshi Is Throwing a Party.cpp
@@ -2,22 +2,52 @@
 using namespace std;
 #define ll long long
 
+const int mx = 1e6;
+ll a[mx];
+ll b[mx];
+ll n;
+
+bool check(int k) {
 
 
+  int r = k - 1, p = 0;
 
-int main( ) {
 
-  ll t; cin >> t;
-  while (t--) {
-    ll n, k;
+  for (int i = 1; i <= n; i++)
+    if (a[i] >= r && b[i] >= p)
+      r--, p++;
+
+
+  return p >= k;
+
+}
+
+int main()
+{
+
+  ll t;
+  cin >> t;
+  while (t--)
+  {
+
     cin >> n;
+    for(int i=1;i<=n;i++)cin>>a[i]>>b[i];
+    int l = 1;
+    int r = n;
+    int mid;
 
-    
-   
-
+    while (l <= r)
+    {
+      mid = (l + r) / 2;
+      cout<<mid<<"m ";
+      if (check(mid))
+        l = mid + 1;
+      else
+        r = mid - 1;
+    }
+    cout<<endl;
+    cout << l - 1 << endl;
   }
 
   return 0;
 }
-
-
