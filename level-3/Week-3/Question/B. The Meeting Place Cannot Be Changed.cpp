5268aa30b04bbe7c5be41f8158fb39ef94efa6c1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-10 15:31:43 +0530

    1

--- level-3/Week-3/Question/B. The Meeting Place Cannot Be Changed.cpp
+++ level-3/Week-3/Question/B. The Meeting Place Cannot Be Changed.cpp
@@ -7,49 +7,58 @@ const int mx = 1e6;
 ll a[mx];
 ll b[mx];
 
-bool isPossible(ll *p, ll *v, ll n, double t){
-    double left=-1e9,right=1e9;
-    for(ll i=0; i<n; i++){
-        left=max(left,p[i]-v[i]*t);
-        right=min(right,p[i]+v[i]*t);
-    }
-    if(left<=right) return true;
-    return false;
+bool isPossible(ll *p, ll *v, ll n, double t)
+{
+  double left = -1e9, right = 1e9;
+  for (ll i = 0; i < n; i++)
+  {
+    left = max(left, p[i] - v[i] * t);
+    right = min(right, p[i] + v[i] * t);
+  }
+  if (left <= right)
+    return true;
+  return false;
 }
 
 int main()
 {
 
-  ll n; cin>>n;
-    ll p[n]; ll v[n];
+  ll n;
+  cin >> n;
+  ll p[n];
+  ll v[n];
 
-    for(ll i=0; i<n; i++) cin>>p[i];
-    for(ll i=0; i<n; i++) cin>>v[i];
+  for (ll i = 0; i < n; i++)
+    cin >> p[i];
+  for (ll i = 0; i < n; i++)
+    cin >> v[i];
 
-    double l=0.0,r=1e9,mid;
-    while(r-l>1e-6){
-        mid=l+(r-l)/2;
-        if(isPossible(p,v,n,mid)){
-            r=mid;
-        }
-        else{
-            l=mid;
-        }
+  double l = 0.0, r = 1e9, mid;
+  while (r - l > 1e-6)
+  {
+    mid = l + (r - l) / 2;
+
+    // cout<<mid<<" ";
+    if (isPossible(p, v, n, mid))
+    {
+      r = mid;
     }
+    else
+    {
+      l = mid;
+    }
+  }
+  cout << endl;
+  cout << endl;
 
-    // if(l==r)  cout<<"YES"<<endl;
-    // else cout<<"NO"<<endl;
+  // if(l==r)  cout<<"YES"<<endl;
+  // else cout<<"NO"<<endl;
 
-
-    // printf("%.9lf ",r);
-    cout << fixed;
-    cout << setprecision(12);
-    cout<<r;
-    return 0;
+  // printf("%.9lf ",r);
+  cout << fixed;
+  cout << setprecision(12);
+  cout << r;
+  return 0;
 
   return 0;
 }
-
-
-
-
