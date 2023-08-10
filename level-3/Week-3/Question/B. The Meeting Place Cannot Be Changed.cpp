7a580b1a6c0701db5b79ec5397d053383ce23cde
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-10 13:01:14 +0530

    1

--- level-3/Week-3/Question/B. The Meeting Place Cannot Be Changed.cpp
+++ level-3/Week-3/Question/B. The Meeting Place Cannot Be Changed.cpp
@@ -7,69 +7,6 @@ const int mx = 1e6;
 ll a[mx];
 ll b[mx];
 
-int main()
-{
-
-  // ll t; cin >> t;
-  // while (t--) {
-
-  ll n;
-  cin >> n;
-  for (int i = 1; i <= n; i++)
-    cin >> a[i];
-
-  for (int i = 1; i <= n; i++)
-    cin >> b[i];
-
-  int l = 1;
-  int r = n + 3;
-  int mid;
-
-  // }
-
-  return 0;
-}
-
-/*
-
-
-#include <iostream>
-using namespace std;
-#define N 60005
-int x[N],v[N];
-int main()
-{
-    int n;
-    cin >> n;
-    for(int i=1;i<=n;i++) cin >> x[i];
-    for(int i=1;i<=n;i++) cin >> v[i];
-    double tmin=0,tmax=1e9;
-    for(int i=1;i<=100;i++)
-    {
-        double tmid=(tmin+tmax)/2;
-        double xmin=0,xmax=1e9;
-        for(int i=1;i<=n;i++)
-        {
-            xmin=max(xmin,x[i]-v[i]*tmid);
-            xmax=min(xmax,x[i]+v[i]*tmid);
-        }
-        if(xmin>=xmax) tmin=tmid;
-        else tmax=tmid;
-    }
-    printf("%.12lf\n",tmax);
-    return 0;
-}
-
-
-
-*/
-
-/*
-
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long int
-
 bool isPossible(ll *p, ll *v, ll n, double t){
     double left=-1e9,right=1e9;
     for(ll i=0; i<n; i++){
@@ -80,9 +17,10 @@ bool isPossible(ll *p, ll *v, ll n, double t){
     return false;
 }
 
-int main(){
+int main()
+{
 
-    ll n; cin>>n;
+  ll n; cin>>n;
     ll p[n]; ll v[n];
 
     for(ll i=0; i<n; i++) cin>>p[i];
@@ -103,6 +41,10 @@ int main(){
     cout << setprecision(12);
     cout<<r;
     return 0;
+
+  return 0;
 }
 
-*/
+
+
+
