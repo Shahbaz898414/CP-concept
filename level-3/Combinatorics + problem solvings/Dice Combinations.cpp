c219517bdcfff198933b91c919ec0ba1a3c809ac
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-11 18:30:43 +0530

    1

--- level-3/Combinatorics + problem solvings/Dice Combinations.cpp
+++ level-3/Combinatorics + problem solvings/Dice Combinations.cpp
@@ -20,17 +20,18 @@ int main()
     for (int i = 2; i <= n; i++)
     {
       for (int j = 1; j <= 6; j++)
-      { // j is denoting dice throw
+      { 
+
         if (j > i)
-        {
+        
           continue;
-        }
+        
 
         dp[i] = (dp[i] % mod + dp[i - j] % mod) % mod;
       }
     }
 
-    cout << dp[n] % mod;
+    cout << dp[n] % mod<<endl;
   }
 
   return 0;
