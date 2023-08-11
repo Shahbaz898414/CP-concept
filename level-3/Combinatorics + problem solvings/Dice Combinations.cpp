611984a4f3bf2e6cf0f590fe74009322234800a9
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-11 19:06:05 +0530

    1

--- level-3/Combinatorics + problem solvings/Dice Combinations.cpp
+++ level-3/Combinatorics + problem solvings/Dice Combinations.cpp
@@ -9,23 +9,19 @@ int main()
   ll t;
   cin >> t;
 
-  while (t--)
-  {
-    ll n;
-    cin >> n;
+  while (t--) {
+    ll n; cin >> n;
 
     vector<ll> dp(n + 1, 0);
     dp[0] = dp[1] = 1;
 
-    for (int i = 2; i <= n; i++)
-    {
-      cout<<i<<" i ";
-      for (int j = 1; j <= 6; j++)
-      { 
+    for (int i = 2; i <= n; i++) {
+      // cout<<i<<" i ";
+      for (int j = 1; j <= 6; j++) { 
 
         if (j > i)
           continue;
-        cout<<j<<" ";
+        // cout<<j<<" ";
        
 
         dp[i] = (dp[i] % mod + dp[i - j] % mod) % mod;
