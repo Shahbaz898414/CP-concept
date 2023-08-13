23c0c84ce304130b4009046ee785a24f4a352e66
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-14 02:34:06 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/C. Sweets Eating.cpp
+++ level-3/Dynamic Programming Introduction/Question/C. Sweets Eating.cpp
@@ -227,7 +227,7 @@ int main()
 
     ll n,x; cin >> n>>x;
     vector<ll> vec(n);
-    vector<int> page(n),price(n);
+    // vector<int> page(n),price(n);
     // ll mx = -1;
 
     // int a,b;
@@ -235,37 +235,11 @@ int main()
     for (ll i = 0; i < n; i++)
     {
       /* code */
-      cin>>price[i]>>page[i];
+      cin>>vec[i];
     }
     
 
-    // for(int&i : price)
-    // cin>>i;
-
-    // for(int&i : page)
-    // cin>>i;
-
-    vector<vector<int>> dp(n+1,vector<int>(x+1,0));
-
-     for(int i=1;i<=n;i++)
-    {
-        for(int j=0;j<=x;j++)
-        {
-            //option1 = book number i-1 is not included, hence no pages added.
-            //option2 = book number i-1 is included, hence pages are added.
-            //option1 = dp[i-1][j] ...book not included so total price(j) remains same.
-            //option2 = dp[i-1][j-price[i-1]] + page[i-1]....total price(j) decreased and pages are counted for book i-1.
-            
-            dp[i][j] = dp[i-1][j];
-            if(j >= price[i-1])
-            {
-                dp[i][j] = max (dp[i][j],dp[i-1][j-price[i-1]] + page[i-1]); //max of both the boxes included in dp[i][j]. 
-            }
-        }
-    }
-
-    cout<< dp[n][x] <<endl;
-
+    
 
 
 
--- level-3/Dynamic Programming Introduction/Question/zzz.cpp
+++ level-3/Dynamic Programming Introduction/Question/zzz.cpp
@@ -227,45 +227,25 @@ int main()
 
     ll n,x; cin >> n>>x;
     vector<ll> vec(n);
-    vector<int> page(n),price(n);
+    // vector<int> page(n),price(n);
     // ll mx = -1;
 
     // int a,b;
 
-    for (ll i = 0; i < n; i++)
-    {
-      /* code */
-      cin>>price[i]>>page[i];
+    // for (ll i = 0; i < n; i++)
+    // {
+    //   /* code */
+    //   cin>>vec[i];
+    // }
+
+    if(n==1){
+      cout<<x<<endl;
+    }else if(n==x){
+      cout<<x<<endl;
     }
     
 
-    // for(int&i : price)
-    // cin>>i;
-
-    // for(int&i : page)
-    // cin>>i;
-
-    vector<vector<int>> dp(n+1,vector<int>(x+1,0));
-
-     for(int i=1;i<=n;i++)
-    {
-        for(int j=0;j<=x;j++)
-        {
-            //option1 = book number i-1 is not included, hence no pages added.
-            //option2 = book number i-1 is included, hence pages are added.
-            //option1 = dp[i-1][j] ...book not included so total price(j) remains same.
-            //option2 = dp[i-1][j-price[i-1]] + page[i-1]....total price(j) decreased and pages are counted for book i-1.
-            
-            dp[i][j] = dp[i-1][j];
-            if(j >= price[i-1])
-            {
-                dp[i][j] = max (dp[i][j],dp[i-1][j-price[i-1]] + page[i-1]); //max of both the boxes included in dp[i][j]. 
-            }
-        }
-    }
-
-    cout<< dp[n][x] <<endl;
-
+    
 
 
 
