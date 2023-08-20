c6ecb7489be435709bf2e2b358bf6ec1884589a6
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-20 17:40:33 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/C - Vacation.cpp
+++ level-3/Dynamic Programming Introduction/Question/C - Vacation.cpp
@@ -229,7 +229,7 @@ signed main()
 
   int n, k;
   cin >> n ;
-  vector<vector<int>> point(n,vector<int>(n)); 
+  vector<vector<int>> points(n,vector<int>(n)); 
 
   for (ll i = 0; i < n; i++)
   {
@@ -237,9 +237,19 @@ signed main()
     for (ll j = 0; j < 3; j++)
     {
       /* code */
-      cin>>point[i][j];
+      cin>>points[i][j];
     }
   }
+
+  vector < int > prev(4, 0);
+
+  prev[0] = max(points[0][1], points[0][2]);
+
+  prev[1] = max(points[0][0], points[0][2]);
+
+  prev[2] = max(points[0][0], points[0][1]);
+
+  prev[3] = max(points[0][0], max(points[0][1], points[0][2]));
 
 
   // for (ll i = 0; i < n; i++)
@@ -252,10 +262,29 @@ signed main()
   //   }
   //   cout<<endl;
   // }
-  
+
+
+  for (int day = 1; day < n; day++) {
+
+    vector < int > temp(4, 0);
+    
+    for(int last = 0; last < 4; last++) {
+      temp[last] = 0;
+
+      for (int task = 0; task <= 2; task++) {
+        if (task != last) {
+
+          temp[last] = max(temp[last], points[day][task] + prev[task]);
+        }
+      }
+    }
+    prev = temp;
+
+  }
 
 
 
- 
+  cout<< prev[3];
+   
   return 0;
 }
