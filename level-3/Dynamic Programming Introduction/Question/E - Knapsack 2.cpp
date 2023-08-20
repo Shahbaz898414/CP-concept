18c66075b8039d50e6457d5d087a2a339a7478f7
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-20 22:02:39 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/E - Knapsack 2.cpp
+++ level-3/Dynamic Programming Introduction/Question/E - Knapsack 2.cpp
@@ -271,97 +271,108 @@ signed main()
 
 */
 
+#include <bits/stdc++.h>
 
-
-
-
-#include<bits/stdc++.h>
-
-
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
-#define int long long 
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define int long long
 #define pb push_back
 #define endl '\n'
 
 using namespace std;
 
+const int mod = 1e9 + 7;
 
-const int mod=1e9+7;
+int binExp(int x, int y)
+{
 
-int binExp( int x, int y){
-	
-	int res=1;
-	while(y){
-		if(y&1)
-			res=(res*x);
-		y>>=1;
-		x=(x*x);
-	}
-	return res;
+  int res = 1;
+  while (y)
+  {
+    if (y & 1)
+      res = (res * x);
+    y >>= 1;
+    x = (x * x);
+  }
+  return res;
 }
-int modAdd(int x,int y){
-	x%=mod;
-	y%=mod;
-	return (x+y)%mod;
+int modAdd(int x, int y)
+{
+  x %= mod;
+  y %= mod;
+  return (x + y) % mod;
 }
-int modSub(int x,int y){
-	x%=mod;
-	y%=mod;
-	return (x-y+mod)%mod;
+int modSub(int x, int y)
+{
+  x %= mod;
+  y %= mod;
+  return (x - y + mod) % mod;
 }
-int modMul(int x,int y){
-	x%=mod;
-	y%=mod;
-	return (x*y)%mod;
+int modMul(int x, int y)
+{
+  x %= mod;
+  y %= mod;
+  return (x * y) % mod;
 }
-int modDiv(int x,int y){
-	y=binExp(y,mod-2);
-	int z=modMul(x,y);
-	return z;
+int modDiv(int x, int y)
+{
+  y = binExp(y, mod - 2);
+  int z = modMul(x, y);
+  return z;
 }
-bool cmp(pair<int,int>&a,pair<int,int>&b){
-	
-	return a.second<b.second;
-}
-int factorial(int n){
-	int fact=1;
-	for(int i=1;i<=n;i++){
-		fact*=i;
-	}
-	return fact;
-}
-int gcd(int a, int b){
-    if (b == 0)
-        return a;
-    return gcd(b,a%b);
-}
-int helper(int i,int n,int w,vector<pair<int,int>>&v,vector<vector<int>>&dp){
-	if(i==n){
-		return 0;
-	}
-	if(dp[i][w]!=-1){
-		return dp[i][w];
-	}
-	int nt=0,t=0;
-	nt=helper(i+1,n,w,v,dp);
-	if(v[i].first<=w){
-		t=v[i].second+helper(i+1,n,w-v[i].first,v,dp);
-	}
-	return dp[i][w]=max(nt,t);
-}
-signed main(){
+bool cmp(pair<int, int> &a, pair<int, int> &b)
+{
 
-	fast;
-	int n,w;
-	cin>>n>>w;
-	vector<pair<int,int>>v;
-	for(int i=0;i<n;i++)
-	{
-		int x,y;
-		cin>>x>>y;
-		v.pb({x,y});
-	}
-	vector<vector<int>>dp(n,vector<int>(w+1,-1));
-	cout<<helper(0,n,w,v,dp);
-	return 0;
+  return a.second < b.second;
+}
+int factorial(int n)
+{
+  int fact = 1;
+  for (int i = 1; i <= n; i++)
+  {
+    fact *= i;
+  }
+  return fact;
+}
+int gcd(int a, int b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+int helper(int i, int n, int w, vector<pair<int, int>> &v, vector<vector<int>> &dp)
+{
+  if (i == n)
+  {
+    return 0;
+  }
+  if (dp[i][w] != -1)
+  {
+    return dp[i][w];
+  }
+  int nt = 0, t = 0;
+  nt = helper(i + 1, n, w, v, dp);
+  if (v[i].first <= w)
+  {
+    t = v[i].second + helper(i + 1, n, w - v[i].first, v, dp);
+  }
+  return dp[i][w] = max(nt, t);
+}
+signed main()
+{
+
+  fast;
+  int n, w;
+  cin >> n >> w;
+  vector<pair<int, int>> v;
+  for (int i = 0; i < n; i++)
+  {
+    int x, y;
+    cin >> x >> y;
+    v.pb({x, y});
+  }
+  vector<vector<int>> dp(n, vector<int>(w + 1, -1));
+  cout << helper(0, n, w, v, dp);
+  return 0;
 }
