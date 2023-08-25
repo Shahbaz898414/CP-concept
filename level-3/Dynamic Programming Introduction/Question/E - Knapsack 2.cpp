85b35c7ac58f95d3aa143227e31532dc2ebaa5fa
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-25 23:39:04 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/E - Knapsack 2.cpp
+++ level-3/Dynamic Programming Introduction/Question/E - Knapsack 2.cpp
@@ -299,9 +299,7 @@ int main()
     dp[0] = 0;
 
     for(ll i=0;i<n;i++)
-    
         for(ll j=max_val-value[i];j>=0;j--)
-        
             dp[j+value[i]] = min(dp[j+value[i]], dp[j]+weight[i]);
         
     
