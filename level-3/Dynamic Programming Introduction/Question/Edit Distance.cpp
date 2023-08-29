a72d0dc55d5253085879c9fecb4acf6b7b2d9c42
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-08-29 23:49:19 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Edit Distance.cpp
+++ level-3/Dynamic Programming Introduction/Question/Edit Distance.cpp
@@ -230,18 +230,19 @@ signed main() {
         for(int j=0;j<=len_s2;j++)
         {
             if(i == 0)
-            dp[i][j] = j; 
-            //when s1 is empty, we just have to copy j elements of string s2
+              dp[i][j] = j; 
+              //when s1 is empty, we just have to copy j elements of string s2
 
             else if(j == 0)
-            dp[i][j] = i; 
-            // when s2 is empty, we just have to remove i elements present in s1.
+              dp[i][j] = i; 
+              // when s2 is empty, we just have to remove i elements present in s1.
 
             else if(s1[i-1] == s2[j-1])
-            dp[i][j] = dp[i-1][j-1]; // if the last element is same in both s1 & s2, just copy the previous dp value. No more changes needed
+              dp[i][j] = dp[i-1][j-1]; 
+              // if the last element is same in both s1 & s2, just copy the previous dp value. No more changes needed
 
             else
-            {
+            
                 dp[i][j] = 1 + min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1]));
                 //case1 = dp[i][j-1] = when we insert an element in s1. 
                 //After insertion, we calculate the dp[for the rest of s1(i)][for the rest of s2 other than the last element which is inserted in s1(j-1)].
@@ -251,7 +252,7 @@ signed main() {
                 //case3 = dp[i-1][j-1] = when element of s1 is replaced. 
 
                 //calculate min of the three. 1 is added as each case counts to 1 operation
-            }
+            
         }
     }
 
