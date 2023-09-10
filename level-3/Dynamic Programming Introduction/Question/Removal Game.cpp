455c0843855b950480469c72e6725cd21fd60664
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-10 14:18:36 +0530

    1

---
+++ level-3/Dynamic Programming Introduction/Question/Removal Game.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll inf = 1e18;
+
+
+int main() {
+
+
+  ll n; cin >> n;
+
+  vector<double> arr(n);
+  for (int i = 0; i < n; i++) cin >> arr[i];
+  
+  
+
+
+
+
+
+
+}
+
+
