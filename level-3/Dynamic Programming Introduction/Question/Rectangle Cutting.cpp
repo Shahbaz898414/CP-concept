1eeeb1778acc09b33a50508d10ef41ca21191ce8
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-17 01:48:43 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Rectangle Cutting.cpp
+++ level-3/Dynamic Programming Introduction/Question/Rectangle Cutting.cpp
@@ -20,27 +20,42 @@ int main()
   ll t = 1;
   // cin >> t;
 
-  while (t--)
-  {
+  while (t--) {
 
     ll n, m;
     cin >> n >> m;
 
-    for (int i = 1; i <= n; i++)
-    {
-
-      for (int j = 1; j <= m; j++)
-      {
-
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
         dp[i][j] = INF;
       }
     }
+
+
+
+    //  for (int i = 0; i < n; i++)
+    // {
+    //   /* code */
+    //   for (int j = 0; j < m; j++)
+    //   {
+    //     /* code */
+    //     cout<<dp[i][j]<<" ";
+    //   }
+
+    //   cout<<endl;
+
+    // }
+    // cout<<endl;
+
+
 
     for (int i = 1; i < nax; i++)
     {
       dp[i][i] = 0;
     }
 
+
+
     // for (int i = 0; i < n; i++)
     // {
     //   /* code */
@@ -53,40 +68,42 @@ int main()
     //   cout<<endl;
 
     // }
-
     // cout<<endl;
 
-    for (int i = 1; i <= n; i++)
-    {
-      for (int j = 1; j <= m; j++)
-      {
 
-        for (int z = 1; z < i; z++)
-        {
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+
+        for (int z = 1; z < i; z++) 
           dp[i][j] = min(dp[i][j], dp[z][j] + dp[i - z][j] + 1);
-        }
 
-        for (int z = 1; z < j; z++)
-        {
+        for (int z = 1; z < j; z++) 
           dp[i][j] = min(dp[i][j], dp[i][z] + dp[i][j - z] + 1);
-        }
+        
       }
     }
 
+
     // for (int i = 0; i < n; i++)
     // {
-    //   /* code */
+      
     //   for (int j = 0; j < m; j++)
     //   {
-    //     /* code */
+        
     //     cout<<dp[i][j]<<" ";
     //   }
 
     //   cout<<endl;
 
     // }
+    // cout<<endl;
+
+
 
     cout << dp[n][m] << endl;
+
+
   }
 
   return 0;
