7661862f0400ebec7fe7e4c9c67711157e50de6e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-17 03:24:15 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Two Sets II.cpp
+++ level-3/Dynamic Programming Introduction/Question/Two Sets II.cpp
@@ -8,6 +8,19 @@ typedef long long ll;
 const ll nax = 505;
 const ll mod = (10e8) + 7;
 
+ll modInverse(ll base,ll exp){
+  if(exp==0){
+    return 1;
+  }
+
+  ll res=modInverse(base,exp/2);
+  if(exp%2==1){
+    return (((res*res)%mod)*base) % mod;
+  }else {
+    return (res*res) % mod;
+  }
+}
+
 #define INF 1e9;
 
 int dp[nax][nax];
@@ -30,8 +43,33 @@ int main()
     ll sum;
 
     sum=(n*(n+1))/2;
+    if(sum%2){
+      cout<<0<<endl;
+      return 0;
+    }
+    sum=sum/2;
 
-    sum=sum/4;
+    vector<ll>  cnt(sum+1);
+
+    cnt[0]=1;
+
+    for (int i = 1; i <=n; i++)
+    {
+      /* code */
+      for (int j = sum; j >=i; j--)
+      {
+        /* code */
+        cnt[j]=(cnt[j]+cnt[j-i])%mod;
+      }
+    }
+
+    
+
+
+    cout<<(cnt[sum]*modInverse(2,mod-2))%mod<<endl;
+    
+
+    
 
     // for(int i=1;i<=n;i++){
 
