fd7b765a7006f4dbf54b8b2fe6f7b1b29580c430
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-16 18:00:39 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Counting Towers.cpp
+++ level-3/Dynamic Programming Introduction/Question/Counting Towers.cpp
@@ -17,80 +17,14 @@ int main() {
   ll t;cin>>t;
 
   while(t--) {
-    
+     ll n,m;cin>>n;
   }
   
   
 
-  ll n,m;cin>>n>>m;
+ 
 
-  vector<int>  val(n);
-
-  for (ll i = 0; i < n; i++)
-  {
-    /* code */
-    cin>>val[i];
-  }
-
-  vector<vector<ll>>  num(n,vector<ll>(m+1,0));
-
-  if(val[0]!=0){
-    num[0][val[0]]=1;
-  }else {
-    for (int i =1; i <=m; i++) {
-      num[0][i]=1;
-    }
-    
-  }
-
-  for (int i = 1; i < n; i++)
-  {
-    
-    if(val[i]==0){
-      for (int j = 1; j <=m; j++)
-      {
-        
-        if(j-1>=1){
-          num[i][j]=(num[i][j]+num[i-1][j-1])%mod;
-        }
-
-        num[i][j]=(num[i][j]+num[i-1][j])%mod;
-
-        if(j+1<=m)   num[i][j]=(num[i][j]+num[i-1][j+1])%mod;
-
-      }
-      
-    }else {
-
-
-      if(val[i]-1>=1){
-        num[i][val[i]]=(num[i][val[i]]+num[i-1][val[i]-1])%mod;
-
-      }
-      
-      num[i][val[i]]=(num[i][val[i]]+num[i-1][val[i]])%mod;
-
-      if(val[i]+1<=m)
-        num[i][val[i]]=(num[i][val[i]]+num[i-1][val[i]+1])%mod;
-
-
-    }
-  }
-
-  ll ans=0;
-
-  for (ll i =1; i <=m; i++)
-  {
-    
-    ans=(ans+num[n-1][i])%mod;
-  }
-
-  cout<<ans<<endl;
-  
-
-  
-// 2 0 0 3 0
-  
+   
 
 
 
