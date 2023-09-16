fcd77dab25b1dd0004625e136057161c7ec738e2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-16 21:54:58 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Rectangle Cutting.cpp
+++ level-3/Dynamic Programming Introduction/Question/Rectangle Cutting.cpp
@@ -5,45 +5,87 @@ using namespace std;
 
 typedef long long ll;
 
-const ll nax = 1e6 + 10;
+const ll nax = 505;
 const ll mod = (10e8) + 7;
 
-int main()
-{
+#define INF 1e9;
+
+int dp[nax][nax];
+
+int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  // vector<vector<ll>> dp(nax, vector<ll>(8, 0));
-
-  // for (int i = 0; i < 8; i++)
-  //   dp[0][i] = 1;
-
-  // for (int i = 1; i < nax; i++)
-  // {
-  //   for (ll j = 0; j < 8; j++)
-  //   {
-  //     /* code */
-  //     if (j == 0 || j == 2 || j == 3 || j == 4 || j == 5) {
-  //       dp[i][j] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][3] +
-  //                   dp[i - 1][4] + dp[i - 1][5]) %
-  //                  mod;
-  //     }
-  //     else {
-
-  //       dp[i][j] = (dp[i - 1][2] + dp[i - 1][6] + dp[i - 1][7]) % mod;
-
-  //     }
-  //   }
-  // }
-
-  ll t=1; 
+  ll t = 1;
   // cin >> t;
 
-  while (t--) {
-    ll n; cin >> n;
+  while (t--)
+  {
 
+    ll n, m;
+    cin >> n >> m;
+
+    for (int i =1; i <=n; i++) {
+     
+      for (int j=1; j <=m; j++)
+      {
+       
+        dp[i][j]=INF;
+      }
+      
+    }
+
+    for(int i=1;i<nax;i++){
+      dp[i][i]=0;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      /* code */
+      for (int j = 0; j < m; j++)
+      {
+        /* code */
+        cout<<dp[i][j]<<" ";
+      }
+
+      cout<<endl;
+      
+    }
+
+    cout<<endl;
+
+    for(int i=1;i<=n;i++) {
+      for(int j=1;j<=m;j++) {
+
+        for(int z=1;z<i;z++) {
+          dp[i][j]=min(dp[i][j],dp[z][j]+dp[i-z][j]+1);
+        }
+
+        for(int z=1;z<j;z++) {
+          dp[i][j]=min(dp[i][j],dp[i][z]+dp[i][j-z]+1);
+        }
+
+      }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      /* code */
+      for (int j = 0; j < m; j++)
+      {
+        /* code */
+        cout<<dp[i][j]<<" ";
+      }
+
+      cout<<endl;
+      
+    }
     
 
+
+
+    cout<<dp[n][m]<<endl;
+    
   }
 
   return 0;
