62d6810f9f2b313fdaf726279f7f0972d57d4c90
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-19 01:38:17 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Elevator Rides.cpp
+++ level-3/Dynamic Programming Introduction/Question/Elevator Rides.cpp
@@ -41,31 +41,40 @@ int main()
   while (t--)
   {
 
-    ll n, m;
-    cin >> n >> m;
+    int n;
+    ll maxWeight;
+    cin >> n >> maxWeight;
+    vector<ll> people(n);
+    for (int i = 0; i < n; i++)
+      cin >> people[i];
 
-    ll sum;
-
-    vector<ll> sub, che, ar(n + 1);
-
-    for (ll i = 0; i < n; i++)
-    {
-      /* code */
-      cin >> ar[i];
-    }
-
-    ll limit = (1 << n);
-
-    vector<pair<ll, ll>> dp(limit);
-
+    // use bit-masking to compute the dp table
+    int limit = 1 << n;
+    vector<pair<ll, ll>> dp(limit); // (numberOfRides, totalWeightOfLastRide)
     dp[0] = {1, 0};
 
-    for (ll i = 1; i < limit; i++)
+    for (int mask = 1; mask < limit; mask++)
     {
-      /* code */
-
-      pair<ll, ll> result = {INT_MAX, INT_MAX};
+      pair<ll, ll> bestResult = {INT_MAX, INT_MAX};
+      for (int i = 0; i < n; i++)
+      {
+        if (((1 << i) & mask) == 0)
+          continue;
+        auto res = dp[(1 << i) ^ mask];
+        if (res.second + people[i] <= maxWeight)
+        {
+          res.second += people[i];
+        }
+        else
+        {
+          res.first += 1;
+          res.second = people[i];
+        }
+        bestResult = min(bestResult, res);
+      }
+      dp[mask] = bestResult;
     }
+    cout << dp[limit - 1].first;
   }
   return 0;
 }
