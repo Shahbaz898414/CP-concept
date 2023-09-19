b7c5dbc4a6bcfc10d1ef000eebd77a10dbf3817e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-19 20:45:45 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/Dpbitmasking.cpp
+++ level-3/Dynamic Programming Introduction/Question/Dpbitmasking.cpp
@@ -4,49 +4,44 @@ using namespace std;
 
 typedef long long ll;
 
-
-void add(ll &subset,ll x){
-  subset=(subset^(1<<(x-1)));
+void add(ll &subset, ll x)
+{
+  subset = (subset ^ (1 << (x - 1)));
 }
 
-void remove (ll &subset, ll x) {
+void remove(ll &subset, ll x)
+{
 
-    subset=(subset^(1<<(x-1)));
-
+  subset = (subset ^ (1 << (x - 1)));
 }
 
-
-void display (ll subset){
-  for (ll i = 0; i <=9; i++)
+void display(ll subset)
+{
+  for (ll i = 0; i <= 9; i++)
   {
-   
-    if(subset&(1<<i)){
-      cout<<i+1<<" ";
+
+    if (subset & (1 << i))
+    {
+      cout << i + 1 << " ";
     }
   }
 }
-
 
 int main()
 {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  ll n;cin>>n;
+  ll n;
+  cin >> n;
 
-  add(n,5);
-  remove(n,5);
-  add(n,4);
-  add(n,6);
-  remove(n,6);
-  remove(n,1);
-
-
+  add(n, 5);
+  remove(n, 5);
+  add(n, 4);
+  add(n, 6);
+  remove(n, 6);
+  remove(n, 1);
 
   display(n);
 
-
   return 0;
 }
-
-
-
