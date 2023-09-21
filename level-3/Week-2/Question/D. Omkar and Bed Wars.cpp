6249f43ed8fb73a568cb857a96e4966ac7ae1222
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-22 04:23:30 +0530

    1

--- level-3/Week-2/Question/D. Omkar and Bed Wars.cpp
+++ level-3/Week-2/Question/D. Omkar and Bed Wars.cpp
@@ -11,43 +11,49 @@ int main()
   ll t = 1;
   cin >> t;
 
-  while (t--) {
+  while (t--)
+  {
 
-    ll n, k, z; cin >> n ;
+    ll n, k, z;
+    cin >> n;
 
-    string s;cin>>s;
+    string s;
+    cin >> s;
     ll ans = 0;
 
     vector<ll> vis(n);
 
-
-    for (ll i = 0; i < n; )
+    for (ll i = 0; i < n;)
     {
       /* code */
 
-      if(vis[i]) break;
+      if (vis[i])
+        break;
 
-      int j=0,k,cnt=0;
+      int j = 0, k, cnt = 0;
 
-      j=i;
-      while(s[j]==s[i] and j<n){
-        vis[j++]=1,cnt=1;
+      j = i;
+      while (s[j] == s[i] and j < n)
+      {
+        vis[j++] = 1, cnt = 1;
       }
 
-      if(i==0){
-        k=n-1;
+      if (i == 0)
+      {
+        k = n - 1;
 
-
-        while(k>i and !vis[k]  and s[j]==s[i]){
-          vis[k--]=1;
+        while (k > i and !vis[k] and s[k] == s[i])
+        {
+          vis[k--] = 1;
           cnt++;
         }
       }
+
+      ans += (cnt + 2 * (cnt == n) / 3);
+
+      i = j;
     }
-    
 
-   
-
-
+    cout << ans << endl;
   }
 }
