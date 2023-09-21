87d6ff108a56a0f69fdb4f51c0dedb6f7acc937e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-22 02:48:32 +0530

    1

---
+++ level-3/Week-2/Question/B. Array Walk.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const ll N = 2e5 + 5;
+
+ll dp[N][2];
+
+int main()
+{
+  ll t = 1;
+  cin >> t;
+
+  while (t--)
+  {
+    ll n, k, z;
+    cin >> n >> k >> z;
+
+    ll ans = 0;
+
+    vector<ll> arr(n + 1);
+
+    for (ll i = 0; i < n; i++)
+    {
+      /* code */
+      cin >> arr[i];
+    }
+
+    for (ll i = 0; i <= z; i++)
+    {
+      /* code */
+      ll mx = 0;
+      ll val = 0;
+
+      for (ll j = 0; j <= k - 2 * i; j++)
+      {
+        /* code */
+        val += arr[i];
+        mx = max(mx, arr[i] + arr[i + 1]);
+      }
+
+      ans = max(ans, val + i * mx);
+    }
+
+    cout << ans << endl;
+  }
+}
