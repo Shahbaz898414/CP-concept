f5441c81087a3fe8fbc1658868fde4d14e28ebc1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-09-22 22:45:07 +0530

    1

--- level-3/Week-2/Question/zzz.cpp
+++ level-3/Week-2/Question/zzz.cpp
@@ -1,48 +1,71 @@
-#include <iostream>
-#include <string>
-
+//{ Driver Code Starts
+#include<bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(const string& s) {
-    int left = 0, right = s.length() - 1;
-    while (left < right) {
-        if (s[left] != s[right]) {
-            return false;
-        }
-        left++;
-        right--;
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+  public:
+  int CountStrings(string s)
+  {
+        int n=s.size(),res=0;
+      for(int i=0;i<n;i++)
+      {
+          vector<int>cnt(10,0);
+          int mx1=0,mx2=0;
+          for(int j=i;j<min(n,i+72);j++)
+          {
+              cnt[s[j]-'0']++;
+              mx1=max(mx1,cnt[s[j]-'0']);
+              mx2=max(mx2,s[j]-'0');
+              if(mx1<mx2) res++;
+          }
+      }
+      return res;
+  }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.CountStrings(s)<<endl;
     }
-    return true;
 }
+// } Driver Code Ends
 
-bool canMakePalindrome(string s) {
-    int n = s.length();
-    
-    // Try removing each possible substring.
-    for (int i = 0; i < n; i++) {
-        string left = s.substr(0, i);
-        string right = s.substr(i);
-        string newString = left + right.substr(1); // Remove the current substring.
-        
-        if (isPalindrome(newString)) {
-            return true;
-        }
-    }
-    
-    return false;
-}
 
-int main() {
-    int n;cin>>n;
-    string s;
-    cin >> s;
+/*
 
-    if (canMakePalindrome(s)) {
-        cout << "YES" << endl;
-    } else {
-        cout << "NO" << endl;
-    }
 
-    return 0;
-}
 
+//User function Template for C++
+class Solution{
+  public:
+  int CountStrings(string s)
+  {
+      int n=s.size(),res=0;
+      for(int i=0;i<n;i++)
+      {
+          vector<int>cnt(10,0);
+          int mx1=0,mx2=0;
+          for(int j=i;j<min(n,i+72);j++)
+          {
+              cnt[s[j]-'0']++;
+              mx1=max(mx1,cnt[s[j]-'0']);
+              mx2=max(mx2,s[j]-'0');
+              if(mx1<mx2) res++;
+          }
+      }
+      return res;
+  }
+};
+
+
+*/
