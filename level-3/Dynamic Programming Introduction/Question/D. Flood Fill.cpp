ac5050fc080e012720ba2270f2348ec68fac0ce4
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-10-01 16:52:03 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/D. Flood Fill.cpp
+++ level-3/Dynamic Programming Introduction/Question/D. Flood Fill.cpp
@@ -59,3 +59,37 @@ int main()
 
 
 
+/*
+
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 5008;
+int n;
+int a[N],dp[N][N][2];
+
+int main()
+{
+	cin>>n;
+	for(int i=1;i<=n;++i)
+		cin>>a[i];
+	for(int i=2;i<=n;++i)
+	{
+		for(int j=1;j+i-1<=n;++j)
+		{
+			int l=j,r=j+i-1;
+			dp[l][r][0]=min(dp[l+1][r][0]+(a[l]!=a[l+1]),dp[l+1][r][1]+(a[l]!=a[r]));
+			dp[l][r][1]=min(dp[l][r-1][0]+(a[r]!=a[l]),dp[l][r-1][1]+(a[r-1]!=a[r]));
+		}
+	}
+	cout<<min(dp[1][n][0],dp[1][n][1])<<endl;
+}
+
+
+
+
+
+
+
+
+*/
