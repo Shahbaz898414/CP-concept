2ee1bb03704a24dcba157f1a8cf417396cd22c18
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-10-15 13:32:20 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/O - Matching.cpp
+++ level-3/Dynamic Programming Introduction/Question/O - Matching.cpp
@@ -4,7 +4,7 @@
 #define pb push_back
 #define fr(a, b) for (int i = a; i < b; i++)
 #define rep(i, a, b) for (int i = a; i < b; i++)
-#define mod 1000000007
+// #define mod 1000000007
 #define inf (1LL << 60)
 #define all(x) (x).begin(), (x).end()
 #define prDouble(x) cout << fixed << setprecision(10) << x
@@ -14,158 +14,126 @@
   cin.tie(NULL)
 using namespace std;
 
-#define mod 1000000007
+#define mo 1000000007
 
-
-
-void add_self(ll& a,ll b) {
-  a+=b;
-  if(a>=mod){
-    a-=mod;
-   }
+void add_self(ll &a, ll b)
+{
+  a += b;
+  if (a >= mo)
+  {
+    a -= mo;
+  }
 }
-
 
 int main()
 {
   fast_io;
 
   // ll n;
-  int t=1;
+  int t = 1;
   // cin >> t;
 
   while (t--)
   {
 
+    ll mod = 1e9 + 7;
 
-/*
-
-
-
- ll n;cin>>n;
-
-    vector<vector<ll>>  can(n,vector<ll>(n));
-
-    for (ll i = 0; i <n; i++) {
-      for (ll j = 0; j <n; j++) {
-        
-        cin>>can[i][j];
-      }
-      
-    }
+    /*
 
 
-    vector<ll>  dp(1<<n);
 
-    dp[0]=1;
+     ll n;cin>>n;
 
-    // for (int a = 0; a < n; a++)
-    // {
-     
-      for (int mask = 0; mask < (1<<n)-1; mask++)
-      {
-       
+        vector<vector<ll>>  can(n,vector<ll>(n));
 
-        int a=__builtin_popcount(mask);
-        for (ll b = 0; b < n; b++)
-        {
-         
-          if(can[a][b] and !(mask & (1<<b))){
-            int m2=mask^(1<<b);
+        for (ll i = 0; i <n; i++) {
+          for (ll j = 0; j <n; j++) {
 
-            add_self(dp[m2],dp[mask]);
+            cin>>can[i][j];
           }
+
         }
-        
+
+
+        vector<ll>  dp(1<<n);
+
+        dp[0]=1;
+
+        // for (int a = 0; a < n; a++)
+        // {
+
+          for (int mask = 0; mask < (1<<n)-1; mask++)
+          {
+
+
+            int a=__builtin_popcount(mask);
+            for (ll b = 0; b < n; b++)
+            {
+
+              if(can[a][b] and !(mask & (1<<b))){
+                int m2=mask^(1<<b);
+
+                add_self(dp[m2],dp[mask]);
+              }
+            }
+
+          }
+
+        // }
+
+        cout<<dp[(1<<n)-1]<<endl;
+
+
+
+    */
+
+    ll n;
+    cin >> n;
+
+    vector<vector<ll>> can(n, vector<ll>(n));
+
+    for (ll i = 0; i < n; i++)
+    {
+      for (ll j = 0; j < n; j++)
+      {
+
+        cin >> can[i][j];
       }
-      
-    // }
-
-    cout<<dp[(1<<n)-1]<<endl;
-
-
-
-*/
-
-
-
-
-    ll n;cin>>n;
-
-    vector<vector<ll>>  can(n,vector<ll>(n));
-
-    for (ll i = 0; i <n; i++) {
-      for (ll j = 0; j <n; j++) {
-        
-        cin>>can[i][j];
-      }
-      
     }
 
+    vector<vector<int>> dp(n, vector<int>((1 << n), -1));
 
-    vector<vector<int>>  dp(n,vector<int>((1<<n),-1));
+    function<int(int, int)> solve = [&](int i, int mask)
+    {
+      if (mask + 1 == (1 << n))
+        return 1;
 
-    function<int(int,int)> solve = [&] (int i,int mask){
+      if (i >= n)
+        return 0;
 
-      if(mask+1==(1<<n)) return 1;
+      if (dp[i][mask] != -1)
+        return dp[i][mask];
 
+      dp[i][mask] = 0;
 
-      if(i>=n)  return 0;
-       
+      for (int j = 0; j < n; j++)
+      {
 
-       if(dp[i][mask]!=-1)  return dp[i][mask];
+        if ((mask & (1 << j)) > 0)
+          continue;
 
+        if (can[i][j] == 1)
+        {
 
-       dp[i][mask]=0;
-
-
-       for (int j = 0; j < n; j++) {
-
-
-        if((mask&(1<<j))>0)continue;
-
-
-        if(can[i][j]==1)  {
-
-
-          (dp[i][mask]+=solve(i+1,mask|(1<<j)))%=mod;
-
-
+          (dp[i][mask] += solve(i + 1, mask | (1 << j))) %= mod;
         }
+      }
 
-
-       }
-
-
-       return dp[i][mask];
-       
-
+      return dp[i][mask];
     };
 
-    cout<<solve(0,0)<<endl;  
-    
-    
+    cout << solve(0, 0) << endl;
   }
 
   return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
