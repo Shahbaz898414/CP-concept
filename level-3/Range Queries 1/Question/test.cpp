2136023a846460011808ba29619dde23fccec73a
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-10-22 18:56:19 +0530

    1

--- level-3/Range Queries 1/Question/test.cpp
+++ level-3/Range Queries 1/Question/test.cpp
@@ -1,148 +1,45 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-// } Driver Code Ends
-
-class Solution {
-  public:
-    int powerup(int N, int K, vector<int> &arr) {
-
-      long long rq=0;
-      long long en=K;
-
-      for (int i = N-1; i >=0; i--)
-      {
-        /* code */
-        if(arr[i]==0){
-          en+=K;
-        }else {
-          rq+=(long long)arr[i];
-        }
-
-        if(rq<=en)  return i;
-      }
-      
-
-        return -1;
-    }
-};
-
-
-//{ Driver Code Starts.
-
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        
-        int N;
-        cin>>N;
-        int K;
-        cin>>K;
-        vector<int> energyArr(N);
-        for(int i=0;i<N;i++)
-            cin>>energyArr[i];
+    cin >> t;
 
-            // cout<<"1 sh"<<endl;
-        Solution obj;
-        int res = obj.powerup(N, K, energyArr);
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
 
-        // cout<<"1"<<endl;
-        cout<<res<<endl;
-        
+        vector<int> arr(n);
+
+        bool fl=0;
+        for (int i = 0; i < n; ++i) {
+            cin >> arr[i];
+
+            if(arr[i]%5==0) fl=1;
+        }
+
+
+        if(k==5 and fl==1){
+            cout<<0<<endl;
+                continue;
+        }else if(k==5){
+            
+        }
+        int operations = 100000000;
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] % k != 0) {
+               
+                int diff = min(k-arr[i]%k,arr[i]%k);
+                operations =min(operations,diff) ;
+            }
+        }
+
+        cout << operations << endl;
     }
+
+    return 0;
 }
 
-// } Driver Code Ends
 
-
-/*
-
-
-int solve(Node* root, int k,int l,int r){
-    if(root==NULL){
-        return 0;
-    }
-    // if(l==k && r==k){
-    //     return root->data;
-    // }
-    int suml=root->data,sumr=root->data,sum=0;
-    if(l<k && root->left!=NULL){
-        suml+=solve(root->left,k,l+1,r);
-    }
-    if(r<k && root->right!=NULL){
-        sumr+=solve(root->right,k,l,r+1);
-    }
-    sum=max(suml,sumr);
-    
-    return sum;
-}
-
-int solve1( Node* root, int k){
-    int ans=0;
-    if(root==NULL){
-        return 0;
-    }
-    ans=max(ans,solve(root,k,0,0));
-    ans=max(ans,solve1(root->left,k));
-    ans=max(ans,solve1(root->right,k));
-    return ans;
-}
-
-class Solution {
-  public:
-    int maximumSum(int n, Node* root, int k) {
-        // code here
-        int ans=0;
-        ans=solve1(root,k);
-        
-        return ans;
-    }
-};
-
-
-
-*/
-
-
-
-
-/*
-
-
-class Solution {
-  private:
-    int hlpr(Node* root, int kr,int kl){
-        if(root==nullptr) {
-            return 0;
-        }
-        int maxsm=0;
-        if(kr>0){
-            maxsm=max(hlpr(root->right, kr-1, kl),maxsm);
-        }
-        if(kl>0){
-            maxsm= max(hlpr(root->left, kr, kl-1),maxsm);
-        }
-        return maxsm+root->data;
-    }
-  public:
-    int maximumSum(int n, Node* root, int k) {
-        int maxsm=0;
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()){
-            Node* node=q.front();
-            q.pop();
-            if(node==nullptr) continue;
-            maxsm=max(hlpr(node,k,k),maxsm);
-            q.push(node->left);
-            q.push(node->right);
-        }
-        return maxsm;
-    }
-};
-
-
-
-*/
--- level-3/Range Queries 1/Question/test1.cpp
+++ level-3/Range Queries 1/Question/test1.cpp
@@ -1,14 +1,17 @@
 #pragma GCC optimize("O3,unroll-loops")
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 // #include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
 using namespace std;
 using namespace chrono;
 using namespace __gnu_pbds;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fastio()                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 #define MOD 1000000007
 #define MOD1 998244353
 #define INF 1e18
@@ -17,6 +20,9 @@ using namespace __gnu_pbds;
 #define ppb pop_back
 #define mp make_pair
 #define ff first
+#define  yes cout<<"YES"<<endl;
+#define  no cout<<"NO"<<endl;
+
 #define ss second
 #define PI 3.141592653589793238462
 #define set_bits(x) __builtin_popcountll(x)
@@ -24,9 +30,12 @@ using namespace __gnu_pbds;
 #define all(x) (x).begin(), (x).end()
 
 #ifdef Priyansh31dec
-#define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
 #else
-#define debug(x);
+#define debug(x) ;
 #endif
 
 using ll = long long;
@@ -34,61 +43,220 @@ using ull = unsigned long long;
 using lld = long double;
 // typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
 
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V>
+void _print(pair<T, V> p);
+template <class T>
+void _print(vector<T> v);
+template <class T>
+void _print(set<T> v);
+template <class T, class V>
+void _print(map<T, V> v);
+template <class T>
+void _print(multiset<T> v);
+template <class T, class V>
+void _print(pair<T, V> p)
+{
+    cerr << "{";
+    _print(p.ff);
+    cerr << ",";
+    _print(p.ss);
+    cerr << "}";
+}
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(map<T, V> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
 // void _print(pbds v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 /*---------------------------------------------------------------------------------------------------------------------------*/
-ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
-ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
-void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
-ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
-ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
-bool revsort(ll a, ll b) {return a > b;}
-ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
-void google(int t) {cout << "Case " << t << ": ";}
-vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
-ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-ll getRandomNumber(ll l, ll r) {return uniform_int_distribution<ll>(l, r)(rng);} 
+ll gcd(ll a, ll b)
+{
+    if (b > a)
+    {
+        return gcd(b, a);
+    }
+    if (b == 0)
+    {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+ll expo(ll a, ll b, ll mod)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = (res * a) % mod;
+        a = (a * a) % mod;
+        b = b >> 1;
+    }
+    return res;
+}
+void extendgcd(ll a, ll b, ll *v)
+{
+    if (b == 0)
+    {
+        v[0] = 1;
+        v[1] = 0;
+        v[2] = a;
+        return;
+    }
+    extendgcd(b, a % b, v);
+    ll x = v[1];
+    v[1] = v[0] - v[1] * (a / b);
+    v[0] = x;
+    return;
+} // pass an arry of size1 3
+ll mminv(ll a, ll b)
+{
+    ll arr[3];
+    extendgcd(a, b, arr);
+    return arr[0];
+} // for non prime b
+ll mminvprime(ll a, ll b) { return expo(a, b - 2, b); }
+bool revsort(ll a, ll b) { return a > b; }
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact)
+{
+    ll val1 = fact[n];
+    ll val2 = ifact[n - r];
+    ll val3 = ifact[r];
+    return (((val1 * val2) % m) * val3) % m;
+}
+void google(int t) { cout << "Case " << t << ": "; }
+vector<ll> sieve(int n)
+{
+    int *arr = new int[n + 1]();
+    vector<ll> vect;
+    for (int i = 2; i <= n; i++)
+        if (arr[i] == 0)
+        {
+            vect.push_back(i);
+            for (int j = 2 * i; j <= n; j += i)
+                arr[j] = 1;
+        }
+    return vect;
+}
+ll mod_add(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a + b) % m) + m) % m;
+}
+ll mod_mul(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a * b) % m) + m) % m;
+}
+ll mod_sub(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (((a - b) % m) + m) % m;
+}
+ll mod_div(ll a, ll b, ll m)
+{
+    a = a % m;
+    b = b % m;
+    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
+} // only for prime m
+ll phin(ll n)
+{
+    ll number = n;
+    if (n % 2 == 0)
+    {
+        number /= 2;
+        while (n % 2 == 0)
+            n /= 2;
+    }
+    for (ll i = 3; i <= sqrt(n); i += 2)
+    {
+        if (n % i == 0)
+        {
+            while (n % i == 0)
+                n /= i;
+            number = (number / i * (i - 1));
+        }
+    }
+    if (n > 1)
+        number = (number / n * (n - 1));
+    return number;
+} // O(sqrt(N))
+ll getRandomNumber(ll l, ll r) { return uniform_int_distribution<ll>(l, r)(rng); }
 
 /*--------------------------------------------------------------------------------------------------------------------------*/
-void topo_sort(int curr, vector<vector<int>>& edges, vector<bool>& vis, vector<int>& topoSort, int &inserted){
+void topo_sort(int curr, vector<vector<int>> &edges, vector<bool> &vis, vector<int> &topoSort, int &inserted)
+{
     vis[curr] = true;
-    for(auto i : edges[curr]){
-        if(!vis[i]){
+    for (auto i : edges[curr])
+    {
+        if (!vis[i])
+        {
             topo_sort(i, edges, vis, topoSort, inserted);
         }
     }
     int n = sz(edges);
-    topoSort[n - inserted - 1] = curr; 
+    topoSort[n - inserted - 1] = curr;
     inserted++;
 }
-void dfs(int curr, vector<vector<int>>& edges, vector<bool>& vis, vector<int>& comp){
+void dfs(int curr, vector<vector<int>> &edges, vector<bool> &vis, vector<int> &comp)
+{
     comp.push_back(curr + 1);
     vis[curr] = true;
-    for(auto i : edges[curr]){
-        if(!vis[i]){
+    for (auto i : edges[curr])
+    {
+        if (!vis[i])
+        {
             dfs(i, edges, vis, comp);
         }
     }
@@ -96,142 +264,159 @@ void dfs(int curr, vector<vector<int>>& edges, vector<bool>& vis, vector<int>& c
 
 class SegTree
 {
-    public:
+public:
     const int N = 5000005;
- 
-    int n;  // array size
-    ll   *sum_tree;
-    ll  res = 0;
- 
+
+    int n; // array size
+    ll *sum_tree;
+    ll res = 0;
+
     SegTree(vector<int> &arr, int n)
     {
-        sum_tree = new ll [N];
+        sum_tree = new ll[N];
         this->n = n;
         build(arr);
     }
- 
+
     void build(vector<int> &arr)
     {
         for (int i = 0; i < n; ++i)
         {
-            sum_tree[n+i]=arr[i];
+            sum_tree[n + i] = arr[i];
         }
- 
+
         for (int i = n - 1; i > 0; --i)
         {
-            sum_tree[i] = sum_tree[i<<1] + sum_tree[i<<1|1];
+            sum_tree[i] = sum_tree[i << 1] + sum_tree[i << 1 | 1];
         }
     }
- 
+
     void result(int l, int r)
     {
         for (l += n, r += n; l < r; l >>= 1, r >>= 1)
         {
-            if (l&1)
+            if (l & 1)
             {
                 res = res + sum_tree[l++];
             }
- 
-            if (r&1)
+
+            if (r & 1)
             {
                 res = res + sum_tree[--r];
             }
         }
     }
- 
-    ll  getSum(int l, int r)
+
+    ll getSum(int l, int r)
     {
-        if(l > r)       return 0;
+        if (l > r)
+            return 0;
         res = 0;
         result(l, r);
- 
+
         return res;
     }
- 
-    void update(int p, ll  value)
-    {   // set value at position p
-        for(sum_tree[p += n] = value; p > 1; p >>= 1)
+
+    void update(int p, ll value)
+    { // set value at position p
+        for (sum_tree[p += n] = value; p > 1; p >>= 1)
         {
-            sum_tree[p>>1] = (sum_tree[p] + sum_tree[p^1]);
+            sum_tree[p >> 1] = (sum_tree[p] + sum_tree[p ^ 1]);
         }
     }
 };
- 
 
+int digitSum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
 
-void solve() {
+int fgf(int x, int k)
+{
+    while (true)
+    {
+        if (digitSum(x) % k == 0)
+        {
+            return x;
+        }
+        x++;
+    }
+}
 
+void solve()
+{
 
-   ll n;cin>>n;
-  //  cout<<n;
+   ll n,k;cin>>n>>k;
 
    vector<ll>  arr(n);
 
-   for (ll i = 0; i <n; i++) {
-
-      cin>>arr[i];
-    
-   }
-
-   vector<int>  x(n);
-
-   map<int,int> cnt;
-
-   for (int i = n-1; i >=0; i--)
-   {
-    /* code */
-    cnt[arr[i]]++;
-    x[i]=cnt[arr[i]];
-   }
-
-   vector<int>  v(n+1);
-
-   for(auto it:x){
-      v[it]++;
-   }
-
-   SegTree sgt(v,n+1);
-
-   cnt.clear();
-
-   ll  ans=0;
+   ll pro=1;
 
    for (int i = 0; i < n; i++)
    {
     /* code */
-    cnt[arr[i]]++;
-    v[x[i]]--;
-    sgt.update(x[i],v[x[i]]);
-    ans+=sgt.getSum(1,cnt[arr[i]]);
+    cin>>arr[i];
+
+    pro*=arr[i];
    }
 
-   cout<<ans<<endl;
-   
-   
+   ll sum=0;
+
    
 
    
+
+   if(k==3){
+
+    if(pro%3==0){
+        cout<<0<<endl;
+    }else {
+        
+        while(pro>0){
+            sum+=(pro%10);
+            pro=pro/10;
+        }
+
+        if(sum%3==0){
+            
+        }
+    }
+
+   }
+
+
+   
+    
+
 }
 
+int main()
+{
+    // #ifdef Priyansh31dec
+    //     freopen("Error.txt", "w", stderr);
+    // #endif
 
-
-int main() {
-// #ifdef Priyansh31dec
-//     freopen("Error.txt", "w", stderr);
-// #endif
-
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // freopen("error.txt", "w", stderr);
+    // #endif
 
     fastio();
     auto start1 = high_resolution_clock::now();
-    solve();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
     auto stop1 = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop1 - start1);
 #ifdef Priyansh31dec
-    cerr << "Time: " << duration . count() / 1000 << " ms" << endl;
+    cerr << "Time: " << duration.count() / 1000 << " ms" << endl;
 #endif
 }
