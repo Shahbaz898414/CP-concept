78471cebbbc64b6b9ed4ac02ee9877d6148378a2
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-10 23:36:01 +0530

    1

--- level-3/Range Queries 1/C. Number of Minimums on a Segment.cpp
+++ level-3/Range Queries 1/C. Number of Minimums on a Segment.cpp
@@ -84,6 +84,7 @@ ll count_mini(ll int ss, ll int se, ll int qs, ll int qe, ll num, ll int index)
     else
       return 0;
   }
+  
   ll int mid = (ss + se) / 2;
 
   ll left = count_mini(ss, mid, qs, qe, num, 2 * index);
@@ -91,6 +92,7 @@ ll count_mini(ll int ss, ll int se, ll int qs, ll int qe, ll num, ll int index)
   ll right = count_mini(mid + 1, se, qs, qe, num, 2 * index + 1);
 
   return left + right;
+
 }
 
 void solve()
@@ -124,6 +126,8 @@ void solve()
   }
 }
 
+
+
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -146,100 +150,3 @@ int main()
 
 
 
-/*
-
-
-
-Today was my 37th day out of the 100 days  hard challenge.
-
-So today, I solved 5 problems .
-
-1. C. Number of Minimums on a Segment  (https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/C)
-
-
-
-2. B. K-th one (https://codeforces.com/edu/course/2/lesson/4/2/practice/contest/273278/problem/B)
-
-
-
-3. B. Segment Tree for the Minimum (https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/B)
-
-
-
-4. A. Segment Tree for the Sum (https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/A)
-
-
-
-5. A. Segment with the Maximum Sum (https://codeforces.com/edu/course/2/lesson/4/2/practice/contest/273278/problem/A)
-
-
-
-my Segment Tree Template
-//Build Segment Tree
-void build_tree(ll int *a, ll int s,ll int e, ll int *tree, ll int index)
-{
-	if(s==e)
-	{
-		tree[index] = a[s];
-		return;
-	}
-	ll int mid = (s+e)/2;
-
-	build_tree(a,s,mid,tree,2*index);
-
-	build_tree(a,mid+1,e,tree,2*index+1);
-
-	//  first change will be here
-
-	tree[index] = min(tree[2*index],tree[2*index+1]);
-	return;
-}
-
-//Query
-ll query(ll *tree,ll ss,ll se,ll qs,ll qe ,ll index)
-{
-	//complete overlap
-	if(ss>=qs and se<=qe)
-	{
-		return tree[index];
-	}
-	//No Overlap
-	if(qe<ss || qs>se)
-      	return INT_MAX;
-
-	//partial overlap
-	ll int mid = (ss + se)/2;
-	ll int left = query(tree,ss,mid,qs,qe,2*index);
-	ll int right = query(tree,mid+1,se,qs,qe,2*index+1);
-
-	// second will be here
-	return min(left,right);
-}
-
-//point update
-void point_update(ll  *tree, ll ss,ll se, ll i,ll inc,ll index)
-{
-	if(i>se || i<ss)
-	return;
-	if(ss == se)
-	{
-		tree[index] = inc;
-		return;
-	}
-	ll int mid = (ss + se)/2;
-	point_update(tree,ss,mid,i,inc,2*index);
-	point_update(tree,mid+1,se,i,inc,2*index+1);
-
-	// third will be here
-	tree[index] = min(tree[2*index],tree[2*index+1]);
-	return;
-}
-
-
-
-
-#100dayschallenge #challenge #consistency
-
-
-
-*/
