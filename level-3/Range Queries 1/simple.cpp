94ecae33bb97d0204a7cc5a29f81f9e1d3fee582
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-18 22:05:33 +0530

    1

--- level-3/Range Queries 1/simple.cpp
+++ level-3/Range Queries 1/simple.cpp
@@ -3,32 +3,16 @@
 #include <bits/stdc++.h>
 #include <algorithm>
 
-// #define int long long
+#define ll long long
 #define all(x) x.begin(), x.end()
 #define vi vector<long long int>
 
 using namespace std;
 
 
-long long countEqualCombinationPairs(vector<long long int>& a) {
-    int n = a.size();
-    unordered_map<int, int> freq;
 
-    long long result = 0;
 
-    // Count the frequency of each element in the array
-    for (int i = 0; i < n; ++i) {
-        freq[a[i]]++;
-    }
 
-    // For each unique combination, add the number of pairs to the result
-    for (auto& kv : freq) {
-        int count = kv.second;
-        result += 1LL * count * (count - 1) / 2;
-    }
-
-    return result;
-}
 
 signed main() {
     ios_base::sync_with_stdio(false);
@@ -38,80 +22,22 @@ signed main() {
     cin >> testCases;
 
     while (testCases--) {
+        ll n,k;cin>>n;
 
-			long long int size;
-    cin >> size;
-    vi numbers(size);
+       if(n==2 || n==1){
+        cout<<0<<endl;
+       }else if(n==3){
+        cout<<2<<endl;
+       }else {
 
-    for (long long int i = 0; i < size; i++)
-        cin >> numbers[i];
+          ll sum=(n*(n+1))/2;
 
+          if(sum&1) {
+            cout<<0<<endl;
+            continue;
+          }
 
-	long long int ans2=	countEqualCombinationPairs(numbers);
-	long long int ans3=	-1;
-
-
-
-
-   
-	 
-	  map<long long int, long long int> frequency;
-
-		
-		 unordered_map<long long int, long long int> freq;
-
-    long long ans = 0;
-
-		
-
-    for (long long int i = 0; i < size; i++){
-        frequency[numbers[i]]++;
-        freq[numbers[i]]++;}
-
-
-
-    long long int result = 0;
-    long long int mx = 0;
-
-
-    for (auto element : frequency) 
-		   result += element.second * (element.second - 1) / 2;
-
-		for (auto& kv : freq) {
-        int count = kv.second;
-        ans += 1LL * count * (count - 1) / 2;
-    }
-
-
-
-
-
-    result += frequency[1] * frequency[2];
-
-		mx=max({result,ans,ans2});
-
-		mx=mx-ans2;
-
-		ans2=ans2-mx;
-
-		
-
-
-
-
-
-
-
-
-
-		if(result== ans)  cout<<ans<<endl;
-		else{
-			 cout << max(result,ans2-mx) << endl;
-		}
-   
-
-
-
+       }
     }
 
     return 0;
@@ -119,75 +45,30 @@ signed main() {
 
 
 
+
+
 /*
 
-Today was my 44th day out of the 100 days  hard challenge.
+
+Today was my 45th day out of the 100 days  hard challenge.
 So today, I solved 5 problems .
 
 
-1. D. Yarik and Musical Notes (https://codeforces.com/contest/1899/problem/D).
+1. Company Queries I (https://cses.fi/problemset/task/1687).
 
 
-2. C. Yarik and Array (https://codeforces.com/contest/1899/problem/C).
+2. Company Queries II (https://cses.fi/problemset/task/1688).
 
 
-3. B. 250 Thousand Tons of TNT (https://codeforces.com/contest/1899/problem/B)
+3. Penalty Shoot-Out II (https://www.codechef.com/problems/PSHOT)
 
 
-4. A. Game with Integers (https://codeforces.com/contest/1899/problem/A).
+4. Bon Appetit (https://www.codechef.com/problems/FGFS).
 
 
-5. A. Addition to Segment (https://codeforces.com/edu/course/2/lesson/5/1/practice/contest/279634/problem/A).
+5. Chefina and Swap (https://www.codechef.com/problems/CHFNSWAP).
 
-
-My Segment Tree Template :-
-struct segtree {
-    int size;
-    vector<ll> tree;
- 
-    void build(int n) {
-        size = 1;
-        while (size < n) size *= 2;
-        tree.assign(2*size, 0);
-    }
- 
-    void add(int l, int r, int v) {
-        add(l, r, v, 0, 0, size);
-    }
- 
-    void add(int l, int r, int v, int x, int lx, int rx) {
-        if (lx >= r) return;
-        if (rx <= l) return;
-        if (lx >= l && rx <= r) {
-            tree[x] += v;
-            return;
-        }
- 
-        int m = (lx + rx) / 2;
-        add(l, r, v, 2*x+1, lx, m);
-        add(l, r, v, 2*x+2, m, rx);
-    }
- 
-    ll get(int idx) {
-        return get(idx, 0, 0, size);
-    }
- 
-    ll get(int idx, int x, int lx, int rx) {
-        if (rx == lx + 1) {
-            return tree[x];
-        }
- 
-        int m = (lx + rx) / 2;
-        if (idx < m)
-            return get(idx, 2*x+1, lx, m) + tree[x];
-        else
-            return get(idx, 2*x+2, m, rx) + tree[x];
-    }
-};
-
-
-#100dayschallenge #challenge #consistency #SegmentTree #lessons
-#learning
+#100dayschallenge #challenge #consistency #SegmentTree #lessons  #learning
 
 
 
@@ -196,6 +77,81 @@ struct segtree {
 
 
 /*
+
+
+Assignment: Log Ingestor and Query Interface
+
+Objective:
+Develop a log ingestor system that can efficiently handle vast volumes of log data, and offer a simple interface for querying this data using full-text search or specific field filters.
+
+Sample Log Data Format:
+
+{
+"level": "error",
+"message": "Failed to connect to DB",
+"resourceID": "server-1234",
+"timestamp": "2023-09-15T08:00:00Z",
+"traceID": "abc-xyz-123",
+"spanID": "span-456",
+"commit": "5e5342f",
+"metadata": {
+"parentResourceId": "server-0987"
+}
+}
+
+Requirements:
+Log Ingestor: Develop a mechanism to ingest logs in the provided format.
+Ensure scalability to handle high volumes of logs efficiently.
+Mitigate potential bottlenecks such as I/O operations, database write speeds, etc.
+
+Query Interface: Offer a user interface (Web UI or CLI) for full-text search across logs.
+
+Include filters based on:
+level
+message
+resourceID
+timestamp
+traceID
+spanID
+commit
+metadata.parentResourceId
+Aim for efficient and quick search results.
+
+Advanced Features (Bonus):
+Implement search within specific date ranges.
+Utilize regular expressions for search.
+Allow combining multiple filters.
+Provide real-time log ingestion and searching capabilities.
+Implement role-based access to the query interface.
+
+Sample Queries:
+Find all logs with the level set to "error".
+Search for logs with the message containing the term "Failed to connect".
+Filter logs between the timestamp "2023-09-10T00:00:00Z" and "2023-09-15T23:59:59Z".
+Retrieve all logs related to resourceID "server-1234".
+
+Evaluation Criteria:
+Volume: The ability of your system to ingest massive volumes.
+Speed: Efficiency in returning search results.
+Scalability: Adaptability to increasing volumes of logs/queries.
+Usability: Intuitive, user-friendly interface.
+Advanced Features: Implementation of bonus functionalities.
+
+Submission:
+Source code.
+README detailing setup instructions, design decisions, challenges faced, potential system improvements.
+Optional video or presentation showcasing the solution.
+
+Tips:
+Consider hybrid database solutions (relational + NoSQL) for a balance of structured data handling and efficient search capabilities.
+Database indexing and sharding might be beneficial for scalability and speed.
+Distributed systems or cloud-based solutions can ensure robust scalability.
+Tech Stack: You are free to use any tech stack you wish!
+Deadline: 20 Nov 2023
+
+Good luck! We look forward to your innovative solutions. If further clarifications are needed, please reach out.
+
+Once you solve this, ping me here again.
 
 
 
