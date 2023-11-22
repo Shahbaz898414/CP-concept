cacf91f2661fca784d1ae3752bf825c181c72f69
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-23 00:07:40 +0530

    1

--- level-3/Range Queries 1/test2.cpp
+++ level-3/Range Queries 1/test2.cpp
@@ -13,55 +13,124 @@ int main()
 
     while (t--)
     {
-       int N;
-        cin >> N;
 
-        vector<int> B(N);
-        for (int i = 0; i < N; i++) {
-            cin >> B[i];
+        ll arraySize, g = 0;
+        cin >> arraySize;
+        ll odd=0,even=0;
+        vector<ll> elements(arraySize);
+
+        vector<ll> evenNumbers, oddNumbers, main;
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            cin >> elements[i];
+            
+            // ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            // main.push_back(rightValue + leftValue);
+            // leftPart.push_back(leftValue);
+            // rightPart.push_back(rightValue);
+            if (elements[i] % 2 == 0)
+            {
+                evenNumbers.push_back(elements[i]);
+            }
+            else
+            {
+                oddNumbers.push_back(elements[i]);
+            }
+
+            main.push_back(elements[i]);
         }
 
-        if (N % 2 != 0) {
+        if (evenNumbers.size() % 2 != 0)
+        {
             cout << -1 << endl;
             continue;
         }
 
-        int M = N / 2;
-        vector<int> A(N, 0);
-        for (int i = 0; i < M; i++) {
-            A[i] = B[i];
-            if (i + M < N) {
-                if (A[i] == B[i + M]) {
-                    A[i + M] = 0;
-                } else {
-                    A[i + M] = abs(A[i] - B[i + M]);
-                }
-            }
-        }
+        vector<ll> leftPart, rightPart;
 
-        for (int i = 0; i < N; i++) {
-            cout << A[i] << " ";
-        }
-        cout << endl;
-
+        sort(oddNumbers.begin(), oddNumbers.end());
+        sort(main.begin(), main.end());
         
+            // ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            // main.push_back(rightValue + leftValue);
+            // leftPart.push_back(leftValue);
+            // rightPart.push_back(rightValue);
+        sort(evenNumbers.begin(), evenNumbers.end());
+
+        ll i = 0, j = evenNumbers.size() - 1;
+        while (i < j)
+        {
+            ll leftValue = (evenNumbers[i] + evenNumbers[j]) / 2;
+
+            ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            main.push_back(rightValue + leftValue);
+            
+            // ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            // main.push_back(rightValue + leftValue);
+            // leftPart.push_back(leftValue);
+            // rightPart.push_back(rightValue);
+            leftPart.push_back(leftValue);
+            rightPart.push_back(rightValue);
+            i++, j--;
+        }
+
+        sort(main.begin(), main.end());
+
+        i = 0, j = oddNumbers.size() - 1;
+        while (i < j)
+        {
+            ll leftValue = (oddNumbers[i] + oddNumbers[j]) / 2;
+            ll rightValue = (oddNumbers[j] - oddNumbers[i]) / 2;
+            main.push_back(leftValue + leftValue);
+            leftPart.push_back(leftValue);
+
+            main.push_back((leftValue + leftValue) / 2);
+            rightPart.push_back(rightValue);
+            i++, j--;
+        }
+
+        // sort(leftPart.begin(), leftPart.end());
+        sort(main.begin(), main.end());
+        
+            // ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            // main.push_back(rightValue + leftValue);
+            // leftPart.push_back(leftValue);
+            // rightPart.push_back(rightValue);
+        // sort(rightPart.begin(), rightPart.end());
+
+        for (int i = 0; i < leftPart.size(); i++)
+        {
+            
+            // ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            // main.push_back(rightValue + leftValue);
+            // leftPart.push_back(leftValue);
+            // rightPart.push_back(rightValue);
+            cout << leftPart[i] + (main[i] * g)  << " ";
+        }
+
+        for (int i = 0; i < rightPart.size(); i++)
+        {
+            
+            // ll rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            // main.push_back(rightValue + leftValue);
+            // leftPart.push_back(leftValue);
+            // rightPart.push_back(rightValue);
+            cout << rightPart[i] + (main[i] * g)  << " ";
+        }
+
+        cout << endl;
     }
 
     return 0;
 }
 
-
-
-
-
-
-
-
-
 /*
 
 
-Consider an array A of size N: A1 ,A2 ,…,AN ​  , where N is even. The AlterEgo of this array A, is another array B, which is computed as follows: 
+
+
+Consider an array A of size N: A1 ,A2 ,…,AN ​  , where N is even. The AlterEgo of this array A, is another array B, which is computed as follows:
 1. Start with an empty array B. For i ranging from  1 1 to 2 2 N ​  , insert the elements     2 ) (A  i ​  +A  i+  2 N ​   ​  ) and     2 ∣ ∣A  i ​  −A  i+  2 N ​   ​  ∣ to the end of array B. Here   ∣ ∣x∣ refers to absolute value of x. Rearrange the array B in some random order. As you can see, because of the third point, an array A can have multiple AlterEgo arrays.  Given an array B of even length N, your job is to find if there exists an array A such that B is the AlterEgo of A. And if it does exist, find such an array A with the maximum possible sum of elements.  That is, if there is no array A such that B is an AlterEgo of A, print  − 1 −1. Else print an array A such that B is an AlterEgo of A, and the sum of the elements of this array A is as large as possible. If there are multiple arrays possible with the maximum sum, print any.  Input Format The first line of input will contain a single integer T, denoting the number of test cases. Each test case consists of two lines of input. The first line of each test case contains a single integer N — the number of elements in the array. The second line of each test case contains N space-separated integers 1  2 , …  B  1 ​  ,B  2 ​  ,…,B  N ​   — the elements of the array. Output Format For each test case, output on a new line, either  − 1 −1, or N space separated integers denoting array A. If there are multiple arrays possible with the maximum sum, print any.  Constraints 1  ≤ 1 0 5 1≤T≤10  5   1  ≤ 3 ⋅ 1 0 5 1≤N≤3⋅10  5  N is even. 1  ≤ 1 0 5 1≤B  i ​  ≤10  5   The sum of N over all test cases won't exceed  3 ⋅ 1 0 5 3⋅10  5  .
 
 
@@ -72,22 +141,6 @@ Consider an array A of size N: A1 ,A2 ,…,AN ​  , where N is even. The AlterE
 
 
 
-
-
-Consider an array A of size N: A1 ,A2 ,…,AN ​  , where N is even. The AlterEgo of this array A, is another array B, which is computed as follows: 
-1. Start with an empty array B.
-2. For i ranging from  1 to N/2 ​, insert the elements (A[i] ​  + A[i+ N/2]) and  | A[i] - A[i-N/2] | to the end of array B.
-Here ∣x∣ refers to absolute value of x.
-3.Rearrange the array B in some random order.
-
-As you can see, because of the third point, an array A can have multiple AlterEgo arrays.
-Given an array B of even length N, your job is to find if there exists an array A such that B is the AlterEgo of A. And if it does exist, find such an array A with the maximum possible sum of elements.  That is, if there is no array A such that B is an AlterEgo of A, print −1. Else print an array A such that B is an AlterEgo of A, and the sum of the elements of this array A is as large as possible. If there are multiple arrays possible with the maximum sum, print any.
-
-Input 
-Format The first line of input will contain a single integer  T, denoting the number of test cases. Each test case consists of two lines of input. The first line of each test case contains a single integer  N — the number of elements in the array. The second line of each test case contains  N space-separated integers  B1 ​,B2 ,…,BN  — the elements of the array.
-
-
-Output Format For each test case, output on a new line, either  −1, or  N space separated integers denoting array  A. If there are multiple arrays possible with the maximum sum, print any.
 
 
 
--- level-3/Range Queries 1/zzzz.cpp
+++ level-3/Range Queries 1/zzzz.cpp
@@ -1,45 +1,70 @@
 #include <iostream>
 #include <vector>
-#include <bits/stdc++.h>
 #include <algorithm>
-
-#define int long long
-#define all(x) x.begin(), x.end()
 
 using namespace std;
 
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+int main() {
     int testCases;
     cin >> testCases;
 
     while (testCases--) {
-       int n;cin>>n;
+        long long arraySize;
+        cin >> arraySize;
+        vector<long long> elements(arraySize);
 
-       vector<int> arr(n);
-        int x=0;
-       for (int i = 0; i < n; i++)
-       {
-        /* code */
-        cin>>arr[i];
+        vector<long long> evenNumbers, oddNumbers;
 
-        x^=arr[i];
-       }
-       
-        
+        for (int i = 0; i < arraySize; i++) {
+            cin >> elements[i];
+            if (elements[i] % 2 == 0) {
+                evenNumbers.push_back(elements[i]);
+            } else {
+                oddNumbers.push_back(elements[i]);
+            }
+        }
+
+        if (evenNumbers.size() % 2 != 0) {
+            cout << -1 << endl;
+            return 0;
+        }
+
+        sort(oddNumbers.begin(), oddNumbers.end());
+        sort(evenNumbers.begin(), evenNumbers.end());
+
+        vector<long long> leftPart, rightPart;
+
+        long long i = 0, j = evenNumbers.size() - 1;
+        while (i < j) {
+            long long leftValue = (evenNumbers[i] + evenNumbers[j]) / 2;
+            long long rightValue = (evenNumbers[j] - evenNumbers[i]) / 2;
+            leftPart.push_back(leftValue);
+            rightPart.push_back(rightValue);
+            i++, j--;
+        }
+
+        i = 0, j = oddNumbers.size() - 1;
+        while (i < j) {
+            long long leftValue = (oddNumbers[i] + oddNumbers[j]) / 2;
+            long long rightValue = (oddNumbers[j] - oddNumbers[i]) / 2;
+            leftPart.push_back(leftValue);
+            rightPart.push_back(rightValue);
+            i++, j--;
+        }
+
+        sort(leftPart.begin(), leftPart.end());
+        sort(rightPart.begin(), rightPart.end());
+
+        for (int i = 0; i < leftPart.size(); i++) {
+            cout << leftPart[i] << " ";
+        }
+
+        for (int i = 0; i < rightPart.size(); i++) {
+            cout << rightPart[i] << " ";
+        }
+
+        cout << endl;
     }
 
     return 0;
 }
-
-
-
-/*
-
-Sonu and Titu are playing a tennis match. They are playing a "best of  N sets" match ( N is always odd). That means that they will play at most  N sets, and the person who has won most sets wins the match. But they are smart, and if they notice at any point that one of them has no chance of winning the match, they will stop the match right then, without playing out all the  N sets needlessly.  For example, suppose  = 11 N=11. And at some point, Sonu has won 5 sets, and Titu has won 4 sets. Sonu is leading now, but it is possible that Titu wins the two remaining sets and wins the whole match. So they will not stop right now.  But suppose Sonu wins the next set as well, and so now the score is (6,4). Now, even though there is one set remaining, there is no chance of Titu winning the whole match, since at best, even if he wins the next set, the score will become  (6,5), and Sonu still wins. So, they will stop at a score of  (6,4) and declare Sonu as the winner.  As another example, if  = 15 N=15, and the score is currently (8,2), then they'll stop right now, since there is no way for Titu to win the match. Even if Titu wins all the  5 5 remaining sets, Titu cannot win the match.  You are watching the match, and you see that the match has ended with a score of (X,Y). That is, Sonu has won  X sets, and Titu has won  Y sets. But you don't know what  N is. Can you find out  N from just knowing the final score?  Input Format The first line of input will contain a single integer  T, denoting the number of test cases. Each test case consists of a single line of input containing two space-separated integers  X and  Y where  X is the number of sets won by Sonu and  Y is the number of sets won by Titu. Output Format For each test case, output on a new line the value of  N. Note that you should only output an odd integer. Constraints 1 ≤ ≤ 10000 1≤T≤10000 1 ≤ ≤ 100 1≤X≤100 1 ≤ ≤ 100 1≤Y≤100 ≠ X=Y, since they would never stop when they have won the same number of sets.
-
-
-
-*/
