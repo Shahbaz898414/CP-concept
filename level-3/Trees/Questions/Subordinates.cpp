5a75fc6c8a278e97fe7123290a4c2aa69eca569c
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-27 19:56:11 +0530

    1

--- level-3/Trees/Questions/Subordinates.cpp
+++ level-3/Trees/Questions/Subordinates.cpp
@@ -33,36 +33,82 @@ int calc (int node) {
 
 int main () {
 
-  int n;cin >> n;
+  // int n;cin >> n;
   
  
 
-  for (int i = 2; i <=n; i++) {
+  // for (int i = 2; i <=n; i++) {
 
-    int boss;cin>>boss;
-    // cin>>arr[i];
+  //   int boss;cin>>boss;
+  //   // cin>>arr[i];
 
-    adj[boss].push_back(i);
+  //   adj[boss].push_back(i);
 
-  }
-
-  // for(auto it:adj){
-  //   for(auto i:it){
-  //     cout<<i<<" ";
-  //   }
-  //   cout<<endl;
   // }
 
-  // cout<<endl;
+  // // for(auto it:adj){
+  // //   for(auto i:it){
+  // //     cout<<i<<" ";
+  // //   }
+  // //   cout<<endl;
+  // // }
 
-  calc(1);
+  // // cout<<endl;
 
-  // cout<<endl<<endl;
+  // calc(1);
+
+  // // cout<<endl<<endl;
 
 
-  for (int i = 1; i <= n; i++) {
-      cout << subtree_size[i] << " ";
+  // for (int i = 1; i <= n; i++) {
+  //     cout << subtree_size[i] << " ";
+  // }
+
+
+  int n ;cin>>n;
+
+  vector<int> par(n+1);
+
+  for (int i =2; i <=n; i++)
+  {
+    /* code */
+    cin>>par[i];
   }
+
+
+  vector<int> add[n+1];
+
+  for (int i = 2; i <=n; i++)
+  {
+    /* code */
+    adj[par[i]].push_back(i);
+  }
+
+    vector<int>  sizes(n+1,0);
+
+  // lamba function
+
+  function<void(int)> dfs = [&] (int node)-> void {
+    sizes[node]=1;
+    for (int child:adj[node])
+      if(child!=par[node]){
+        dfs(child);
+        sizes[node]+=sizes[child];
+      }
+
+  };
+
+  dfs(1);
+
+  for (int i = 1; i <=n; i++)
+  {
+    /* code */
+    cout<<sizes[i]-1<<" ";
+  }
+  cout<<endl;
+  
+  
+  
   
 
     
--- level-3/Trees/Questions/zpp.cpp
+++ level-3/Trees/Questions/zpp.cpp
@@ -107,238 +107,73 @@ const long long inf = 1e18;
 const int MOD = 1e9 + 7;
 const int MAX = 1e6;
 
-bool isValid(string s)
+int largestSubmatrix(vector<vector<int>> &matrix)
 {
-  int len = s.size();
-  for (int i = 0; i < len / 2; i++)
+  int m = matrix.size();
+  int n = matrix[0].size();
+  for (int i = 1; i < m; i++)
   {
-    if (s[i] != s[len - 1 - i])
-      return false;
-  }
-  return true;
-}
-
-void rotateMatrix(vector<vector<int>> &v, int n)
-{
-  for (int i = 0; i < n / 2; i++)
-  {
-    for (int j = i; j < n - i - 1; j++)
+    for (int j = 0; j < n; j++)
     {
-      int ptr = v[i][j];
-      v[i][j] = v[n - 1 - j][i];
-      v[n - 1 - j][i] = v[n - 1 - i][n - 1 - j];
-      v[n - 1 - i][n - 1 - j] = v[j][n - 1 - i];
-      v[j][n - 1 - i] = ptr;
-    }
-  }
-}
-
-vector<bool> is_prime(10001, 1);
-vector<int> primes;
-
-void seive()
-{
-  is_prime[0] = 0;
-  is_prime[1] = 0;
-  for (int i = 2; i < 10001; i++)
-  {
-    if (is_prime[i])
-    {
-      primes.push_back(i);
-      for (int j = i + i; j < 10001; j += i)
+      if (matrix[i][j] == 1)
       {
-        is_prime[j] = 0;
+        matrix[i][j] = matrix[i - 1][j] + 1;
       }
     }
   }
-}
-
-
-/*
-
-find out the biggest kth parent of X for which it is not equal to the parent of Y
-return the 1st parent of Z
-
-
-*/
 
 
 
-
-
-
-// dfs(0, edge, -1, parents, maxparent);
-
-void dfs(int curr, vector<vector<int>> &edgs, int currParent, vector<vector<int>> &parents,vector<int>&levels)
-{
-    parents[curr][0] = currParent;
-
-    if(currParent!=-1){
-      levels[curr]=levels[currParent]+1;
+  for (ll i = 0; i < n; i++) {
+    for (ll j = 0; j < m; j++) { 
+      cout << matrix[i][j] << " ";
     }
-
-    for (auto child : edgs[curr])
-    {
-        if (child != currParent)
-        {
-            dfs(child, edgs, curr, parents,levels);
-        }
-    }
-}
-
-int kthParent(int x, int k, vector<vector<int>> &parents)
-{
-    int maxParent = parents[0].size();
-
-    for (int i = 0; i < maxParent; i++)
-    {
-        if (((k >> i) & 1))
-        {
-            if (x != -1)
-                x = parents[x][i];
-        }
-    }
-
-    return x;
-}
-
-
-
-
-
-int lca(int x,int y,vector<vector<int>> &parent,vector<int>&level) {
-
-    if(x==y)  return x;
-
-    if(level[x]<level[y]){
-      swap(x,y);
-
-
-    }
-
-    int diffLevel=level[x]-level[y];
-
-    x=kthParent(x,diffLevel,parent);
-
-
-  int mxlog = parent[0].size();
-
-
-  for (int i = mxlog-1; i >=0; i--) {
-
-
-    int parentofX=parent[x][i];
-    int parentofY=parent[y][i];
-
-
-    if(parentofX!=parentofY){
-
-
-      x=parentofX;
-      y=parentofY;
-
-
-    }
-
-
+    cout << endl;
   }
 
+  
 
- return parent[x][0];
- 
+  int ans = 0;
+  for (auto itr : matrix)
+  {
+    sort(itr.begin(), itr.end());
 
+    for (int k = n - 1, w = 1; k >= 0 && itr[k] > 0; k--, w++)
+    {
+
+      
+      int area = itr[k] * w;
+
+      ans = max(ans, area);
+
+
+    }
+  }
+  return ans;
 }
-
 
 int32_t main()
 {
 
-  ll t=1;
-  // cin >> t;
-  // seive();
-  while (t--)
+  ll n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> arr(n, vector<int>(m));
+
+  for (ll i = 0; i < n; i++)
   {
-    int n, q;
-    cin >> n >> q;
-
-   int root = 0;
-
-
-    int maxparent = log2(n) + 1;
-
-
-    // cout<<n<<" "<<maxparent<<endl;
-
-
-    vector<vector<int>> parents(n, vector<int>(maxparent, -1));
-
-
-    vector<int>  levels(n);
-
-    vector<vector<int>> edge(n, vector<int>());
-
-
-    for (int i = 1; i < n; i++)  {
-        // int a,b;cin>>a>>b;
-        // edge[a-1].push_back(b-1);
-        // edge[b-1].push_back(a-1);
-
-        int a;cin>>a;
-
-
-        edge[a-1].push_back(i);
-
-    }
-
- 
-    dfs(root, edge, -1, parents,levels);
-
-
-    // debug(parents);
-
-
-    for (int j = 1; j < maxparent; j++)
+    /* code */
+    for (ll j = 0; j < m; j++)
     {
-        for (int i = 0; i < n; i++)
-        {
-            int intermediate = parents[i][j - 1];
-            if (intermediate != -1)
-            {
-                parents[i][j] = parents[intermediate][j - 1];
-            }
-        }
+      /* code */
+      cin >> arr[i][j];
     }
-
-
-    // debug(parents);
-
-
-    vector<int> inTime(n),outTime(n);
-    int globalTime=0;
-
-    // dfs_in_out(root,edge,-1,inTime,outTime,globalTime);
-
-    while(q--) {
-
-
-      int a,b;cin>>a>>b; a--;b--;
-
-
-      cout<<lca(a,b,parents,levels)+1<<endl;
-
-
-    }
-
-    
-    
   }
+
+  cout << largestSubmatrix(arr);
+
   return 0;
 }
-
-
-
-
-
 
 /*
 
@@ -367,7 +202,6 @@ So today, I solved 5 problems .
 
 
 */
-
 
 /*
 
@@ -404,8 +238,6 @@ public:
 
 */
 
-
-
 /*
 
  int m=matrix.size();
@@ -424,9 +256,9 @@ public:
         for(auto itr:matrix)
         {
             sort(itr.begin(),itr.end());
-    
+
             for(int k=n-1,w=1;k>=0 && itr[k]>0;k--,w++)
-            {   
+            {
                 int area=itr[k]*w;
                 ans=max(ans,area);
             }
