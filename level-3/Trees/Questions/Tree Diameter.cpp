61a040099a69c3dc6a023c023a83b2be7eefe971
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-28 01:14:07 +0530

    1

--- level-3/Trees/Questions/Tree Diameter.cpp
+++ level-3/Trees/Questions/Tree Diameter.cpp
@@ -1,81 +1,78 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define ll long long
-
 #define MAXN 202020
 
+int main() {
+    int n;
+    cin >> n;
 
+    vector<int> adj[n + 1];
 
+    for (int i = 0; i <n-1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
 
+    vector<int> depth(n + 1, 0);
+    vector<int> diameter(n + 1, 0);
+    vector<int> ans(n + 1, 0);
 
+    // Lambda function
+    function<void(int, int)> dfs = [&](int node, int parent) -> void {
+        depth[node] = 1;
+        diameter[node] = 1;
 
+        int mx = 0;
 
+        for (int child : adj[node]) {
+            if (child != parent) {
+                dfs(child, node);
 
-int main () {
+                depth[node] = max(depth[node],1+ depth[child]);
+                diameter[node] = max({diameter[node], diameter[child],1 + mx + depth[child]});
+                // diameter[node] = max(diameter[node], 1 + mx + depth[child]);
 
- 
+                mx = max(mx, depth[child]);
+            }
+        }
+    };
 
+    dfs(1, 0);
 
-  int n ;cin>>n;
+    cout << diameter[1]-1 << endl;
 
-  vector<int> adj[n+1];
-
-  for (int i = 0; i <n-1; i++)
-  {
-    /* code */
-    int u,v;cin>>u>>v;
-
-    adj[u].push_back(v);
-    adj[v].push_back(u);
-  }
-
-
-  vector<int>  depth(n+1,0);
-  vector<int>  diameter(n+1,0);
-  vector<int>  ans(n+1,0);
-  
-
-  // lamba function
-
-  function<void(int,int)> dfs = [&] (int node,int parent)-> void {
-    depth[node]=1;
-    diameter[node]=1;
-
-    int mx=0;
-
-    for(int child:adj[node]){
-      if(child!=parent){
-        dfs(child,parent);
-
-        depth[node]=max(depth[node],depth[child]);
-        diameter[node]=max(diameter[node],diameter[child]);
-        diameter[node]=max(diameter[node],1+mx+depth[child]);
-
-        mx=max(mx,depth[child]);
-      }
-    }  
-
-  };
-
-  dfs(1,0);
-
- 
-  
-  cout<<depth[1]<<endl;
-  
-  
-
-    
+    return 0;
 }
 
 
 /*
 
-2 3 
-4 
-5
+
+Today was my 53th day out of the 100 days  hard challenge.
+So today, I solved 5 problems .
+
+
+1. B. Death's Blessing (https://codeforces.com/problemset/problem/1749/B).
+
+
+2. B. BAN BAN (https://codeforces.com/problemset/problem/1747/B).
+
+
+3. C. Stripes (https://codeforces.com/problemset/problem/1742/C)
+
+
+4. A. Ela Sorting Books (https://codeforces.com/problemset/problem/1737/A).
+
+
+5. B. Tea with Tangerines (https://codeforces.com/problemset/problem/1735/B).
+
+
+#100dayschallenge #challenge #consistency #Cp #lessons  #learning #setmentTree
 
 
 
