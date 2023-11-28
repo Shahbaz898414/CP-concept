5b3581f841c618e04af5479f2688c024a9dd94e6
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-11-28 09:54:16 +0530

    1

---
+++ level-3/Graph/Question/zzz.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define MAXN 202020
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> adj[n + 1];
+
+    for (int i = 0; i <n-1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<ll> sizes(n + 1, 0);
+    vector<ll> sum(n + 1, 0);
+    vector<ll> ans(n + 1, 0);
+
+    // Lambda function
+    function<void(int, int)> dfs = [&](int node, int parent) -> void {
+        sizes[node] = 1;
+        sum[node] = 0;
+
+        // int mx = 0;
+
+        for (int child : adj[node]) {
+            if (child != parent) {
+                dfs(child, node);
+
+               sizes[node]+=sizes[child];
+               sum[node]+=sum[child]+sizes[child];
+            }
+        }
+    };
+
+
+
+    auto change = [&] (int node,int child) {
+
+        sum[node]-=sum[child]+sizes[child];
+
+        sizes[node]-=sizes[child];
+
+        sizes[child]+=sizes[node];
+
+        sum[child] += sum[node]+sizes[node];
+
+    };
+
+
+
+    function<void(int,int)> reroot=[&] (int node,int parent){
+        ans[node] = sum[node];
+
+        for(int child:adj[node])
+            if(child!=parent){
+                change(node,child);
+                reroot(child,node);
+                change(child,node);
+            }
+    };
+
+
+
+    dfs(1, 0);
+
+
+    reroot(1,0);
+
+
+    for(int i=1;i<=n;i++){
+        cout<<ans[i]<<" ";
+    }
+
+   
+
+    return 0;
+}
+
+
