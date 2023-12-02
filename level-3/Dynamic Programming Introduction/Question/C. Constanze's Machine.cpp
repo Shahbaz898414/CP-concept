89f4dd217ed9afc98af2512c62c57e55b1ee27dd
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-03 04:34:56 +0530

    1

---
+++ level-3/Dynamic Programming Introduction/Question/C. Constanze's Machine.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const ll mod = (10e8) + 7;
+
+// ll   f[2][100001],a[2][100001];
+
+int main()
+{
+
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  string s;
+  cin >> s;
+
+  ll len = s.size();
+
+  for (char i : s)
+  {
+    if (i == 'w' || i == 'm')
+    {
+      cout << 0 << endl;
+      return 0;
+    }
+  }
+
+  int n = s.size();
+  vector<int> dp(n + 1);
+  dp[0] = dp[1] = 1;
+  for (int i = 2; i <= n; i++)
+  {
+    dp[i] = dp[i - 1];
+    if (s[i - 1] == s[i - 2] && (s[i - 1] == 'u' || s[i - 1] == 'n'))
+    {
+      dp[i] = (dp[i] + dp[i - 2]) % mod;
+    }
+  }
+  cout << dp[n] << endl;
+
+  
+}
+
+// ouuokarinn
+/*
+
+ouuokarinn
+owokarim
+
+
+*/
--- level-3/Dynamic Programming Introduction/Question/C. Letter.cpp
+++ level-3/Dynamic Programming Introduction/Question/C. Letter.cpp
@@ -46,23 +46,23 @@ int main()
 
 /*
 
-Today was my 58th day out of the 100 days  hard challenge.
+Today was my 59th day out of the 100 days  hard challenge.
 So today, I solved 5 problems .
 
 
-1. E - Unfair Sugoroku (https://lnkd.in/dmw-kQpm).
+1. C. Constanze's Machine (https://codeforces.com/problemset/problem/1245/C).
 
 
-2. A. Halloumi Boxes (https://lnkd.in/dsU-9AUW).
+2. D. Polycarp and Div 3 (https://codeforces.com/contest/1005/problem/D).
 
 
-3. B. StORage room (https://lnkd.in/dYAsrKAA)
+3. C. Basketball Exercise (https://codeforces.com/contest/1195/problem/C)
 
 
-4. C. Theofanis' Nightmare (https://lnkd.in/dmUDz3wy).
+4. C. Letter (https://codeforces.com/contest/180/problem/C).
 
 
-5. E. Anfisa the Monkey (https://lnkd.in/dtfHusss).
+5. E. Anfisa the Monkey (https://codeforces.com/contest/44/problem/E).
 
 
 #100dayschallenge #challenge #consistency #Cp #lessons  #learning
