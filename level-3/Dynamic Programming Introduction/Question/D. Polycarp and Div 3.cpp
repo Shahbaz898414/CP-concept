294ee4ac90cf9880e6472b1a39c80ea8a6180f63
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-03 02:58:00 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/D. Polycarp and Div 3.cpp
+++ level-3/Dynamic Programming Introduction/Question/D. Polycarp and Div 3.cpp
@@ -19,5 +19,41 @@ int main()
 
   ll len=s.size();
 
+  // cout<<len;
+
+  vector<ll>  arr(len,0);
+
+  for (ll i = 0; i < len; i++)
+  {
+    /* code */
+    arr[i]=(s[i]-'0')%3;
+  }
+
+  ll ans=0;
+  int i=0;
+
+  while (i<len)
+  {
+    /* code */
+    if(arr[i]==0){
+      i++;
+      ans++;
+    }else if(i<len-1 and arr[i]==2 and arr[i+1]==1){
+      ans++;
+      i+=2;
+    }else if(i<len-1 and arr[i]==1 and arr[i+1]==2){
+      ans++;
+      i+=2;
+    }else if(i<len-2 and arr[i]==1 and arr[i+1]==1 and arr[i+2]==1){
+      ans++;
+      i+=3;
+    }else if(i<len-2 and arr[i]==2 and arr[i+1]==2 and arr[i+2]==2){
+      ans++;
+      i+=3;
+    }else i++;
+  }
+  
+  cout<<ans<<endl;
+
 
 }
