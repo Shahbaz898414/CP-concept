069455fbcb747cef7109373b2dfbc6b7f0128987
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-03 02:23:26 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/D. Polycarp and Div 3.cpp
+++ level-3/Dynamic Programming Introduction/Question/D. Polycarp and Div 3.cpp
@@ -15,40 +15,9 @@ int main()
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  ll n;cin >> n;
+  string s;cin>>s;
 
-
-  ll a[n], b[n];
-
-
-  for (ll i = 0; i < n; i++) cin >> a[i];
-
-
-  for (ll i = 0; i < n; i++) cin >> b[i];
-
-
-  for (ll i = 1; i < n; i++ )  {
-
-
-    a[i] = max(a[i - 1], a[i] + b[i - 1]);
-
-
-    b[i] = max(b[i - 1], b[i] + a[i - 1]);
-
-
-  }
-
-  // for (ll i = 0; i < n; i++)
-  // {
-  //   /* code */
-  //   cout<<a[i]<<" "<<b[i]<<endl;
-  // }
-
-  // cout<<endl;
-  
-
-
-  cout << max(a[n - 1], b[n - 1]);
+  ll len=s.size();
 
 
 }
