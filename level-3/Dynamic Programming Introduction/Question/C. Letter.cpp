dbd3684608133cf041dac9af4cd1cbdfe3457bb1
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-02 18:33:43 +0530

    1

---
+++ level-3/Dynamic Programming Introduction/Question/C. Letter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const ll mod = (10e8) + 7;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  // ll k, a, b;
+  // cin >> k >> a >> b;
+
+  string s;
+  cin >> s;
+
+  ll len = s.size();
+
+  
+
+
+  
+}
+
--- level-3/Dynamic Programming Introduction/Question/E. Anfisa the Monkey.cpp
+++ level-3/Dynamic Programming Introduction/Question/E. Anfisa the Monkey.cpp
@@ -26,62 +26,24 @@ int main()
   }
   else
   {
+
+
     int l = 0, r = 0;
-    while (k != 0)
-    {
+    cout<<len<<endl;
+
+
+    while (k != 0) {
+
+
       r = (s.size() - l) / k;
+     
       cout << s.substr(l, r) << endl;
       l += r;
+      //  cout<<r<<" "<<k<<" "<<l<<endl;
       k--;
     }
+
+
   }
 }
 
-/*
-
-
-#include<bits/stdc++.h>
-using namespace std;
-int k,a,b,l,r,len;
-string s;
-int main(){
-  cin >> k >> a >> b >> s;
-  len=s.size();
-  if (s.size()<(k*a) || s.size()>(k*b))
-    return cout << "No solution",0;
-  for(;k>0;k--){
-    r=(s.size()-l)/k;
-    cout << s.substr(l,r) << endl;
-    l+=r;
-  }
-}
-
-
-
-
-
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-int main() {
-  ios::sync_with_stdio(0); cin.tie(0);
-  int k, a, b;
-  string s;
-  cin >> k >> a >> b >> s;
-  int n = s.size();
-  if (k*a > n || k*b < n) {
-    cout << "No solution" << endl;
-  } else {
-    for (int i = 0; i < k; i++) {
-      cout << s.substr(i*n/k,(i+1)*n/k-i*n/k) << endl;
-    }
-  }
-}
-
-
-
-*/
