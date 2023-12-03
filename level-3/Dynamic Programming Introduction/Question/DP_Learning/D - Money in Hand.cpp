2625745aad6a4cebaa599c69f640d39a8865999e
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-04 04:18:23 +0530

    1

--- level-3/Dynamic Programming Introduction/Question/DP_Learning/D - Money in Hand.cpp
+++ level-3/Dynamic Programming Introduction/Question/DP_Learning/D - Money in Hand.cpp
@@ -9,63 +9,89 @@ const ll mod = (10e8) + 7;
 
 // ll   f[2][100001],a[2][100001];
 
-int dp[51][10001];
-
-int rec(int i,int x,vector<vector<int>>&a){
-  if(i==a.size()){
-    if(x==0)   return 1;
-    else return 0;
-  }
-
-  if(x==0){
-    return 1;
-  }
-
-  if(dp[i][x]!=-1){
-    return dp[i][x];
-  }
-
-  int take=rec(i+1,x,a);
-  for (int j = 1; j <=a[i][1]; j++)
-  {
-    /* code */
-    if(x>=(a[i][0]*j)){
-      take|=rec(i+1,x-(a[i][0]*j),a);
-    }
-  }
-  
-
-
-  return dp[i][x]=take;
-}
 
 
 
-int main()
-{
+int main() {
+
 
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  memset(dp,-1,sizeof(dp));
 
 
-  int n,x;cin>>n>>x;
+   int N, X; cin >> N >> X;
 
-  vector<vector<int>> a(n,vector<int>(2,0));
 
-  for (int i = 0; i < n; i++)
-  {
-    /* code */
-    cin>>a[i][0]>>a[i][1];
-  }
-  
+    vector<int> A(N), B(N);
 
-  int ans=rec(0,x,a);
 
-  cout<<(ans==0?"NO":"YES")<<endl;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i] >> B[i];
+    }
+
+
+    vector<bool> dp(X + 1);
+
+
+    dp[0] = true;
+
+
+    for (int i = 0; i < N; i++) {
+
+        for (int j = X - 1; j >= 0; j--) {
+
+            if (dp[j]) {
+              cout<<j<<" ";
+
+                for (int k = 1; k <= B[i]; k++) {
+
+                    if (j + A[i] * k > X) break;
+
+                    dp[j + A[i] * k] = true;
+
+                }
+                
+            }
+
+        }
+
+        cout<<endl;
+
+    }
+
+
+
+    if (dp[X]) cout << "Yes" << endl;
+
+
+    else cout << "No" << endl;
 
 
 
 }
 
+
+
+
+/*
+
+
+Score : 400 points
+
+Problem Statement Takahashi has  N kinds of coins; specifically, for  1≤i≤N, he has  B  i ​   coins worth  A  i ​   yen (the currency in Japan) each.  Determine if Takahashi can pay exactly  X yen (without change) with the coins he currently has.  
+
+Constraints
+ 1≤N≤50
+  1≤X≤10^4
+  1≤ Ai ≤100
+  1≤ Bi ​≤50 
+  Ai ​are pairwise distinct.
+  All values in the input are integers.
+  Input The input is given from Standard Input in the following format:  N  X A  1 ​    B  1 ​   A  2 ​    B  2 ​   ⋮ A  N ​    B  N
+
+Output
+Print Yes if Takahashi can pay exactly  X yen with the coins he currently has; print No otherwise.  Sample Input 1 Copy 2 19 2 3 5 6 Sample Output 1 Copy Yes Takahashi has three  2-yen coins and six  5-yen coins. He can use two  2-yen coins and three  5-yen coins to pay exactly  2×2+5×3=19 yen. Thus, Yes should be printed.  Sample Input 2 Copy 2 18 2 3 5 6 Sample Output 2 Copy No There is no combination of the coins that he can use to pay exactly  18 yen. Thus, No should be printed.  Sample Input 3 Copy 3 1001 1 1 2 1 100 10 Sample Output 3 Copy Yes He need not use all kinds of coins.
+
+
+*/
