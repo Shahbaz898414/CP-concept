7e88e44776a7352e78a63f3ad43557dda1af036d
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-09 16:55:29 +0530

    1

--- level-3/Trees/Questions/A. Timofey and a tree.cpp
+++ level-3/Trees/Questions/A. Timofey and a tree.cpp
@@ -1,20 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define N 100005
+int n, cnt, u[N], v[N], c[N], a[N];
 
+int main()
+{
 
+  int i;
+  scanf("%d", &n);
+  for (i = 0; i < n - 1; i++)
+    scanf("%d%d", &u[i], &v[i]);
+  for (i = 1; i <= n; i++)
+    scanf("%d", &c[i]);
+  for (i = 0; i < n - 1; i++)
+  {
+    if (c[u[i]] != c[v[i]])
+    {
+      cnt++, a[u[i]]++, a[v[i]]++;
+    }
+  }
+  for (i = 1; i <= n; i++)
+  {
+    if (a[i] == cnt)
+    {
+      printf("YES\n%d", i);
+      return 0;
+    }
+  }
+  puts("NO");
 
-
-int main() {
-
- int n;cin>>n;
-
-
-    
-  
   return 0;
 }
 
+/*
 
+#include <cstdio>
+#define N 100005
+int n, cnt, u[N], v[N], c[N], a[N];
+int main() {
+  int i;
+  scanf("%d",&n);
+  for(i=0; i<n-1; i++) scanf("%d%d",&u[i],&v[i]);
+  for(i=1; i<=n; i++) scanf("%d",&c[i]);
+  for(i=0; i<n-1; i++) {
+    if(c[u[i]] != c[v[i]]) {
+      cnt++, a[u[i]]++, a[v[i]]++;
+    }
+  }
+  for(i=1; i<=n; i++) {
+    if(a[i] == cnt) {printf("YES\n%d", i); return 0;}
+  }
+  puts("NO");
+  return 0;
+}
 
-// sigverif
+*/
--- level-3/Trees/Questions/zzz2.cpp
+++ level-3/Trees/Questions/zzz2.cpp
@@ -1,26 +1,59 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void findNumbers(int N) {
-    if (N % 3 != 0) {
-        cout << "-1\n"; // If N is not divisible by 3, no solution exists.
+#define ll long long
+
+void findNumbers(int N)
+{
+
+    if (N % 3 != 0)
+    {
+
+        cout << "-1\n";
+
         return;
     }
 
     int A = N / 3;
+
     int B = N / 3;
+
     int C = 1;
 
     cout << A << " " << B << " " << C << "\n";
 }
 
-int main() {
-    int T, N;
+int main()
+{
+    int T;
     cin >> T;
 
-    while (T--) {
-        cin >> N;
-        findNumbers(N);
+    while (T--)
+    {
+        ll n, m;
+        cin >> n;
+        string s;
+        cin >> s;
+        ll ans = 0;
+        if (s.find('B') == -1)
+            cout << "0\n";
+        else
+        {
+            ll h = n - 1;
+            while (s[h] == 'A')
+                --h;
+            ll pr = h;
+            for (int i = h; i >= 0; --i)
+            {
+                if (s[i] == 'A')
+                {
+                    ans += pr - i;
+                    pr = i;
+                }
+            }
+            cout << ans << '\n';
+        }
     }
 
     return 0;
