ac051b7fa41bf2ee3ed0a2edb23284937e6d3ff9
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-10 20:44:58 +0530

    1

--- level-3/Trees/Questions/G. Path Prefixes.cpp
+++ level-3/Trees/Questions/G. Path Prefixes.cpp
@@ -1,64 +1,99 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long
-
-const int maxn=2e5+5;
-vector<int> ch[maxn];
-int a[maxn];
-int b[maxn];
-int ans[maxn];
-vector<int> vb;
-int curb=0;
-int cura=0;
-
-void dfs(int x){
-    curb+=b[x];
-    cura+=a[x];
-    vb.push_back(curb);
-    ans[x]=upper_bound(vb.begin(),vb.end(),cura)-vb.begin();
-    for(int v:ch[x]){
-        dfs(v);
-    }
-    curb-=b[x];
-    cura-=a[x];
-    vb.pop_back();
-}
-int32_t main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
-    int t;
-    cin>>t;
-    while(t--){
-        int n;cin>>n;
-        for(int i=0;i<n;++i) ch[i].clear();
-        for(int i=1;i<n;++i){
-            int pr,a1,b1;
-            cin>>pr>>a1>>b1;
-            --pr;
-            ch[pr].push_back(i);
-            a[i]=a1;
-            b[i]=b1;
+#define ll long long
+vector<ll> a1, b1;
+ll si;
+ll bs(ll l, ll h, ll k)
+{
+    if(l<=h){
+        ll m=(l+h)/2;
+        
+        if(b1[m]<=k){
+            if(m+1<=h){
+                if(b1[m+1]>k){
+                    return m;
+                }else{
+                    return bs(m+1, h, k);
+                }
+            }else{
+                return m;
+            }
+        }else{
+            return bs(l, m-1, k);
         }
-        dfs(0);
-        for(int i=1;i<n;++i) cout<<ans[i]-1<<' ';
-        cout<<'\n';
+        
+    }else{
+        return -1;
     }
-    return 0;
 }
-
-
-/*
-
-You are given an integer array nums and an integer k.  The frequency of an element x is the number of times it occurs in an array.  An array is called good if the frequency of each element in this array is less than or equal to k.  Return the length of the longest good subarray of nums.  A subarray is a contiguous non-empty sequence of elements within an array.
-
-Example 1:  
-Input: nums = [1,2,3,1,2,3,1,2], k = 2 Output: 6 Explanation: The longest possible good subarray is [1,2,3,1,2,3] since the values 1, 2, and 3 occur at most twice in this subarray. Note that the subarrays [2,3,1,2,3,1] and [3,1,2,3,1,2] are also good. It can be shown that there are no good subarrays with length more than 6. 
-Example 2:
-Input: nums = [1,2,1,2,1,2,1,2], k = 1 Output: 2 Explanation: The longest possible good subarray is [1,2] since the values 1 and 2 occur at most once in this subarray. Note that the subarray [2,1] is also good. It can be shown that there are no good subarrays with length more than 2. 
-Example 3:  
-Input: nums = [5,5,5,5,5,5,5], k = 4 Output: 4 Explanation: The longest possible good subarray is [5,5,5,5] since the value 5 occurs 4 times in this subarray. It can be shown that there are no good subarrays with length more than 4.    Constraints:  1 <= nums.length <= 105 1 <= nums[i] <= 109 1 <= k <= nums.length.
-
-
-
-*/
+void dfs(ll i, ll a[], ll b[], ll ans[], vector<ll> adj[], bool v[])
+{
+    v[i]=true;
+    if(si==0){
+        a1.push_back(a[i]);
+        b1.push_back(b[i]);
+        si++;
+    }else{
+        a1.push_back(a1[si-1]+a[i]);
+        b1.push_back(b1[si-1]+b[i]);
+        si++;
+        
+        ans[i]=bs(0, si-1, a1[si-1]);
+       
+        
+        
+    }
+    
+    ll j, l=adj[i].size();
+    
+    for(j=0; j<l; j++)
+    {
+        if(v[adj[i][j]]==false)
+        {
+            dfs(adj[i][j], a, b, ans, adj, v);
+        }
+    }
+    
+    a1.pop_back();
+    b1.pop_back();
+    si--;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t, n, i, j, p;
+    
+    cin>>t;
+    
+    for(;t--;)
+    {
+        cin>>n;
+        si=0;
+        vector<ll> adj[n+1];
+        bool v[n+1];
+        ll a[n+1], b[n+1], ans[n+1];
+        
+        for(i=0; i<=n; i++)
+        {
+            v[i]=false;
+        }
+        
+        a[1]=b[1]=0;
+        for(i=2; i<=n; i++)
+        {
+            cin>>p>>a[i]>>b[i];
+            adj[i].push_back(p);
+            adj[p].push_back(i);
+        }
+        
+        dfs(1, a, b, ans, adj, v);
+        
+        for(i=2; i<=n; i++){
+            cout<<ans[i]<<" ";
+        }
+        cout<<"\n";
+    }
+}
