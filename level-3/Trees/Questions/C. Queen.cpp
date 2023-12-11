6587b80002b3462460d3414a82e6094032083618
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-11 09:54:18 +0530

    1

--- level-3/Trees/Questions/C. Queen.cpp
+++ level-3/Trees/Questions/C. Queen.cpp
@@ -2,25 +2,30 @@
 using namespace std;
 #define ll long long
 
-bool dfs(int node, int parent, vector<vector<ll>>& adj, vector<ll>& color, vector<ll>& ones) {
+bool dfs(int node, int parent, vector<vector<ll>> &adj, vector<ll> &color, vector<ll> &ones)
+{
     bool fl = true;
-    for (auto c : adj[node]) {
+    for (auto c : adj[node])
+    {
         if (c == parent)
             continue;
 
-        if (!dfs(c, node, adj, color, ones)) {
+        if (!dfs(c, node, adj, color, ones))
+        {
             fl = false;
         }
     }
 
-    if (fl == true and color[node] == 1) {
+    if (fl == true and color[node] == 1)
+    {
         ones.push_back(node);
     }
 
     return color[node] == 1;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
@@ -29,7 +34,8 @@ int main() {
 
     cin >> t;
 
-    for (; t--;) {
+    for (; t--;)
+    {
         ll n;
         cin >> n;
 
@@ -38,10 +44,12 @@ int main() {
 
         int root;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++)
+        {
             int x, y;
             cin >> x >> y;
-            if (x == -1) {
+            if (x == -1)
+            {
                 root = i;
                 continue;
             }
@@ -56,12 +64,14 @@ int main() {
 
         dfs(root, -1, adj, color, ones);
 
-        sort(ones.begin(),ones.end());
+        sort(ones.begin(), ones.end());
 
         if (ones.size() == 0)
             cout << -1 << endl;
-        else {
-            for (auto x : ones) {
+        else
+        {
+            for (auto x : ones)
+            {
                 cout << x << " ";
             }
             cout << endl;
@@ -70,4 +80,56 @@ int main() {
     return 0;
 }
 
+/*
 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,x,y;
+    cin>>n;
+    int z=2,c[n+1]={0};
+    for(i=1;i<=n;i++) {
+        cin>>x>>y;
+        if(y!=1) {
+            c[i]=1;
+            if(x!=-1)
+            c[x]=1;
+        }
+    }
+
+
+    for(i=1;i<=n;i++)
+        if(c[i]!=1) {
+           cout<<i<<" ";
+           z=1;
+        }
+
+    if(z==2)
+      cout<<-1;
+}
+
+
+
+\*/
+
+
+/*
+666666666666666666666666666666666666666666666666666666666666666666666666666666666666++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++445555555555555555555555555555555555555555545556+696666666666666666666666666666666666666666665854
+
+
+
+
+
+
+
+..
+..
+3
+0
+
+.30
+
+
+*/
