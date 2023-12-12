659b060b827da6ef01cdf0f065252e8a2a931d16
Shahbaz898414 <76616023+Shahbaz898414@users.noreply.github.com>
2023-12-12 23:46:38 +0530

    1

--- level-3/Combinatorics + problem solvings/Question/B. Hamming Distance Sum.cpp
+++ level-3/Combinatorics + problem solvings/Question/B. Hamming Distance Sum.cpp
@@ -5,12 +5,26 @@ using namespace std;
 
 const ll mod = 1e9 + 7;
 
+int x[200003];
 
+int main()
+{
 
-int main() {
+  string a, b;
+  cin >> a >> b;
+  for (auto i = 0; i < b.size(); i++)
+    x[i + 1] = x[i] + (b[i] - '0');
 
-  string a,b;cin>>a>>b;
+  int l = b.size() - a.size() + 1;
+  long long ans = 0;
 
-
-  
+  for (auto i = 0; i < a.size(); i++)
+  {
+    if (a[i] == '0')
+      ans += abs(x[i] - x[i + l]);
+    else
+      ans += l - abs(x[i] - x[i + l]);
+  }
+  cout << ans;
 }
+
